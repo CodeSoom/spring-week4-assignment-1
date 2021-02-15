e6d08534e72a8c3250798f131f3101649ff11d56
rlawlstjd0077 <rlawlstjd077@gmail.com>
2021-02-15 15:44:08 +0900

    feat: Add 'Product' domain

---
+++ app/src/main/java/com/codesoom/assignment/domain/Product.java
@@ -0,0 +1,62 @@
+package com.codesoom.assignment.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    private String maker;
+
+    private Long price;
+
+    private String imageURL;
+
+    public Product() { }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getMaker() {
+        return maker;
+    }
+
+    public Long getPrice() {
+        return price;
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setMaker(String maker) {
+        this.maker = maker;
+    }
+
+    public void setPrice(Long price) {
+        this.price = price;
+    }
+
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
---
+++ app/src/test/java/com/codesoom/assignment/domain/ProductTest.java
@@ -0,0 +1,33 @@
+package com.codesoom.assignment.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("Product 클래스")
+class ProductTest {
+    final Long ID = 0L;
+    final String NAME = "My Toy";
+    final String MAKER = "My Home";
+    final Long PRICE = 5000L;
+    final String IMAGE_URL = "https://cdn.pixabay.com/photo/2016/10/01/20/54/mouse-1708347_1280.jpg";
+
+    @DisplayName("Product 생성")
+    @Test
+    void create() {
+        Product product = new Product();
+        product.setId(ID);
+        product.setName(NAME);
+        product.setMaker(MAKER);
+        product.setPrice(PRICE);
+        product.setImageURL(IMAGE_URL);
+
+        assertThat(product.getId()).isEqualTo(ID);
+        assertThat(product.getName()).isEqualTo(NAME);
+        assertThat(product.getMaker()).isEqualTo(MAKER);
+        assertThat(product.getPrice()).isEqualTo(PRICE);
+        assertThat(product.getImageURL()).isEqualTo(IMAGE_URL);
+    }
+}
