e5937fe7b20b45889a19014b4ef7d6aa6ce24d84
rlawlstjd0077 <rlawlstjd077@gmail.com>
2021-02-15 12:10:08 +0900

    feat: Let's start!

--- app/src/main/java/com/codesoom/assignment/App.java
+++ app/src/main/java/com/codesoom/assignment/App.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class App {
     public String getGreeting() {
-        return "Hello, world!";
+        return "Let's Start!";
     }
 
     public static void main(String[] args) {
