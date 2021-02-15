af48b6c126b5f53fb6a444a46d106eee5cbf4f47
etff <wish2rich@gmail.com>
2021-02-15 22:17:40 +0900

    feature: add product domain

---
+++ app/src/main/java/com/codesoom/assignment/product/domain/Product.java
@@ -0,0 +1,36 @@
+package com.codesoom.assignment.product.domain;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String maker;
+
+    private int price;
+
+    private String imageUrl;
+
+    public Product(String name, String maker, int price, String imageUrl) {
+        this.name = name;
+        this.maker = maker;
+        this.price = price;
+        this.imageUrl = imageUrl;
+    }
+
+}
