62d19016c7d74fea3957ca910702ef7df6507702
newoo <newoo4297@naver.com>
2021-02-15 18:52:56 +0900

    Create toy model and pass tests

---
+++ app/src/main/java/com/codesoom/assignment/domain/Toy.java
@@ -0,0 +1,52 @@
+package com.codesoom.assignment.domain;
+
+/**
+ * 이름, 메이커, 가격, 이미지 상태를 가지고 있는 장난감 모델 클래스
+ */
+public class Toy {
+    private Long id;
+    private String name;
+    private String brand;
+    private int price;
+    private String imageUrl;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
---
+++ app/src/test/java/com/codesoom/assignment/domain/ToyTest.java
@@ -0,0 +1,51 @@
+package com.codesoom.assignment.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ToyTest {
+    private final Long toyId = 1L;
+    private final String toyName = "장난감 뱀";
+    private final String toyBrand = "코드숨";
+    private final int toyPrice = 5000;
+    private final String toyImageUrl = "https://cdn.shopify.com/s/files/1/0940/6942/products/DSC0243_800x.jpg";
+
+    private Toy toy;
+
+    @BeforeEach
+    void setToy() {
+        toy = new Toy();
+    }
+
+    @Test
+    void handleId() {
+        toy.setId(toyId);
+        assertThat(toy.getId()).isEqualTo(toyId);
+    }
+
+    @Test
+    void handleName() {
+        toy.setName(toyName);
+        assertThat(toy.getName()).isEqualTo(toyName);
+    }
+
+    @Test
+    void handleBrand() {
+        toy.setBrand(toyBrand);
+        assertThat(toy.getBrand()).isEqualTo(toyBrand);
+    }
+
+    @Test
+    void handlePrice() {
+        toy.setPrice(toyPrice);
+        assertThat(toy.getPrice()).isEqualTo(toyPrice);
+    }
+
+    @Test
+    void handleImageUrl() {
+        toy.setImageUrl(toyImageUrl);
+        assertThat(toy.getImageUrl()).isEqualTo(toyImageUrl);
+    }
+}
