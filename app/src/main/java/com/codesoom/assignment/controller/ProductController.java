274aabc2f579f173ec10b45459ea754d5d238be7
seungin yang <rhfpdk92@naver.com>
2021-02-15 17:40:50 +0900

    Add Get /products

---
+++ app/src/main/java/com/codesoom/assignment/controller/ProductController.java
@@ -0,0 +1,31 @@
+package com.codesoom.assignment.controller;
+
+import com.codesoom.assignment.domain.Product;
+import com.codesoom.assignment.service.ProductService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+//TODO        고양이 장난감 목록 얻기 - GET /products - 테스트 미작성, 장난감 등록 후 확인
+//TODO        고양이 장난감 상세 조회하기 - GET /products/{id}
+//TODO        고양이 장난감 등록하기 - POST /products
+//TODO        고양이 장난감 수정하기 - PATCH /products/{id}
+//TODO        고양이 장난감 삭제하기 - DELETE /products/{id}
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping()
+    public List<Product> getProducts() {
+        return productService.getProducts();
+    }
+
+}
---
+++ app/src/main/java/com/codesoom/assignment/service/ProductService.java
@@ -0,0 +1,21 @@
+package com.codesoom.assignment.service;
+
+import com.codesoom.assignment.domain.Product;
+import com.codesoom.assignment.repository.ProductRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public List<Product> getProducts() {
+        return productRepository.findAll();
+    }
+}
