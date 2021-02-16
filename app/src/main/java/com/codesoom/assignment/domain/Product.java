c67780e2c9fe2a8cdd69a4b1868206f067d0efe1
Las <haho013039@gmail.com>
2021-02-16 20:36:33 +0900

    Implement product domain object

--- app/src/main/java/com/codesoom/assignment/domain/Product.java
+++ app/src/main/java/com/codesoom/assignment/domain/Product.java
@@ -1,4 +1,25 @@
 package com.codesoom.assignment.domain;
 
 public class Product {
+    private Long id;
+
+    private String name;
+    private String maker;
+    private String price;
+    private String imageURL;
+
+    public Product(String name, String maker, String price, String imageURL) {
+        this.name = name;
+        this.maker = maker;
+        this.price = price;
+        this.imageURL = imageURL;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
