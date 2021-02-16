6938d7447c2d2a34a0ea2087555ae53d923de5d5
wenodev <logicalstark1105@gmail.com>
2021-02-17 02:00:39 +0900

    ProductService 생성

---
+++ app/src/main/java/com/codesoom/assignment/application/ProductService.java
@@ -0,0 +1,7 @@
+package com.codesoom.assignment.application;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+}
--- app/src/main/java/com/codesoom/assignment/controller/ProductController.java
+++ app/src/main/java/com/codesoom/assignment/controller/ProductController.java
@@ -1,5 +1,7 @@
 package com.codesoom.assignment.controller;
 
+import com.codesoom.assignment.application.ProductService;
+import com.codesoom.assignment.domain.Product;
 import com.codesoom.assignment.dto.ProductRequestDto;
 import com.codesoom.assignment.dto.ProductResponseDto;
 import org.springframework.http.HttpStatus;
@@ -14,6 +16,9 @@ import java.util.List;
 @RequestMapping("/products")
 @RestController
 public class ProductController {
+
+    private final ProductService productService;
+
 
     /**
      * @return 상품 정보 리스트
@@ -33,6 +38,11 @@ public class ProductController {
         return null;
     }
 
+    /**
+     *
+     * @param id 상품 id
+     * @return id 값으로 찾은 상품
+     */
     @GetMapping("/{id}")
     public ProductResponseDto getProductsById(@PathVariable Long id){
         return null;
