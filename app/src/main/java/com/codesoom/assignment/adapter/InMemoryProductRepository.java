0ba79ef0dd0864d02e5529e33167cd91c567bb8f
Las <haho013039@gmail.com>
2021-02-16 20:52:44 +0900

    Implement in memory product repositroy

---
+++ app/src/main/java/com/codesoom/assignment/adapter/InMemoryProductRepository.java
@@ -0,0 +1,39 @@
+package com.codesoom.assignment.adapter;
+
+import com.codesoom.assignment.domain.Product;
+import com.codesoom.assignment.domain.ProductRepository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Optional;
+
+public class InMemoryProductRepository implements ProductRepository {
+    private final HashMap<Long, Product> inMemoryMap = new HashMap<>();
+    private Long lastId = 1L;
+
+    @Override
+    public void save(Product product) {
+        product.setId(nextId());
+        inMemoryMap.put(product.getId(), product);
+    }
+
+    @Override
+    public Optional<Product> find(Long id) {
+        Product product = inMemoryMap.get(id);
+        if (product == null) {
+            return Optional.empty();
+        }
+        return Optional.of(product);
+    }
+
+    @Override
+    public List<Product> findAll() {
+        return new ArrayList<>(inMemoryMap.values());
+    }
+
+    @Override
+    public Long nextId() {
+        return lastId++;
+    }
+}
