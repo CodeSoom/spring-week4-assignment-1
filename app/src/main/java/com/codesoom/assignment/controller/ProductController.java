6e4bc39160368ab792bd03d36b8f686c45d01803
wenodev <logicalstark1105@gmail.com>
2021-02-16 23:25:12 +0900

    RequestDto 생성

--- app/src/main/java/com/codesoom/assignment/controller/ProductController.java
+++ app/src/main/java/com/codesoom/assignment/controller/ProductController.java
@@ -1,12 +1,23 @@
 package com.codesoom.assignment.controller;
 
+import com.codesoom.assignment.dto.ProductRequestDto;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * 상품 컨트롤러
  * @Author wenodev
  */
+@RequestMapping("/products")
 @RestController
 public class ProductController {
+
+    @GetMapping
+    public List<ProductRequestDto> getProducts(){
+
+    }
+
 }
