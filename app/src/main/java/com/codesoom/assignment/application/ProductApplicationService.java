cd1623d4b6e4a60a903b7d9465c0630fc22bff52
Las <haho013039@gmail.com>
2021-02-16 20:38:21 +0900

    Implement ProductApplicationService get all product and create product

--- app/src/main/java/com/codesoom/assignment/application/ProductApplicationService.java
+++ app/src/main/java/com/codesoom/assignment/application/ProductApplicationService.java
@@ -1,16 +1,24 @@
 package com.codesoom.assignment.application;
 
 import com.codesoom.assignment.domain.Product;
+import com.codesoom.assignment.domain.ProductRepository;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class ProductApplicationService {
+    ProductRepository productRepository;
+
+    public ProductApplicationService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
     public List<Product> getAllProducts() {
-        return new ArrayList<>();
+        return productRepository.findAll();
     }
 
     public Product createProduct(String name, String maker, String price, String imageURL) {
-        return null;
+        Product newProduct = new Product(name, maker, price, imageURL);
+        productRepository.save(newProduct);
+        return newProduct;
     }
 }
