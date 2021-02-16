33c180c580f7ae07ddef437cf879797ff7918d59
rlawlstjd0077 <rlawlstjd077@gmail.com>
2021-02-16 20:20:07 +0900

    refactor: Rollback 'Let's Start!'

--- app/src/main/java/com/codesoom/assignment/App.java
+++ app/src/main/java/com/codesoom/assignment/App.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class App {
     public String getGreeting() {
-        return "Let's Start!";
+        return "Hello, world!";
     }
 
     public static void main(String[] args) {
