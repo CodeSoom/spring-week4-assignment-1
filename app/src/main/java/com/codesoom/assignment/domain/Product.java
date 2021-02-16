d4bc7f1e69c1ba9c1864c3d2c68d6dc593f57eae
wenodev <logicalstark1105@gmail.com>
2021-02-16 23:07:19 +0900

    속성 생성

--- app/src/main/java/com/codesoom/assignment/domain/Product.java
+++ app/src/main/java/com/codesoom/assignment/domain/Product.java
@@ -1,4 +1,44 @@
 package com.codesoom.assignment.domain;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+/**
+ * 고양이 장남간 가게의 상품 엔티티
+ * @Author wenodev
+ */
+
+@NoArgsConstructor
+@Getter
+@Table(name = "soom_product")
+@Entity
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String maker;
+
+    @Column
+    private int price;
+
+    @Column
+    private String imgName;
+
+    @Builder
+    public Product(String name, String maker, int price, String imgName) {
+        this.name = name;
+        this.maker = maker;
+        this.price = price;
+        this.imgName = imgName;
+    }
+
 }
--- app/src/test/java/com/codesoom/assignment/domain/ProductTest.java
+++ app/src/test/java/com/codesoom/assignment/domain/ProductTest.java
@@ -1,7 +1,43 @@
 package com.codesoom.assignment.domain;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+
+// 1. 필요한 변수 작성 : 이름, 메이커, 가격, 이미지
+
 class ProductTest {
+
+    private Product product;
+
+    String name = "toy-snake";
+    String maker = "codesoom";
+    int price = 5000;
+    String imgName = "snake-sample.jpg";
+
+    @BeforeEach
+    void setUp(){
+
+        product = Product.builder()
+                .name(name)
+                .maker(maker)
+                .price(price)
+                .imgName(imgName)
+                .build();
+
+    }
+
+    @DisplayName("변수값 확인")
+    @Test
+    void testProduct(){
+        assertThat(name).isEqualTo(product.getName());
+        assertThat(maker).isEqualTo(product.getMaker());
+        assertThat(name).isEqualTo(product.getName());
+        assertThat(imgName).isEqualTo(product.getImgName());
+    }
 
 }
