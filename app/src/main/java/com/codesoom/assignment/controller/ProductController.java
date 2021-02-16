ef722df5fcac76dec21f1293efda2a630b8a12bc
wenodev <logicalstark1105@gmail.com>
2021-02-16 23:22:01 +0900

    controller 생성

---
+++ app/src/main/java/com/codesoom/assignment/controller/ProductController.java
@@ -0,0 +1,12 @@
+package com.codesoom.assignment.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 상품 컨트롤러
+ * @Author wenodev
+ */
+@RestController
+public class ProductController {
+}
