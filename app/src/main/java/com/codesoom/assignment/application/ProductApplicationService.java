151467998839534c6c7b00c9632037b2d535b82b
Las <haho013039@gmail.com>
2021-02-16 20:14:25 +0900

    모든 task를 가져오는 경우에 대하여 임시로 구현하였습니다.

---
+++ app/src/main/java/com/codesoom/assignment/application/ProductApplicationService.java
@@ -0,0 +1,12 @@
+package com.codesoom.assignment.application;
+
+import com.codesoom.assignment.domain.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductApplicationService {
+    public List<Product> getAllProducts() {
+        return new ArrayList<>();
+    }
+}
