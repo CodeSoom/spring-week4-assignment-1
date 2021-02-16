532f6716dd877393dad28a8faec4fb1791bc5b72
wenodev <logicalstark1105@gmail.com>
2021-02-16 23:32:06 +0900

    getProducts 생성

--- app/src/main/java/com/codesoom/assignment/controller/ProductController.java
+++ app/src/main/java/com/codesoom/assignment/controller/ProductController.java
@@ -1,9 +1,7 @@
 package com.codesoom.assignment.controller;
 
 import com.codesoom.assignment.dto.ProductResponseDto;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,7 +15,7 @@ public class ProductController {
 
     @GetMapping
     public List<ProductResponseDto> getProducts(){
-
+        return null;
     }
 
 }
--- app/src/test/java/com/codesoom/assignment/controller/ProductControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controller/ProductControllerTest.java
@@ -1,7 +1,26 @@
 package com.codesoom.assignment.controller;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@WebMvcTest(ProductController.class)
 class ProductControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    private final String BASE_URL = "/products";
+
+    @Test
+    void testGetProducts() throws Exception {
+        mockMvc.perform(get(BASE_URL))
+                .andExpect(status().isOk());
+    }
 
 }
