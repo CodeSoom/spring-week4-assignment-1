d34ab997d7420a4339d8f482a5a294743a8fd42b
rlawlstjd0077 <rlawlstjd077@gmail.com>
2021-02-17 20:49:45 +0900

    refactor: Add javadoc in 'ProductController'

--- app/src/main/java/com/codesoom/assignment/controllers/ProductController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/ProductController.java
@@ -15,6 +15,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 상품에 대한 HTTP 요청 핸들러.
+ *
+ * @see ProductService
+ * @see Product
+ */
 @RestController
 @RequestMapping("/products")
 @CrossOrigin
@@ -25,27 +31,53 @@ public class ProductController {
         this.productService = productService;
     }
 
+    /**
+     * 주어진 product을 저장한 뒤, product를 응답힙니다.
+     *
+     * @param source 저장하고자 하는 product
+     * @return 저장된 product
+     */
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Product create(@RequestBody Product source) {
         return productService.create(source);
     }
 
+    /**
+     * 모든 product 리스트를 응답합니다.
+     */
     @GetMapping
     public List<Product> list() {
         return productService.findAll();
     }
 
+    /**
+     * 주어진 id에 해당하는 product를 찾아 응답합니다.
+     *
+     * @param id product id
+     * @return id와 일치하는 product
+     */
     @GetMapping(path = "{id}")
     public Product find(@PathVariable Long id) {
         return productService.find(id);
     }
 
+    /**
+     * 주어진 id에 해당하는 product를 수정한 후 수정된 product를 응답합니다.
+     *
+     * @param id product id
+     * @param source 수정하고자 하는 product
+     * @return 수정된 product
+     */
     @PatchMapping(path = "{id}")
     public Product update(@PathVariable Long id, @RequestBody Product source) {
         return productService.update(id, source);
     }
 
+    /**
+     * 주어진 id에 해당하는 product를 삭제합니다.
+     * @param id product id
+     */
     @DeleteMapping(path = "{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable Long id) {
