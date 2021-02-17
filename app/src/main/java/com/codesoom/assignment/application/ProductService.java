e681034b6907823d7d61f356b2f09a7a04c855d2
Lee Dong Wook <sh95119@gmail.com>
2021-02-17 22:42:11 +0900

    FEATURE: add createProduct method of ProductService Class

---
+++ app/src/main/java/com/codesoom/assignment/application/ProductService.java
@@ -0,0 +1,23 @@
+package com.codesoom.assignment.application;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.codesoom.assignment.domain.Product;
+import com.codesoom.assignment.repository.ProductRepository;
+
+@Service
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public Product createProduct(Product product) {
+        return productRepository.save(product);
+    }
+
+}
