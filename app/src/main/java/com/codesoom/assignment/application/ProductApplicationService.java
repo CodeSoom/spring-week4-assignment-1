638b641d7e97858b820f045c0a1b7ac0ec2c6392
Las <haho013039@gmail.com>
2021-02-17 21:17:44 +0900

    add javadoc to application service

--- app/src/main/java/com/codesoom/assignment/application/ProductApplicationService.java
+++ app/src/main/java/com/codesoom/assignment/application/ProductApplicationService.java
@@ -7,17 +7,33 @@ import com.codesoom.assignment.domain.ProductRepository;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * ProductApplicationService는 product에 대한 use case들을 담당합니다.
+ */
 public class ProductApplicationService {
     ProductRepository productRepository;
 
+    /**
+     * @param productRepository product를 저장하고 가져오는데 필요한 repositroy
+     */
     public ProductApplicationService(ProductRepository productRepository) {
         this.productRepository = productRepository;
     }
 
+    /**
+     * @return 생성된 모든 product들을 반환합니다.
+     */
     public List<Product> getAllProducts() {
         return productRepository.findAll();
     }
 
+    /**
+     * @param name 생성될 product의 이름
+     * @param maker 생성될 product의 제조회사
+     * @param price 생성될 product의 가격
+     * @param imageURL 생성될 product의 이미지 주소
+     * @return 생성된 product을 반환합니다.
+     */
     public Product createProduct(String name, String maker, String price, String imageURL) {
         ProductId id = productRepository.nextId();
         Product newProduct = new Product(id, name, maker, price, imageURL);
@@ -25,6 +41,9 @@ public class ProductApplicationService {
         return newProduct;
     }
 
+    /**
+     * @param id 삭제할 product의 id
+     */
     public void deleteProduct(Long id) {
         Optional<Product> product = productRepository.find(id);
         productRepository.remove(product.get());
