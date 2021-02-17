fb9905db2d7ca771f342e0136ce46e9870c64b68
Gyu <melon9751@naver.com>
2021-02-17 19:42:16 +0900

    Make GET /products ControllerTest, domain, service
    Make list() method in ControllerTest, Product.class in domain
    package, ProductService.class in application package

---
+++ app/src/main/java/com/codesoom/assignment/application/ProductService.java
@@ -0,0 +1,9 @@
+package com.codesoom.assignment.application;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+
+
+}
--- app/src/main/java/com/codesoom/assignment/controllers/ProductController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/ProductController.java
@@ -1,13 +1,22 @@
 package com.codesoom.assignment.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.codesoom.assignment.application.ProductService;
+import com.codesoom.assignment.domain.Product;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class ProductController {
 
-    @RequestMapping("/products")
-    public String list() {
-        return null;
+    private final ProductService productService;
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("/products")
+    public List<Product> list() {
+        return productService.getProducts();
     }
 }
