b565bd76ba780b902a8e589233c074ac53184f06
Lee Dong Wook <sh95119@gmail.com>
2021-02-18 22:05:03 +0900

    TEST: add ProductControllerWebTest

---
+++ app/src/test/java/com/codesoom/assignment/controllers/ProductControllerWebTest.java
@@ -0,0 +1,24 @@
+package com.codesoom.assignment.controllers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ProductControllerWebTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void createProductTest() throws Exception {
+        mockMvc.perform(post("/products"))
+            .andExpect(status().isCreated());
+    }
+}
