d07d9ba10f47f8f15a4771125131bf58de41461b
rlawlstjd0077 <rlawlstjd077@gmail.com>
2021-02-18 19:29:53 +0900

    refactor: Refactor test message

--- app/src/test/java/com/codesoom/assignment/domain/ProductTest.java
+++ app/src/test/java/com/codesoom/assignment/domain/ProductTest.java
@@ -7,23 +7,21 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("Product 클래스")
 class ProductTest {
-    final Long ID = 0L;
+    //fixture
     final String NAME = "My Toy";
     final String MAKER = "My Home";
     final Long PRICE = 5000L;
     final String IMAGE_URL = "https://cdn.pixabay.com/photo/2016/10/01/20/54/mouse-1708347_1280.jpg";
 
-    @DisplayName("Product가 정상적으로 생성되고 값이 세팅되었는지 확인하는 테스트")
+    @DisplayName("Product가 정상적으로 생성되고 값이 세팅되었는지 확인합니다")
     @Test
-    void create() {
-        //when
+    void productCreateTest() {
         Product product = new Product();
         product.setName(NAME);
         product.setMaker(MAKER);
         product.setPrice(PRICE);
         product.setImageUrl(IMAGE_URL);
 
-        //then
         assertThat(product.getName()).isEqualTo(NAME);
         assertThat(product.getMaker()).isEqualTo(MAKER);
         assertThat(product.getPrice()).isEqualTo(PRICE);
