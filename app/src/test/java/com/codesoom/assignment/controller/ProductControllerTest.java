a14303fb3ad546d40614c22496a924c8976d51d4
Las <haho013039@gmail.com>
2021-02-18 21:07:16 +0900

    Write controller test for get specific product

--- app/src/test/java/com/codesoom/assignment/controller/ProductControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controller/ProductControllerTest.java
@@ -2,6 +2,7 @@ package com.codesoom.assignment.controller;
 
 import com.codesoom.assignment.adapter.InMemoryProductRepository;
 import com.codesoom.assignment.application.ProductApplicationService;
+import com.codesoom.assignment.domain.Product;
 import com.codesoom.assignment.domain.ProductRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -12,11 +13,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class ProductControllerTest {
     private ProductController controller;
+    private ProductApplicationService service;
 
     @BeforeEach
     void initController() {
         ProductRepository repository = new InMemoryProductRepository();
-        ProductApplicationService service = new ProductApplicationService(repository);
+        service = new ProductApplicationService(repository);
         controller = new ProductController(service);
     }
 
@@ -25,5 +27,21 @@ public class ProductControllerTest {
         List<ProductDTO> products = controller.getAllProduct();
 
         assertThat(products).hasSize(0);
+    }
+
+    @Test
+    void getSpecificProduct() {
+        String name = "고양이 인형";
+        String maker = "라스 공방";
+        String price = "1000원";
+        String imageURL = "https://magical.dev/static/las.jpg";
+
+        Product createdProduct = service.createProduct(name, maker, price, imageURL);
+
+        ProductDTO product = controller.getSpecificProduct(createdProduct.productId().id());
+
+        assertThat(product).isNotNull();
+        assertThat(product.id).isEqualTo(createdProduct.productId().id());
+
     }
 }
