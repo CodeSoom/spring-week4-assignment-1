0524dd23265b88f6f1a394ec550941031f3dfe2b
양효주 <yhyojoo@icloud.com>
2021-02-18 14:04:31 +0900

    add @Getter @Setter in Product.java

--- app/src/main/java/com/codesoom/assignment/domain/Product.java
+++ app/src/main/java/com/codesoom/assignment/domain/Product.java
@@ -1,5 +1,8 @@
 package com.codesoom.assignment.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -7,6 +10,8 @@ import javax.persistence.Id;
 // 1. Entity (domain)
 //  ** RDB의 Entity와 다름
 // 2. JPA의 Entity 역할도 같이 함
+@Getter
+@Setter
 @Entity
 public class Product {
     // Identifier - Unique Key
@@ -17,28 +22,4 @@ public class Product {
     private String name;
 
     private int price;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getPrice() {
-        return price;
-    }
-
-    public void setPrice(int price) {
-        this.price = price;
-    }
 }
