f634657dd182a7d0e5432f1d74ae11da6eaad2b0
Las <haho013039@gmail.com>
2021-02-18 20:44:54 +0900

    add annotation to controller

--- app/src/main/java/com/codesoom/assignment/controller/ProductController.java
+++ app/src/main/java/com/codesoom/assignment/controller/ProductController.java
@@ -1,9 +1,16 @@
 package com.codesoom.assignment.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@RestController
+@RequestMapping("/products")
 public class ProductController {
+    @GetMapping
     public List<ProductDTO> getAllProduct() {
         return new ArrayList<>();
     }
