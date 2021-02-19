a98455ffc27121f993caa58b59f8526ed5ac9d10
seungin yang <rhfpdk92@naver.com>
2021-02-19 16:31:51 +0900

    Add javadoc

--- app/src/main/java/com/codesoom/assignment/domain/Product.java
+++ app/src/main/java/com/codesoom/assignment/domain/Product.java
@@ -11,6 +11,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Lob;
 
+/**
+ * 상품 엔티티
+ * Equals()와 Hashcode()를 생성할때 명시적으로 id를 포함시켜 동등성 비교를 id로 한다.
+ */
 @Entity
 @Getter
 @NoArgsConstructor
@@ -21,8 +25,14 @@ public class Product {
     @GeneratedValue
     private Long id;
 
+    /**
+     * 상품명
+     */
     private String name;
 
+    /**
+     * 상품의 브랜드
+     */
     private String maker;
 
     private Integer price;
@@ -39,6 +49,12 @@ public class Product {
         this.imageUrl = imageUrl;
     }
 
+    /**
+     * 상품을 수하고 수정된 상품을 반환합니다.
+     *
+     * @param productDto 수정할 상품의 정보
+     * @return 수정된 product
+     */
     public Product update(ProductDto productDto) {
         this.name = productDto.getName();
         this.maker = productDto.getMaker();
