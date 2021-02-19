ef2ba32156d350d1b23ba3636a77a2efcccd0fdc
Lee Dong Wook <sh95119@gmail.com>
2021-02-19 23:44:39 +0900

    CLEAN UP: remove unused comments and imports

--- app/src/test/java/com/codesoom/assignment/controllers/ProductControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/ProductControllerTest.java
@@ -1,15 +1,6 @@
 package com.codesoom.assignment.controllers;
 
-import static org.assertj.core.api.Assertions.*;
-
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-// TODO
-// 1. 어떤 것을 작성할까?
-// 2. 어떤 테스트가 필요할까?
-// C.R.U.D 뭐 이런거 있겠지
 
 class ProductControllerTest {
 
