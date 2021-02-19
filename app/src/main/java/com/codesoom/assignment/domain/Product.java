860f88786197ce96664591f40749b04268a88386
Minwook Park <mwpark0975@gmail.com>
2021-02-19 21:58:15 +0900

    refactoring test for getProducts

--- app/src/main/java/com/codesoom/assignment/domain/Product.java
+++ app/src/main/java/com/codesoom/assignment/domain/Product.java
@@ -1,12 +1,16 @@
 package com.codesoom.assignment.domain;
 
+import lombok.Builder;
+
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
+@Builder
 public class Product {
 
-    @Id
+    @Id @GeneratedValue
     private Long id;
     private String name;
     private String maker;
--- app/src/test/java/com/codesoom/assignment/application/ProductServiceTest.java
+++ app/src/test/java/com/codesoom/assignment/application/ProductServiceTest.java
@@ -3,39 +3,61 @@ package com.codesoom.assignment.application;
 import com.codesoom.assignment.domain.Product;
 import com.codesoom.assignment.domain.ProductRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
-@SpringBootTest
 class ProductServiceTest {
 
     private ProductService productService;
-    @Autowired
     private ProductRepository productRepository;
+    private List<Product> products;
 
     @BeforeEach
     void setUp() {
 
-        List<Product> products = new ArrayList<>();
+        productRepository = mock(ProductRepository.class);
+        productService = new ProductService(productRepository);
 
-        given(productRepository.findAll()).willReturn(products);
+        products = new ArrayList<>();
+        Product product1 = Product.builder()
+                .id(1L)
+                .name("name 1")
+                .image("imageURL 1")
+                .maker("brand 1")
+                .price(100)
+                .build();
+        Product product2 = Product.builder()
+                .id(2L)
+                .name("name 2")
+                .image("imageURL 2")
+                .maker("brand 2")
+                .price(200)
+                .build();
+
+        products.add(product1);
+        products.add(product2);
+
     }
 
     @Test
+    @DisplayName("모든 상품을 조회한다")
     void getProducts() {
+        given(productRepository.findAll()).willReturn(products);
 
-        productService.getProducts();
+        List<Product> products = productService.getProducts();
+
         verify(productRepository).findAll();
+
+        assertThat(products).hasSize(2);
     }
+
+
 }
