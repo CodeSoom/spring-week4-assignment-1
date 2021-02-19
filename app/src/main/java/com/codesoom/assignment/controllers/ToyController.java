af4b772c83f01671907f6a00d11c1f58af5ca598
newoo <newoo4297@naver.com>
2021-02-19 20:06:03 +0900

    Pass GET /products request tests and create ToyController

--- app/src/main/java/com/codesoom/assignment/controllers/ToyController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/ToyController.java
@@ -1,5 +1,46 @@
 package com.codesoom.assignment.controllers;
 
-public class ToyController {
+import com.codesoom.assignment.application.ToyService;
+import com.codesoom.assignment.domain.Toy;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+@CrossOrigin
+public class ToyController {
+    private ToyService toyService;
+
+    public ToyController(ToyService toyService) {
+        this.toyService = toyService;
+    }
+
+    @GetMapping
+    public List<Toy> list() {
+        return toyService.getToys();
+    }
+
+    @GetMapping("{id}")
+    public Toy detail(@PathVariable Long id) {
+        return null;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Toy create(@RequestBody Toy toy) {
+        return null;
+    }
+
+    @PatchMapping("{id}")
+    public Toy patch(@PathVariable Long id, @RequestBody Toy toy) {
+        return null;
+    }
+
+    @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id) {
+
+    }
 }
