2d05ed4506c7c89a19b53635f74a6eae40b2cfe5
seungin yang <rhfpdk92@naver.com>
2021-02-19 16:55:06 +0900

    Add CrossOrigin annotation

--- app/src/main/java/com/codesoom/assignment/controller/ProductController.java
+++ app/src/main/java/com/codesoom/assignment/controller/ProductController.java
@@ -6,6 +6,7 @@ import com.codesoom.assignment.exception.ProductNotFoundException;
 import com.codesoom.assignment.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -31,6 +32,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/products")
 @RequiredArgsConstructor
+@CrossOrigin
 public class ProductController {
 
     private final ProductService productService;
