45273ed4bf118f394253bb98554fc14cda7c2458
Lee Dong Wook <sh95119@gmail.com>
2021-02-20 00:37:21 +0900

    TEST: add getProducts() test of ProductController
    장난감 리스트에 장난감이 있을 때와, 없을 때 비어있는 것과 비어있지 않는것을 보장하는 테스트를 추가해주었다.

--- app/src/test/java/com/codesoom/assignment/controllers/ProductControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/ProductControllerTest.java
@@ -1,18 +1,49 @@
 package com.codesoom.assignment.controllers;
 
+import static org.mockito.Mockito.mock;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import com.codesoom.assignment.application.ProductService;
+import com.codesoom.assignment.domain.Product;
 
 class ProductControllerTest {
 
-    private ProductController productController;
+    ProductController productController;
+    ProductService productService;
 
     @BeforeEach
     void setUp() {
-        productController = new ProductController();
+        productService = mock(ProductService.class);
+        productController = new ProductController(productService);
     }
 
+    @DisplayName("장난감 리스트에 장난감이 아무것도 없을 때, 비어있는 것을 보장하는 테스트입니다.")
     @Test
-    void createController() {
-        assertThat(productController).isNotNull();
+    void getProductListFromEmptyListTest() {
+        List<Product> productList = new ArrayList<>();
+        given(productService.getProducts()).willReturn(productList);
+        assertThat(productController.getProducts()).isEmpty();
+    }
+
+    @DisplayName("장난감 리스트에 장난감이 있을 때, 비어있지 않는 것을 보장하는 테스트입니다.")
+    @Test
+    void getProductListFromSomeListTest() {
+        List<Product> productList = new ArrayList<>();
+        Product product1 = new Product();
+        Product product2 = new Product();
+        Product product3 = new Product();
+
+        productList.add(product1);
+        productList.add(product2);
+        productList.add(product3);
+
+        given(productService.getProducts()).willReturn(productList);
+
+        assertThat(productController.getProducts()).isNotEmpty();
     }
 }
