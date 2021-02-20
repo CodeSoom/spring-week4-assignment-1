811bae6554674837443e12e84f7da3c43393b3f9
thkim <thkim2017@gmail.com>
2021-02-20 14:26:37 +0900

    Implement ProductController With list() method

--- app/src/main/java/com/codesoom/assignment/controllers/ProductController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/ProductController.java
@@ -1,4 +1,17 @@
 package com.codesoom.assignment.controllers;
 
+import com.codesoom.assignment.domain.Product;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
 public class ProductController {
+    @GetMapping
+    List<Product> list() {
+        return null;
+    }
 }
