08a0d7405034b6aa65f2023c7bbf54cf8258eb3e
Lee Dong Wook <dwlee@crscube.io>
2021-02-20 15:20:10 +0900

    TEST: add test of ProductController
    존재하는 아이디를 getProduct()로 호출하였을 때 예외를 발생시키는 경우와 존재하는 아이디를 호출하였을 때, 예외를 발생시키지 않는 테스트를 작성하였다.

--- app/src/test/java/com/codesoom/assignment/controllers/ProductControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/ProductControllerTest.java
@@ -49,4 +49,35 @@ class ProductControllerTest {
 
         assertThat(productController.getProducts()).isNotEmpty();
     }
+
+    @DisplayName("존재하는 장난감 ID를 호출하였을 떄, 유효한 장난감을 반환하는 것을 보장하는 테스트")
+    @Test
+    void getProductWithValidIdTest() {
+        Product product1 = new Product();
+        product1.setName("탱탱볼");
+        product1.setBrand("토이저러스");
+        product1.setPrice(500L);
+        product1.setImage("rubber_ball.png");
+        Product product2 = new Product();
+        Product product3 = new Product();
+
+        given(productService.getProduct(1L)).willReturn(product1);
+        given(productService.getProduct(2L)).willReturn(product2);
+        given(productService.getProduct(3L)).willReturn(product3);
+
+        assertThat(productController.getProduct(1L)).isEqualTo(product1);
+        assertThat(productController.getProduct(2L)).isEqualTo(product2);
+        assertThat(productController.getProduct(3L)).isEqualTo(product3);
+
+        assertThat(productController.getProduct(1L).getName()).isEqualTo("탱탱볼");
+        assertThat(productController.getProduct(1L).getBrand()).isEqualTo("토이저러스");
+        assertThat(productController.getProduct(1L).getPrice()).isEqualTo(500L);
+        assertThat(productController.getProduct(1L).getImage()).isEqualTo("rubber_ball.png");
+    }
+
+    @DisplayName("존재하지 않는 장난감 ID를 호출하였을 떄, 장난감이 존재하지 않는 예외를 호출하는 것을 보장하는 테스트")
+    @Test
+    void getProductWithInValidIdTest() {
+        given(productService.getProduct(100L)).willThrow(productNonExistException.class);
+    }
 }
