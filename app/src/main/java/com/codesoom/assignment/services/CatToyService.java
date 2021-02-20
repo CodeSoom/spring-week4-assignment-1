c9e42fdf0a18de085f0761cdef9e9f7709adc3ba
juunini <juuni.ni.i@gmail.com>
2021-02-20 21:49:06 +0900

    Fill CatToyService method logic and tests

--- app/src/main/java/com/codesoom/assignment/services/CatToyService.java
+++ app/src/main/java/com/codesoom/assignment/services/CatToyService.java
@@ -1,6 +1,8 @@
 package com.codesoom.assignment.services;
 
+import com.codesoom.assignment.exceptions.ToyNotFoundException;
 import com.codesoom.assignment.models.Toy;
+import com.codesoom.assignment.repositories.ToyRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -9,28 +11,38 @@ import java.util.List;
 @Service
 @Transactional
 public class CatToyService implements ToyService {
+    private final ToyRepository toyRepository;
+
+    public CatToyService(ToyRepository toyRepository) {
+        this.toyRepository = toyRepository;
+    }
+
     @Override
     public List<Toy> find() {
-        return null;
+        return toyRepository.findAll();
     }
 
     @Override
     public Toy find(Long id) {
-        return null;
+        return toyRepository
+                .findById(id)
+                .orElseThrow(() -> new ToyNotFoundException(id));
     }
 
     @Override
     public void insert(Toy toy) {
-
+        toyRepository.save(toy);
     }
 
     @Override
     public void modify(Long id, Toy toy) {
-
+        this.find(id).modify(toy);
     }
 
     @Override
     public void delete(Long id) {
+        this.find(id);
 
+        toyRepository.deleteById(id);
     }
 }
--- app/src/test/java/com/codesoom/assignment/services/CatToyServiceTest.java
+++ app/src/test/java/com/codesoom/assignment/services/CatToyServiceTest.java
@@ -1,14 +1,32 @@
 package com.codesoom.assignment.services;
 
+import com.codesoom.assignment.exceptions.ToyNotFoundException;
+import com.codesoom.assignment.models.CatToy;
+import com.codesoom.assignment.models.Toy;
+import com.codesoom.assignment.repositories.ToyRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @DisplayName("CatToyService 클래스")
 class CatToyServiceTest {
-    private final ToyService givenCatToyService = new CatToyService();
+    private final Toy givenToy1 = new CatToy(0L, "캣닢", "(주)고양이컴퍼니", 1000D, "https://cat.toy/cat-nip.png");
+    private final Toy givenToy2 = new CatToy(1L, "캣타워", "(주)고양이컴퍼니", 10000D, "https://cat.toy/cat-tower.png");
+
+    private ToyRepository toyRepository;
+    private ToyService catToyService;
+
+    @BeforeEach
+    void setup() {
+        toyRepository = new ToyRepository.Fake();
+        catToyService = new CatToyService(toyRepository);
+    }
 
     @Nested
     @DisplayName("find 메소드는")
@@ -16,10 +34,18 @@ class CatToyServiceTest {
         @Nested
         @DisplayName("주어진 id가 없을 때")
         class Context_without_given_id {
+            @BeforeEach
+            void setup() {
+                toyRepository.save(givenToy1);
+                toyRepository.save(givenToy2);
+            }
+
             @Test
             @DisplayName("모든 장난감들을 리턴한다.")
             void It_returns_all_toy() {
+                List<Toy> toys = catToyService.find();
 
+                assertThat(toys).hasSize(2);
             }
         }
 
@@ -29,20 +55,30 @@ class CatToyServiceTest {
             @Nested
             @DisplayName("주어진 id의 장난감이 없을 때.")
             class Context_when_not_exist_given_id {
-                @Test
-                @DisplayName("id를 찾을 수 없다는 예외를 던진다.")
-                void It_throws_not_found_id_exception() {
+                private final Long givenID = 1L;
 
+                @Test
+                @DisplayName("장난감을 찾을 수 없다는 예외를 던진다.")
+                void It_throws_not_found_id_exception() {
+                    assertThatThrownBy(() -> catToyService.find(givenID))
+                            .isInstanceOf(ToyNotFoundException.class);
                 }
             }
 
             @Nested
             @DisplayName("주어진 id의 장난감이 있을 때.")
             class Context_when_exist_given_id {
+                @BeforeEach
+                void setup() {
+                    toyRepository.save(givenToy1);
+                }
+
                 @Test
                 @DisplayName("장난감을 리턴한다.")
                 void It_returns_toy() {
+                    Toy toy = catToyService.find(givenToy1.id());
 
+                    assertThat(toy.id()).isEqualTo(givenToy1.id());
                 }
             }
         }
@@ -54,7 +90,8 @@ class CatToyServiceTest {
         @Test
         @DisplayName("주어진 장난감을 저장한다.")
         void It_save_given_toy() {
-
+            catToyService.insert(givenToy1);
+            catToyService.find(givenToy1.id());
         }
     }
 
@@ -67,17 +104,29 @@ class CatToyServiceTest {
             @Test
             @DisplayName("id를 찾을 수 없다는 예외를 던진다.")
             void It_throws_not_found_id_exception() {
-
+                assertThatThrownBy(() -> catToyService.modify(givenToy1.id(), givenToy1))
+                    .isInstanceOf(ToyNotFoundException.class);
             }
         }
 
         @Nested
         @DisplayName("주어진 id의 장난감이 있을 때.")
         class Context_when_exist_given_id {
+            @BeforeEach
+            void setup() {
+                toyRepository.save(givenToy1);
+            }
+
+            private final Toy modifiedCatToy = new CatToy(givenToy1.id(), "마따따비 막대", givenToy1.brand(), givenToy1.price(), givenToy1.imageURL());
+
             @Test
             @DisplayName("해당 id의 장난감 정보를 변경한다.")
             void It_modify_given_id_toy() {
+                catToyService.modify(givenToy1.id(), modifiedCatToy);
 
+                final Toy toy = catToyService.find(modifiedCatToy.id());
+
+                assertThat(toy.name()).isEqualTo(modifiedCatToy.name());
             }
         }
     }
@@ -91,17 +140,26 @@ class CatToyServiceTest {
             @Test
             @DisplayName("id를 찾을 수 없다는 예외를 던진다.")
             void It_throws_not_found_id_exception() {
-
+                assertThatThrownBy(() -> catToyService.delete(givenToy1.id()))
+                        .isInstanceOf(ToyNotFoundException.class);
             }
         }
 
         @Nested
         @DisplayName("주어진 id의 장난감이 있을 때.")
         class Context_when_exist_given_id {
+            @BeforeEach
+            void setup() {
+                toyRepository.save(givenToy1);
+            }
+
             @Test
             @DisplayName("해당 id의 장난감을 제거한다.")
             void It_delete_given_id_toy() {
+                catToyService.delete(givenToy1.id());
 
+                assertThatThrownBy(() -> catToyService.find(givenToy1.id()))
+                        .isInstanceOf(ToyNotFoundException.class);
             }
         }
     }
