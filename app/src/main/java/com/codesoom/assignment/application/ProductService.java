7d403edea7ccf2d1f257a8ebb0f4ec533c43d1b2
Minwook Park <mwpark0975@gmail.com>
2021-02-20 17:14:51 +0900

    imeplement ProductService - createProduct

--- app/src/main/java/com/codesoom/assignment/application/ProductService.java
+++ app/src/main/java/com/codesoom/assignment/application/ProductService.java
@@ -18,4 +18,8 @@ public class ProductService {
     public List<Product> getProducts() {
         return productRepository.findAll();
     }
+
+    public Product createProduct(Product product) {
+        return productRepository.save(product);
+    }
 }
