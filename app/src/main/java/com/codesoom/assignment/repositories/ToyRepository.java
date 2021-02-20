6e820a76e72aebe550012b27b03d5bd08c9d15e4
juunini <juuni.ni.i@gmail.com>
2021-02-20 21:07:31 +0900

    Change ToyRepository Fake class

--- app/src/main/java/com/codesoom/assignment/repositories/ToyRepository.java
+++ app/src/main/java/com/codesoom/assignment/repositories/ToyRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -25,30 +24,31 @@ public interface ToyRepository extends CrudRepository<Toy, Long> {
     void deleteById(Long id);
 
     final class Fake implements ToyRepository {
-        private final CatToy givenCatToy1 = new CatToy(1L, "캣닢", "(주)고양이회사", 1000D, "https://cat.toy/cat-nip.png");
-        private final CatToy givenCatToy2 = new CatToy(2L, "캣타워", "(주)고양이회사", 2000D, "https://cat.toy/cat-tower.png");
+        private final List<Toy> toys = new ArrayList<>();
 
         @Override
         public List<Toy> findAll() {
-            return new ArrayList<>(Arrays.asList(givenCatToy1, givenCatToy2));
+            return toys;
         }
 
         @Override
         public Optional<Toy> findById(Long id) {
-            return switch (id.intValue()) {
-                case 1 -> Optional.of(givenCatToy1);
-                case 2 -> Optional.of(givenCatToy2);
-                default -> Optional.empty();
-            };
+            final Toy toy = toys.get(id.intValue());
+            return Optional.ofNullable(toy);
         }
 
         @Override
         public Toy save(Toy toy) {
-            return new CatToy(3L, toy.name(), toy.brand(), toy.price(), toy.imageURL());
+            final Long id = Integer.toUnsignedLong(toys.size());
+            final Toy newToy = new CatToy(id, toy.name(), toy.brand(), toy.price(), toy.imageURL());
+
+            toys.add(newToy);
+            return newToy;
         }
 
         @Override
         public void deleteById(Long id) {
+            toys.remove(id.intValue());
         }
 
         public Iterable<Toy> findAllById(Iterable<Long> longs) {
