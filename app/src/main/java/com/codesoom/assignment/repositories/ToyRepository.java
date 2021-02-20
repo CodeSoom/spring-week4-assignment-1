011fe339199e9ca82ff0ebb667bae7945b5e8dca
juunini <juuni.ni.i@gmail.com>
2021-02-20 19:33:23 +0900

    Add repository annotation and write override

--- app/src/main/java/com/codesoom/assignment/repositories/ToyRepository.java
+++ app/src/main/java/com/codesoom/assignment/repositories/ToyRepository.java
@@ -2,18 +2,22 @@ package com.codesoom.assignment.repositories;
 
 import com.codesoom.assignment.models.Toy;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface ToyRepository extends CrudRepository<Toy, Long> {
+    @Override
     List<Toy> findAll();
 
-    Optional<Toy> findById(long id);
+    @Override
+    Optional<Toy> findById(Long id);
 
+    @Override
     Toy save(Toy toy);
 
-    void modify(long id, Toy toy);
-
-    void deleteById(long id);
+    @Override
+    void deleteById(Long id);
 }
