5876788e881c614230befb898701e03f5e4c3a96
thkim <thkim2017@gmail.com>
2021-02-20 13:43:11 +0900

    Add a test for fields of Product There are Id, name, maker, and price

--- app/src/test/java/com/codesoom/assignment/domain/ProductTest.java
+++ app/src/test/java/com/codesoom/assignment/domain/ProductTest.java
@@ -7,8 +7,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 class ProductTest {
     @Test
     void creation() {
-        Product product = new Product("오뎅꼬치");
+        Product product = new Product(1L, "오뎅꼬치", "야옹아멍멍해봐", 3000);
 
+        assertThat(product.getId()).isEqualTo(1L);
         assertThat(product.getName()).isEqualTo("오뎅꼬치");
+        assertThat(product.getMaker()).isEqualTo("야옹아멍멍해봐");
+        assertThat(product.getPrice()).isEqualTo(3000);
     }
 }
