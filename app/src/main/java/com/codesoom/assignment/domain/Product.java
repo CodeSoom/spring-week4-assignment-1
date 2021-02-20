5435e214563843e4cf2647183335628411b7b39a
thkim <thkim2017@gmail.com>
2021-02-20 12:30:39 +0900

    Implement a getName() in Product class Plus Add a constructor with String name

--- app/src/main/java/com/codesoom/assignment/domain/Product.java
+++ app/src/main/java/com/codesoom/assignment/domain/Product.java
@@ -1,7 +1,13 @@
 package com.codesoom.assignment.domain;
 
 public class Product {
+    private final String name;
+
+    public Product(String name) {
+        this.name = name;
+    }
+
     public String getName() {
-        return "";
+        return name;
     }
 }
