bed0fbbc7c88cf532a9ab6284fb1fc63439666bf
thkim <thkim2017@gmail.com>
2021-02-20 14:38:39 +0900

    Add a test for a content of list There should be the title of a product "오뎅꼬치"

--- app/src/test/java/com/codesoom/assignment/controllers/ProductControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/ProductControllerTest.java
@@ -5,7 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
@@ -19,8 +21,10 @@ class ProductControllerTest {
 
     @Test
     void list() throws Exception {
+        //현재 상품이 등록되어있는 상태
         mockMvc.perform(get("/products"))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("오뎅꼬치")));
     }
 
 }
