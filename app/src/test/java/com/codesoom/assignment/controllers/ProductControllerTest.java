4eaa822dfbde48e699aec412b5660a2068c4d965
thkim <thkim2017@gmail.com>
2021-02-20 15:02:10 +0900

    Add a test for ProductService in ProductControllerTest With new product, productService.getProducts() will return a List of product

--- app/src/test/java/com/codesoom/assignment/controllers/ProductControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/ProductControllerTest.java
@@ -1,12 +1,17 @@
 package com.codesoom.assignment.controllers;
 
+import com.codesoom.assignment.domain.Product;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.List;
+
 import static org.hamcrest.Matchers.containsString;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -20,9 +25,15 @@ class ProductControllerTest {
     @Autowired
     private ProductController productController;
 
+    @MockBean
+    private ProductService productService;
+
     @Test
     void list() throws Exception {
-        //현재 상품이 등록되어있는 상태
+        Product product = new Product(1L, "오뎅꼬치", "야옹아멍멍해봐", 3000);
+
+        given(productService.getProducts()).willReturn(List.of(product));
+
         mockMvc.perform(
                 get("/products")
                 .accept(MediaType.APPLICATION_JSON_UTF8)
