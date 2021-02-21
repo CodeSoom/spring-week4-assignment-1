d5b2d7383fdd323860905b0cfe53d4f2b9fd540c
Lee Dong Wook <dwlee@crscube.io>
2021-02-21 20:50:52 +0900

    DOC: add javadoc in ProductController

--- app/src/main/java/com/codesoom/assignment/controllers/ProductController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/ProductController.java
@@ -28,27 +28,48 @@ public class ProductController {
         this.productService = productService;
     }
 
+    /**
+     * 서비스에 상품 생성을 요청합니다.
+     * @param product
+     * @return
+     */
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Product createProduct(@RequestBody Product product) {
         return productService.createProduct(product);
     }
 
+    /**
+     * 서비스에 상품 목록을 요청합니다.
+     * @return 상품 목록
+     */
     @GetMapping
     public List<Product> getProducts() {
         return productService.getProducts();
     }
 
+    /**
+     * 서비스에 특정한 상품을 요청합니다.
+     * @return 검색된 상품
+     */
     @GetMapping("{id}")
     public Product getProduct(@PathVariable Long id) {
         return productService.getProduct(id);
     }
 
+    /**
+     * 서비스에 특정한 상품 수정을 요청합니다.
+     * @return 수정된 상품
+     */
     @PutMapping("{id}")
     public Product updateProduct(@PathVariable Long id, @RequestBody Product product) {
         return productService.updateProduct(id, product);
     }
 
+    /**
+     * 서비스에 특정한 상품 삭제를 요청합니다.
+     * @return 삭제된 상품
+     */
     @DeleteMapping("{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public Product deleteProduct(@PathVariable Long id) {
