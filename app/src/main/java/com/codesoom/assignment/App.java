e71e9edd1f3fea175d18cbbe9a478036e8d8c9e9
juunini <juuni.ni.i@gmail.com>
2021-02-21 17:31:47 +0900

    Remove App test

--- app/src/main/java/com/codesoom/assignment/App.java
+++ app/src/main/java/com/codesoom/assignment/App.java
@@ -7,5 +7,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
-    }
+    }   
 }
