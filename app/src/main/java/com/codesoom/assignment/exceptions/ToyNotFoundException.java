98797f5b732948baabbbe5a2a1878b9ba7f31e63
juunini <juuni.ni.i@gmail.com>
2021-02-21 18:45:30 +0900

    Set ToyNotFoundException response not found

--- app/src/main/java/com/codesoom/assignment/exceptions/ToyNotFoundException.java
+++ app/src/main/java/com/codesoom/assignment/exceptions/ToyNotFoundException.java
@@ -1,5 +1,9 @@
 package com.codesoom.assignment.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ToyNotFoundException extends RuntimeException {
     public ToyNotFoundException(Long id) {
         super("Not found toy id: " + id);
