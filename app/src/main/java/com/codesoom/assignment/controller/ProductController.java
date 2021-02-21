520ee6afd0ca9a0563c199fe9be0e1ccd5f6e0c5
Las <haho013039@gmail.com>
2021-02-21 11:45:08 +0900

    implement create product

--- app/src/main/java/com/codesoom/assignment/controller/ProductController.java
+++ app/src/main/java/com/codesoom/assignment/controller/ProductController.java
@@ -2,10 +2,7 @@ package com.codesoom.assignment.controller;
 
 import com.codesoom.assignment.application.ProductApplicationService;
 import com.codesoom.assignment.domain.Product;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -28,8 +25,15 @@ public class ProductController {
             .collect(Collectors.toList());
     }
 
+    @GetMapping("/{id}")
     public ProductDTO getSpecificProduct(@PathVariable Long id) {
         Optional<Product> product = this.applicationService.getProduct(id);
         return ProductDTO.from(product.get());
+    }
+
+    @PostMapping
+    public ProductDTO createProduct(ProductDTO product) {
+        Product createdProduct = applicationService.createProduct(product.name, product.maker, product.price, product.imageURL);
+        return ProductDTO.from(createdProduct);
     }
 }
