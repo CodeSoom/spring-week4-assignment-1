dcd114ca40d5e44959cc11a5605d887a46c9e902
sedin <sedin@kakao.com>
2021-05-25 16:33:44 +0900

    Feat: Product 서비스 로직 구현
    - ProductService 로직 구현

--- app/src/main/java/com/codesoom/assignment/application/ProductService.java
+++ app/src/main/java/com/codesoom/assignment/application/ProductService.java
@@ -1,8 +1,51 @@
 package com.codesoom.assignment.application;
 
+import com.codesoom.assignment.ProductNotFoundException;
+import com.codesoom.assignment.domain.Product;
+import com.codesoom.assignment.domain.ProductRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ProductService {
 
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public List<Product> getProductList() {
+        return productRepository.findAll();
+    }
+
+    public Product getProduct(Long productId) {
+        return productRepository.findById(productId)
+                                .orElseThrow(() -> new ProductNotFoundException(productId));
+    }
+
+    public Product createProduct(Product newProduct) {
+        return productRepository.save(newProduct);
+    }
+
+    public Product updateProduct(Long productId, Product product) {
+        Product foundProduct = getProduct(productId);
+        Product updatedProduct = updateFoundProduct(foundProduct, product);
+        return productRepository.save(updatedProduct);
+    }
+
+    public void deleteProduct(Long productId) {
+        Product foundProduct = getProduct(productId);
+        productRepository.delete(foundProduct);
+    }
+
+    private Product updateFoundProduct(Product existedProduct, Product product) {
+        existedProduct.setName(product.getName());
+        existedProduct.setMaker(product.getMaker());
+        existedProduct.setPrice(product.getPrice());
+        existedProduct.setImageUrl(product.getImageUrl());
+
+        return existedProduct;
+    }
 }
--- app/src/main/java/com/codesoom/assignment/controllers/ProductController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/ProductController.java
@@ -1,6 +1,8 @@
 package com.codesoom.assignment.controllers;
 
+import com.codesoom.assignment.application.ProductService;
 import com.codesoom.assignment.domain.Product;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,35 +11,46 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @CrossOrigin
 @RestController
 @RequestMapping("/products")
 public class ProductController {
 
-    @GetMapping("")
-    public void getProductList() {
+    private final ProductService productService;
 
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("")
+    public List<Product> getProductList() {
+        return productService.getProductList();
     }
 
     @GetMapping("/{productId}")
-    public void getDetailProduct(@PathVariable Long productId) {
-
+    public Product getDetailProduct(@PathVariable Long productId) {
+        return productService.getProduct(productId);
     }
 
     @PostMapping("")
-    public void createProduct(@RequestBody Product product) {
-
+    @ResponseStatus(HttpStatus.CREATED)
+    public Product createProduct(@RequestBody Product newProduct) {
+        return productService.createProduct(newProduct);
     }
 
     @PatchMapping("/{productId}")
-    public void updateProduct(@PathVariable Long productId) {
-
+    public Product updateProduct(@PathVariable Long productId, @RequestBody Product product) {
+        return productService.updateProduct(productId, product);
     }
 
     @DeleteMapping("/{productId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteProduct(@PathVariable Long productId) {
-
+        productService.deleteProduct(productId);
     }
 }
