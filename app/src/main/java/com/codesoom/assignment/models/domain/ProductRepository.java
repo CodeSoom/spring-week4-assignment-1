74747e28a032f003334215cd4093f849cbf659e0
DevRunner21 <devrunner21@gmail.com>
2021-05-25 21:27:15 +0900

    ProductRepository를 작성하라
    - findAll : 모든 상품목록 조회
    - findProductById : 상품 단건 조회
    
    Signed-off-by: DevRunner21 <devrunner21@gmail.com>

--- app/src/main/java/com/codesoom/assignment/models/domain/ProductRepository.java
+++ app/src/main/java/com/codesoom/assignment/models/domain/ProductRepository.java
@@ -1,4 +1,25 @@
 package com.codesoom.assignment.models.domain;
 
-public class ProductRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    /**
+     * 모든 상품을 조회합니다.
+     * @return 조회된 상품 목록을 반환합니다.
+     */
+    List<Product> findAll();
+
+    /**
+     * 주어진 ID에 해당하는 상품을 조회합니다.
+     * @param id 조회할 상품 ID
+     * @return 조회된 상품을 반합니다.
+     */
+    Optional<Product> findProductById(Long id);
+
 }
