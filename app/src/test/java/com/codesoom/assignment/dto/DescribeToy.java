b6f99152e36feb766d5098adb54d3e18c1a34313
Joowan Kim <rlawndhks217@gmail.com>
2021-05-26 08:31:13 +0900

    Add Toy domain test

---
+++ app/src/test/java/com/codesoom/assignment/dto/DescribeToy.java
@@ -0,0 +1,34 @@
+package com.codesoom.assignment.dto;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Nested
+@DisplayName("Toy 클래스는")
+class DescribeToy {
+    // TODO
+    // 1. getter test
+    // 2. setter test
+
+    @Nested
+    @DisplayName("getter가 호출되었을 경우")
+    class ContextWithGetter {
+
+        @BeforeEach
+        void toyFixtureSetUp() {
+
+        }
+
+        @Test
+        @DisplayName("속성을 반환한다")
+        void ItReturnsAttributes() {
+
+        }
+    }
+
+
+}
