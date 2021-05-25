5d03b0cc37fec005ed8adf056d96acb5ae9b0272
humblEgo <injun.woo30000@gmail.com>
2021-05-25 21:10:35 +0900

    Product 엔티티를 생성하라

---
+++ app/src/main/java/com/codesoom/assignment/domain/Product.java
@@ -0,0 +1,68 @@
+package com.codesoom.assignment.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String maker;
+
+    @Column
+    private Long price;
+
+    @Column
+    private String image;
+
+    public Product() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMaker() {
+        return maker;
+    }
+
+    public void setMaker(String maker) {
+        this.maker = maker;
+    }
+
+    public Long getPrice() {
+        return price;
+    }
+
+    public void setPrice(Long price) {
+        this.price = price;
+    }
+
+    public String getImagePath() {
+        return image;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.image = imagePath;
+    }
+}
