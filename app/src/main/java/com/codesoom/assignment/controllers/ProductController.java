f5c78cf68e7c5513572ba999b85343226e1df1a5
DevRunner21 <devrunner21@gmail.com>
2021-05-25 21:30:45 +0900

    ProductController를 작성하라(미완)
    - API EndPoint만 정의
    
    Signed-off-by: DevRunner21 <devrunner21@gmail.com>

--- app/src/main/java/com/codesoom/assignment/controllers/ProductController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/ProductController.java
@@ -1,9 +1,42 @@
 package com.codesoom.assignment.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.codesoom.assignment.models.dto.ProductDto;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/products")
 public class ProductController {
 
+    @GetMapping
+    public List<ProductDto> list() {
+
+        return null;
+    }
+
+    @GetMapping("{id}")
+    public ProductDto detail(@PathVariable("id") Long productId) {
+
+        return null;
+    }
+
+    @PostMapping
+    public ProductDto save(@RequestBody ProductDto productDto) {
+
+        return null;
+    }
+
+    @RequestMapping(method={RequestMethod.PUT, RequestMethod.PATCH})
+    public ProductDto update(@PathVariable("id") Long productId, @RequestBody ProductDto productDto) {
+
+        return null;
+    }
+
+    @DeleteMapping("{id}")
+    public ProductDto delete(@PathVariable("id") Long productId) {
+
+        return null;
+    }
 
 }
