7ec1a35e71efce7b54deb380d27ce47aef2aeb14
Joowan Kim <rlawndhks217@gmail.com>
2021-05-26 08:30:56 +0900

    Add Toy domain

---
+++ app/src/main/java/com/codesoom/assignment/domain/Toy.java
@@ -0,0 +1,63 @@
+package com.codesoom.assignment.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.Min;
+
+@Entity
+public class Toy {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "brand")
+    private String brand;
+
+    @Column(name = "price")
+    @Min(0)
+    private Double price;
+
+    @Column(name = "img_url")
+    private String imgUrl;
+
+    public static class Builder {
+        private final String name;
+        private final String brand;
+
+        private Double price = 0.0;
+        private String imgUrl = "";
+
+        public Builder(String name, String brand) {
+            this.name = name;
+            this.brand = brand;
+        }
+
+        public Builder price(Double price) {
+            this.price = price;
+            return this;
+        }
+
+        public Builder imgUrl(String imgUrl) {
+            this.imgUrl = imgUrl;
+            return this;
+        }
+
+        public Toy build() {
+            return new Toy(this);
+        }
+    }
+
+    public Toy() {}
+
+    private Toy(Builder builder) {
+        this.name = builder.name;
+        this.brand = builder.brand;
+        this.price = builder.price;
+        this.imgUrl = builder.imgUrl;
+    }
+}
