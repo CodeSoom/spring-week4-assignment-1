49ad5f18490063ee0953a7b6ad75cc99f529628a
Joowan Kim <rlawndhks217@gmail.com>
2021-05-26 08:31:43 +0900

    Add skeletons

---
+++ app/src/main/java/com/codesoom/assignment/controller/ToyController.java
@@ -0,0 +1,7 @@
+package com.codesoom.assignment.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ToyController {
+}
---
+++ app/src/main/java/com/codesoom/assignment/service/ToyService.java
@@ -0,0 +1,7 @@
+package com.codesoom.assignment.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ToyService {
+}
