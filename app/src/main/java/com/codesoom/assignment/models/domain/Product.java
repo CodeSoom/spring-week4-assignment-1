36c4cedadc3bd2884c43de2a67e04037130891d6
DevRunner21 <devrunner21@gmail.com>
2021-05-26 21:23:23 +0900

    ProductService를 구현하라
    - (update) Product Entity에수정기능  구현
    - (getProductList) 상품 목록 조회 기능 구현
    - (getProductOne) 상품 단건 조회 기능 구현
    - (saveProduct) 상품 등록 기능  구현
    - (updateProduct) 상품 수정 기능 구현
    - (deleteProduct) 상품 삭제 기능 구현
    
    Signed-off-by: DevRunner21 <devrunner21@gmail.com>

--- app/src/main/java/com/codesoom/assignment/models/domain/Product.java
+++ app/src/main/java/com/codesoom/assignment/models/domain/Product.java
@@ -1,10 +1,15 @@
 package com.codesoom.assignment.models.domain;
 
+import com.codesoom.assignment.models.dto.ProductInfoDto;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+/**
+ * 상품
+ */
 @Entity
 public class Product {
 
@@ -23,6 +28,13 @@ public class Product {
     private String imgUrl;
 
     public Product() {}
+
+    public Product(String name, String maker, int price, String imgUrl) {
+        this.name = name;
+        this.maker = maker;
+        this.price = price;
+        this.imgUrl = imgUrl;
+    }
 
     public Product(Long id, String name, String maker, int price, String imgUrl) {
         this.id = id;
@@ -52,4 +64,18 @@ public class Product {
         return imgUrl;
     }
 
+    /**
+     * 자신(상품)의 정보를 수정합니다.
+     * @param updateParam 수정할 정보
+     * @return 수정된 상품을 반환합니다.
+     */
+    public Product update(ProductInfoDto updateParam) {
+
+        this.name = updateParam.getName();
+        this.maker = updateParam.getMaker();
+        this.price = updateParam.getPrice();
+        this.imgUrl = updateParam.getImageUrl();
+
+        return this;
+    }
 }
