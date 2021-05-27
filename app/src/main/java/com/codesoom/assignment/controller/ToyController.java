21375df92d5c0795b0badb57bb784b63a4cda554
Joowan Kim <rlawndhks217@gmail.com>
2021-05-28 00:54:50 +0900

    Add get list feature

--- app/src/main/java/com/codesoom/assignment/controller/ToyController.java
+++ app/src/main/java/com/codesoom/assignment/controller/ToyController.java
@@ -1,7 +1,28 @@
 package com.codesoom.assignment.controller;
 
+import com.codesoom.assignment.domain.Toy;
+import com.codesoom.assignment.service.ToyService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/products")
 public class ToyController {
+
+    private ToyService toyService;
+
+    public ToyController(ToyService toyService) {
+        this.toyService = toyService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Toy>> getList() {
+        List<Toy> toys = this.toyService.list();
+        return new ResponseEntity<>(toys, HttpStatus.OK);
+    }
 }
--- app/src/main/java/com/codesoom/assignment/service/ToyService.java
+++ app/src/main/java/com/codesoom/assignment/service/ToyService.java
@@ -1,7 +1,21 @@
 package com.codesoom.assignment.service;
 
+import com.codesoom.assignment.domain.Toy;
+import com.codesoom.assignment.repository.ToyRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ToyService {
+
+    private ToyRepository toyRepository;
+
+    public ToyService(ToyRepository toyRepository) {
+        this.toyRepository = toyRepository;
+    }
+
+    public List<Toy> list() {
+        return (List<Toy>) toyRepository.findAll();
+    }
 }
