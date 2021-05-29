90835b744fb3431c0e48e713abccf86af1a9d013
Joowan Kim <rlawndhks217@gmail.com>
2021-05-29 15:59:29 +0900

    Add JavaDoc

--- app/src/main/java/com/codesoom/assignment/controller/ToyController.java
+++ app/src/main/java/com/codesoom/assignment/controller/ToyController.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
 
+/**
+ * 장난감에 대한 HTTP request를 처리한다.
+ */
 @RestController
 @RequestMapping("/products")
 public class ToyController {
@@ -20,12 +23,23 @@ public class ToyController {
         this.toyService = toyService;
     }
 
+    /**
+     * 장난감 목록을 HttpResponse로 반환한다.
+     *
+     * @return 장난감 목록을 포함한 HttpResponse
+     */
     @GetMapping
     public ResponseEntity<List<Toy>> getList() {
         List<Toy> toys = this.toyService.list();
         return new ResponseEntity<>(toys, HttpStatus.OK);
     }
 
+    /**
+     * 장난감 정보를 생성한다.
+     *
+     * @param toySaveRequest 생성될 장난감의 정보
+     * @return 생성된 장난감의 정보를 포함한 HttpResponse
+     */
     @PostMapping
     public ResponseEntity<Toy> createToy(
             @PathVariable @Valid ToySaveRequest toySaveRequest
--- app/src/main/java/com/codesoom/assignment/service/ToyService.java
+++ app/src/main/java/com/codesoom/assignment/service/ToyService.java
@@ -7,6 +7,9 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+/**
+ * 장난감 정보에 대한 작업을 처리한다.
+ */
 @Service
 public class ToyService {
 
@@ -16,10 +19,21 @@ public class ToyService {
         this.toyRepository = toyRepository;
     }
 
+    /**
+     * 장난감 목록을 반환한다.
+     *
+     * @return 장난감 목록
+     */
     public List<Toy> list() {
         return (List<Toy>) toyRepository.findAll();
     }
 
+    /**
+     * 장난감 정보를 생성한다.
+     *
+     * @param toy 장난감 정보 객체
+     * @return 장난감 정보
+     */
     public Toy create(Toy toy) {
         return this.toyRepository.save(toy);
     }
