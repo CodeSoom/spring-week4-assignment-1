aa3a8bce69608e72c6f68217e285ffa33ee17e39
Changsu Im <imcxsu@gmail.com>
2021-05-30 02:31:58 +0900

    Remove unnecessary scope from repository test

--- app/src/test/java/com/codesoom/assignment/product/domain/ProductRepositoryTest.java
+++ app/src/test/java/com/codesoom/assignment/product/domain/ProductRepositoryTest.java
@@ -1,5 +1,6 @@
 package com.codesoom.assignment.product.domain;
 
+import com.codesoom.assignment.product.ProductFixtures;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -8,8 +9,7 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("ProductRepository 인터페이스의")
 @DataJpaTest
@@ -23,26 +23,23 @@ class ProductRepositoryTest {
     @Nested
     @DisplayName("save 메서드는")
     class Describe_save {
+        private final Product product = ProductFixtures.laser();
 
-        @Nested
-        @DisplayName("만약 1개의 상품이 주어진다면")
-        class Context_with_one_product {
-            // given
-            private final String name = "cat1";
-            private final String maker = "codesoom";
-            private final Long price = 33_000L;
-            private final Product product = new Product(name, maker, price);
+        @Test
+        @DisplayName("새로운 상품을 생성한 후 생성된 상품을 반환한다")
+        void It_saves_the_product_and_returns_it() {
+            Product createdProduct = productRepository.save(product);
 
-            @Test
-            @DisplayName("새로운 상품을 생성한 후 생성된 상품을 반환한다")
-            void It_saves_the_product_and_returns_it() {
-                Product createdProduct = productRepository.save(product);
-
-                assertEquals(name, createdProduct.getName());
-                assertEquals(maker, createdProduct.getMaker());
-                assertEquals(price, createdProduct.getPrice());
-                assertNull(createdProduct.getImageUrl());
-            }
+            assertThat(product.getId())
+                    .isEqualTo(createdProduct.getId());
+            assertThat(product.getName())
+                    .isEqualTo(createdProduct.getName());
+            assertThat(product.getMaker())
+                    .isEqualTo(createdProduct.getMaker());
+            assertThat(product.getPrice())
+                    .isEqualTo(createdProduct.getPrice());
+            assertThat(product.getImageUrl())
+                    .isEqualTo(createdProduct.getImageUrl());
         }
     }
 }
