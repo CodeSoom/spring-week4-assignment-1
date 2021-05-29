9bb2fd2a8e06499b8ca0a199913c151c7671d524
Changsu Im <imcxsu@gmail.com>
2021-05-29 16:07:15 +0900

    Add test to service for update, delete methods

--- app/src/test/java/com/codesoom/assignment/product/service/ProductServiceMockTest.java
+++ app/src/test/java/com/codesoom/assignment/product/service/ProductServiceMockTest.java
@@ -21,6 +21,8 @@ import java.util.Optional;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.verify;
 
 @DisplayName("ProductService 클래스의")
 @ExtendWith(MockitoExtension.class)
@@ -33,6 +35,17 @@ public class ProductServiceMockTest {
     @Mock
     ProductRepository productRepository;
 
+    private List<Product> products;
+    private Product productLaser;
+    private Product productHelm;
+
+    @BeforeEach
+    void prepareProduct() {
+        products = new ArrayList<>();
+        productLaser = ProductFixtures.laser();
+        productHelm = ProductFixtures.helm();
+    }
+
     @Nested
     @DisplayName("create 메서드는")
     class Describe_create {
@@ -40,26 +53,25 @@ public class ProductServiceMockTest {
         @Nested
         @DisplayName("만약 상품이 주어진다면")
         class Context_with_one_product {
-            private final Product product = ProductFixtures.laser();
 
             @BeforeEach
             void mocking() {
-                given(productRepository.save(product))
-                        .willReturn(product);
+                given(productRepository.save(productLaser))
+                        .willReturn(productLaser);
             }
 
             @Test
             @DisplayName("새로운 상품을 생성한 후 생성된 상품을 반환한다")
             void It_creates_the_product_and_returns_it() {
                 // when
-                Product createdProduct = productService.create(product);
+                Product createdProduct = productService.create(productLaser);
 
                 // then
                 // FIXME: 생성됐다는 것을 검증
-                assertThat(product.getName()).isEqualTo(createdProduct.getName());
-                assertThat(product.getMaker()).isEqualTo(createdProduct.getMaker());
-                assertThat(product.getPrice()).isEqualTo(createdProduct.getPrice());
-                assertThat(product.getImageUrl()).isEqualTo(createdProduct.getImageUrl());
+                assertThat(createdProduct.getName()).isEqualTo(productLaser.getName());
+                assertThat(createdProduct.getMaker()).isEqualTo(productLaser.getMaker());
+                assertThat(createdProduct.getPrice()).isEqualTo(productLaser.getPrice());
+                assertThat(createdProduct.getImageUrl()).isEqualTo(productLaser.getImageUrl());
             }
         }
     }
@@ -71,25 +83,24 @@ public class ProductServiceMockTest {
         @Nested
         @DisplayName("만약 등록되어 있는 상품의 유효한 식별자가 주어진다면")
         class Context_with_valid_id {
-            private final Product product = ProductFixtures.laser();
 
             @BeforeEach
             void mocking() {
-                given(productRepository.findById(product.getId()))
-                        .willReturn(Optional.ofNullable(product));
+                given(productRepository.findById(productLaser.getId()))
+                        .willReturn(Optional.ofNullable(productLaser));
             }
 
             @Test
             @DisplayName("해당하는 상품을 반환한다")
             void It_returns_the_product() {
                 // when
-                Product testProduct = productService.get(product.getId());
+                Product testProduct = productService.get(productLaser.getId());
 
                 // then
-                assertThat(product.getName()).isEqualTo(testProduct.getName());
-                assertThat(product.getMaker()).isEqualTo(testProduct.getMaker());
-                assertThat(product.getPrice()).isEqualTo(testProduct.getPrice());
-                assertThat(product.getImageUrl()).isEqualTo(testProduct.getImageUrl());
+                assertThat(testProduct.getName()).isEqualTo(productLaser.getName());
+                assertThat(testProduct.getMaker()).isEqualTo(productLaser.getMaker());
+                assertThat(testProduct.getPrice()).isEqualTo(productLaser.getPrice());
+                assertThat(testProduct.getImageUrl()).isEqualTo(productLaser.getImageUrl());
             }
         }
 
@@ -114,14 +125,12 @@ public class ProductServiceMockTest {
         @Nested
         @DisplayName("만약 등록되어 있는 상품이 2개 있다면")
         class Context_with_one_product {
-            private final List<Product> products = new ArrayList<>();
-            private final Product product = ProductFixtures.laser();
             private final int totalProductCount = 2;
 
             @BeforeEach
             void mocking() {
                 for (int index = 1; index <= totalProductCount; index++) {
-                    products.add(product);
+                    products.add(productLaser);
                 }
 
                 given(productRepository.findAll())
@@ -138,13 +147,13 @@ public class ProductServiceMockTest {
                 assertThat(testProductList.size())
                         .isEqualTo(totalProductCount);
                 assertThat(testProductList.get(0)
-                                          .getName()).contains(product.getName());
+                                          .getName()).contains(productLaser.getName());
                 assertThat(testProductList.get(0)
-                                          .getMaker()).isEqualTo(product.getMaker());
+                                          .getMaker()).isEqualTo(productLaser.getMaker());
                 assertThat(testProductList.get(0)
-                                          .getPrice()).isEqualTo(product.getPrice());
+                                          .getPrice()).isEqualTo(productLaser.getPrice());
                 assertThat(testProductList.get(0)
-                                          .getImageUrl()).isEqualTo(product.getImageUrl());
+                                          .getImageUrl()).isEqualTo(productLaser.getImageUrl());
             }
         }
 
@@ -171,4 +180,65 @@ public class ProductServiceMockTest {
         }
     }
 
+    @Nested
+    @DisplayName("update 메서드는")
+    class Describe_update {
+
+        @Nested
+        @DisplayName("만약 등록되어 있는 상품의 식별자와 변경시키려는 상품 데이터가 주어진다면")
+        class Context_with_valid_product_id_and_new_product_data {
+            private final Long validProductId = 1L;
+
+            @BeforeEach
+            void mocking() {
+                given(productRepository.findById(validProductId))
+                        .willReturn(Optional.ofNullable(productHelm));
+            }
+
+            @Test
+            @DisplayName("새로운 상품을 생성한 후 생성된 상품을 반환한다")
+            void It_returns_updated_product() {
+                // when
+                Product updatedProduct =
+                        productService.update(validProductId, productHelm);
+
+                // then
+                assertThat(updatedProduct.getName())
+                        .isEqualTo(productHelm.getName());
+                assertThat(updatedProduct.getMaker())
+                        .isEqualTo(productHelm.getMaker());
+                assertThat(updatedProduct.getPrice())
+                        .isEqualTo(productHelm.getPrice());
+                assertThat(updatedProduct.getImageUrl())
+                        .isEqualTo(productHelm.getImageUrl());
+
+                verify(productRepository).findById(validProductId);
+            }
+        }
+    }
+
+    @Nested
+    @DisplayName("delete 메서드는")
+    class Describe_delete {
+
+        @Nested
+        @DisplayName("만약 등록되어 있지 않은 상품의 식별자가 주어진다면")
+        class Context_with_invalid_product_id {
+            private final Long invalidProductId = -1L;
+
+            @BeforeEach
+            void mocking() {
+                given(productRepository.existsById(invalidProductId))
+                        .willThrow(new ProductNotFoundException());
+            }
+
+            @Test
+            @DisplayName("상품을 찾을 수 없다는 예외를 던진다")
+            void It_throws_product_not_found() {
+                assertThatThrownBy(() -> productService.delete(invalidProductId))
+                        .isInstanceOf(ProductNotFoundException.class);
+
+            }
+        }
+    }
 }
