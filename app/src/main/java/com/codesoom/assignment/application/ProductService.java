4c17746d8124b768d4ae28127e2218cb9c5164eb
humblEgo <injun.woo30000@gmail.com>
2021-05-30 20:16:12 +0900

    ProductService에 javadoc을 추가하라

--- app/src/main/java/com/codesoom/assignment/application/ProductService.java
+++ app/src/main/java/com/codesoom/assignment/application/ProductService.java
@@ -13,26 +13,58 @@ public class ProductService implements ProductManagable {
 
     private ProductRepository productRepository;
 
+    /**
+     * 주어진 Repository 객체로 초기화하며 ProductService 객체를 생성합니다.
+     *
+     * @param productRepository 주어진 저장소 객체
+     */
     public ProductService(ProductRepository productRepository) {
         this.productRepository = productRepository;
     }
 
+    /**
+     * 모든 상품의 목록을 반환합니다.
+     *
+     * @return 상품 목록입니다.
+     */
     @Override
     public List<Product> getAllProducts() {
         return productRepository.findAll();
     }
 
+    /**
+     * 지정된 id에 해당하는 상품을 반환합니다.
+     *
+     * @param id 상품의 id
+     * @return id에 해당하는 상품
+     * @exception EmptyResultDataAccessException
+     *      id에 해당하는 상품이 없음
+     */
     @Override
     public Product getProduct(Long id) {
         return findById(id);
-
     }
 
+    /**
+     * 주어진 상품을 저장합니다.
+     *
+     * @param product 주어진 상품
+     * @return 저장된 상품
+     */
     @Override
     public Product addProduct(Product product) {
         return productRepository.save(product);
     }
 
+    /**
+     * id에 해당하는 상품을 갱신합니다.
+     *
+     * @param id 상품의 id
+     * @param sourceProduct 갱신할 내용
+     * @return 갱신된 상품
+     * @exception EmptyResultDataAccessException
+     *      id에 해당하는 상품이 없음
+     */
     @Override
     @Transactional
     public Product updateProduct(Long id, Product sourceProduct) {
@@ -41,6 +73,13 @@ public class ProductService implements ProductManagable {
         return productRepository.save(product);
     }
 
+    /**
+     * id에 해당하는 상품을 삭제합니다.
+     *
+     * @param id 주어진 id
+     * @exception EmptyResultDataAccessException
+     *      id에 해당하는 상품이 없음
+     */
     @Override
     @Transactional
     public void deleteProduct(Long id) {
