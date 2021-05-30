ae1c927f78aa86b8eafbecf81b20e5636060a6e6
humblEgo <injun.woo30000@gmail.com>
2021-05-30 20:31:43 +0900

    ProductErrorAdvice에 javadoc을 작성하라

--- app/src/main/java/com/codesoom/assignment/controllers/ProductErrorAdvice.java
+++ app/src/main/java/com/codesoom/assignment/controllers/ProductErrorAdvice.java
@@ -9,6 +9,12 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class ProductErrorAdvice {
 
+    /**
+     * 404 상태코드와 함께 예외 내용을 응답합니다.
+     *
+     * @param e 예외 내용이 담긴 예외객체
+     * @return 예외 내용
+     */
     @ExceptionHandler(EmptyResultDataAccessException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public String handleEmptyResultDataAccessException(EmptyResultDataAccessException e) {
