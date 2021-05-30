1a6cfa54715a376bcdc1841ab81c61293324d0e4
Lim <40200760+tbnsok40@users.noreply.github.com>
2021-05-30 11:47:22 +0900

    TaskService javadoc 생성

--- app/src/main/java/com/codesoom/assignment/Task/application/TaskService.java
+++ app/src/main/java/com/codesoom/assignment/Task/application/TaskService.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 
 /**
- * Task Entity 를 다루는 메소드 작성
+ * TaskController 의 요청을 수행합니다.
  */
 @Service
 @Transactional
@@ -18,19 +18,38 @@ public class TaskService {
 
     private final TaskRepository taskRepository;
 
+    /**
+     * TaskService 생성자에 TaskRepository 를 주입합니다.
+     * @param taskRepository 생성자에 주입할 TaskRepository DAO
+     */
     public TaskService(TaskRepository taskRepository) {
         this.taskRepository = taskRepository;
     }
 
+
+    /**
+     * 모든 Task 를 리턴합니다.
+     * @return 모든 Task 를 담고 있는 list
+     */
     public List<Task> getTasks() {
         return taskRepository.findAll();
     }
 
+    /**
+     * 요청한 id의 Task 를 리턴합니다.
+     * @param id 요청 id
+     * @return 요청한 id의 Task 객체
+     */
     public Task getTask(Long id) {
         return taskRepository.findById(id)
                 .orElseThrow(() -> new TaskNotFoundException(id));
     }
 
+    /**
+     * 새로운 Task 를 생성합니다.
+     * @param source Task 객체
+     * @return 생성된 Task
+     */
     public Task createTask(Task source) {
         Task task = new Task();
         task.setTitle(source.getTitle());
@@ -38,6 +57,12 @@ public class TaskService {
         return taskRepository.save(task);
     }
 
+    /**
+     * 기존 Task 를 업데이트 합니다.
+     * @param id 요청 id
+     * @param source Task 객체
+     * @return 업데이트한 Task
+     */
     public Task updateTask(Long id, Task source) {
         Task task = taskRepository.findById(id)
                 .orElseThrow(() -> new TaskNotFoundException(id));;
@@ -46,6 +71,11 @@ public class TaskService {
         return task;
     }
 
+    /**
+     * 저장된 Task를 삭제합니다.
+     * @param id 요청 id
+     * @return 삭제한 Toy
+     */
     public Task deleteTask(Long id) {
         Task task = taskRepository.findById(id)
                 .orElseThrow(() -> new TaskNotFoundException(id));
--- app/src/main/java/com/codesoom/assignment/Task/domain/TaskRepository.java
+++ app/src/main/java/com/codesoom/assignment/Task/domain/TaskRepository.java
@@ -3,12 +3,7 @@ package com.codesoom.assignment.Task.domain;
 import java.util.List;
 import java.util.Optional;
 
-//@Component
-//@Repository
 public interface TaskRepository{
-
-//    private List<Task> tasks = new ArrayList<>();
-//    private Long newId = 0L;
 
     Optional<Task> findById(Long newId);
 
