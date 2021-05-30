383371ffdf57a64d4a5a49da58a9025197a164b0
Joowan Kim <rlawndhks217@gmail.com>
2021-05-30 19:19:19 +0900

    Add getters to ToyResponse

--- app/src/main/java/com/codesoom/assignment/dto/ToyResponse.java
+++ app/src/main/java/com/codesoom/assignment/dto/ToyResponse.java
@@ -3,12 +3,38 @@ package com.codesoom.assignment.dto;
 import com.codesoom.assignment.domain.Toy;
 
 public class ToyResponse {
-    
+
+    private final Long id;
+    private final String name;
+    private final String maker;
+    private final Double price;
+    private final String imageUrl;
+
     public ToyResponse(Toy toy) {
-        Long id = toy.getId();
-        String name = toy.getName();
-        String maker = toy.getMaker();
-        Double price = toy.getPrice();
-        String imageUrl = toy.getImageUrl();
+        this.id = toy.getId();
+        this.name = toy.getName();
+        this.maker = toy.getMaker();
+        this.price = toy.getPrice();
+        this.imageUrl = toy.getImageUrl();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getMaker() {
+        return maker;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
     }
 }
