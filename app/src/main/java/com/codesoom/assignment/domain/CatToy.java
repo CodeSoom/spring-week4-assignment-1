3dde64e3da61f9338d8e9034dc0a9b24877626c7
wjy5446 <wjy5446@gmail.com>
2021-08-30 22:37:35 +0900

    feat) CatToy 클래스 추가

--- app/src/main/java/com/codesoom/assignment/domain/CatToy.java
+++ app/src/main/java/com/codesoom/assignment/domain/CatToy.java
@@ -1,2 +1,52 @@
-package com.codesoom.assignment.domain;public class CatToy {
+package com.codesoom.assignment.domain;
+
+public class CatToy {
+
+    private Long id;
+    private String name;
+    private String maker;
+    private Long price;
+    private String imageURI;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMaker() {
+        return maker;
+    }
+
+    public void setMaker(String maker) {
+        this.maker = maker;
+    }
+
+    public Long getPrice() {
+        return price;
+    }
+
+    public void setPrice(Long price) {
+        this.price = price;
+    }
+
+    public String getImageURI() {
+        return imageURI;
+    }
+
+    public void setImageURI(String imageURI) {
+        this.imageURI = imageURI;
+    }
+
 }
