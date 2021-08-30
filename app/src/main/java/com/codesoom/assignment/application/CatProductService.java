dba968279aa6125c8d6c3baf8da0005c368cc676
Sungeheon <elon4856@gmail.com>
2021-08-30 20:47:33 +0900

    feat: CatProduct 서비스에 대한 기본적인 세팅을 하세요

---
+++ app/src/main/java/com/codesoom/assignment/application/CatProductService.java
@@ -0,0 +1,5 @@
+package com.codesoom.assignment.application;
+
+public class CatProductService {
+
+}
---
+++ app/src/test/java/com/codesoom/assignment/controllers/CatProductControllerTest.java
@@ -0,0 +1,7 @@
+package com.codesoom.assignment.controllers;
+
+
+class CatProductControllerTest {
+
+
+}
