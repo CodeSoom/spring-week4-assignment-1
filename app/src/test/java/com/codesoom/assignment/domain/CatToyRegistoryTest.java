b9501d8c9a3071de5d3a618110f532e871430fdc
wjy5446 <wjy5446@gmail.com>
2021-08-30 23:57:04 +0900

    test) catToyRegistory의 get 테스트 추가

--- app/src/test/java/com/codesoom/assignment/domain/CatToyRegistoryTest.java
+++ app/src/test/java/com/codesoom/assignment/domain/CatToyRegistoryTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @DisplayName("CatToyRegistoryTest 클래스")
 class CatToyRegistoryTest {
@@ -76,6 +77,41 @@ class CatToyRegistoryTest {
                 assertThat(catToys.get(0)).isEqualTo(catToy1);
                 assertThat(catToys.get(1)).isEqualTo(catToy2);
                 assertThat(catToys.get(2)).isEqualTo(catToy3);
+            }
+        }
+    }
+
+    @Nested
+    @DisplayName("getOne 메소드")
+    class Describe_getOne {
+
+        @BeforeEach
+        void prepare() {
+            catToyRegistory.create(catToy1);
+        }
+
+        @Nested
+        @DisplayName("해당되는 id의 catToy가 있다면")
+        class Context_with_valid_id {
+
+            @Test
+            @DisplayName("해당 Id의 catToy를 반환합니다.")
+            void it_return_catToy() {
+                CatToy findedCatToy = catToyRegistory.get(1L);
+
+                assertThat(findedCatToy).isEqualTo(catToy1);
+            }
+        }
+
+        @Nested
+        @DisplayName("해당되는 id의 catToy가 없다면")
+        class Context_with_invalid_id {
+
+            @Test
+            @DisplayName("CatToyNotFoundException을 던집니다.")
+            void it_throw_CatToyNotFoundException() {
+                assertThatThrownBy(() -> catToyRegistory.get(1000L))
+                        .isInstanceOf(CatToyNotFoundException.class);
             }
         }
     }
