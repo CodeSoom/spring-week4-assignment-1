ffe12c5d5d8fcbb40ace75dd50376200b8ec5c90
wjy5446 <wjy5446@gmail.com>
2021-08-30 23:33:55 +0900

    feat) CatToyRegistory의 create, getAll 메소드 추가

--- app/src/main/java/com/codesoom/assignment/domain/CatToyRegistory.java
+++ app/src/main/java/com/codesoom/assignment/domain/CatToyRegistory.java
@@ -1,2 +1,27 @@
-package com.codesoom.assignment.domain;public class CatToyRegistory {
+package com.codesoom.assignment.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CatToyRegistory {
+    private List<CatToy> catToys;
+    private Long nextId;
+
+    public CatToyRegistory() {
+        this.catToys = new ArrayList<CatToy>();
+        this.nextId = 1L;
+    }
+
+    public void create(CatToy catToy) {
+        catToy.setId(generateId());
+        this.catToys.add(catToy);
+    }
+
+    public List<CatToy> getAll() {
+        return this.catToys;
+    }
+
+    private Long generateId() {
+        return nextId + 1L;
+    }
 }
