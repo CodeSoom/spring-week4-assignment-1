66bd1750a733848ab1d65ab6c9c8a865db57426c
injun <ison@sfu.ca>
2021-08-30 23:21:07 +0900

    feat: CatToy Dto 생성

---
+++ app/src/main/java/com/codesoom/assignment/domain/CatToy.java
@@ -0,0 +1,13 @@
+package com.codesoom.assignment.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CatToy {
+    private String name;
+    private String maker;
+    private int price;
+    private String image;
+}
