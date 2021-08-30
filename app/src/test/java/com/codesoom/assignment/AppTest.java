95f85c65a7ff5596de1df3431ed91edc0f73a4e6
daeungkim <daeungkim93@gmail.com>
2021-08-30 17:38:49 +0900

    AppTest @DisplayName 추가
    AppTest @DisplayName 추가
    -> 테스트의 대상인 메서드의 의도를 더 명확히 보여주기 위하여 변경

--- app/src/test/java/com/codesoom/assignment/AppTest.java
+++ app/src/test/java/com/codesoom/assignment/AppTest.java
@@ -1,15 +1,29 @@
 package com.codesoom.assignment;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+
+@DisplayName("App 클래스")
+@ExtendWith(MockitoExtension.class)
 class AppTest {
-    @Test
-    void appHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
-        assertEquals(classUnderTest.getGreeting(), "Hello, world!");
+    @Nested
+    @DisplayName("getGreeting 메서드는")
+    class Describe_getGreeting {
+
+        @InjectMocks
+        App app;
+
+        @Test
+        @DisplayName("App개체가 재대로 작동하고 있음을 알려준다.")
+        void it_inform_object_work_fine() {
+            assertThat(app.getGreeting()).isNotNull();
+        }
     }
 }
