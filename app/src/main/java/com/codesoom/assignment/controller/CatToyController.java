46007820d18b9b1d0d921812beac0200884d9971
catsbi <killfine1988@gmail.com>
2021-08-31 14:50:37 +0900

    refactor: CatToyController 주석 작성

--- app/src/main/java/com/codesoom/assignment/controller/CatToyController.java
+++ app/src/main/java/com/codesoom/assignment/controller/CatToyController.java
@@ -18,9 +18,8 @@ import java.util.List;
 
 
 /**
- * 고양이 장난감에 대한 Http Request 요청 컨트롤러
+ * 고양이 장난감에 대한 Http Request 요청을 처리합니다.
  */
-
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/products")
@@ -29,31 +28,55 @@ public class CatToyController {
     private final CatToyService catToyService;
 
 
+    /**
+     * 고양이 장남감 전체 목록을 조회한다.
+     * @return 장난감이 존재하지 않을 땐 빈 목록을 반환한다.
+     */
     @GetMapping
     public List<CatToy> findAll() {
         return catToyService.findAll();
     }
 
+    /**
+     * 식별자를 이용해 고양이 장난감 상세정보를 조회한다.
+     * @param id 고양이 장난감 식별자
+     * @return 고양이 장난감 상세 정보
+     */
     @GetMapping("/{id}")
     public CatToy findById(@PathVariable Long id) {
         return catToyService.findById(id);
     }
 
+    /**
+     * 새로운 고양이 장난감 정보를 생성한다.
+     * @param catToy 고양이 장난감 정보
+     * @return 생성된 고양이 장난감 상세 정보
+     */
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public CatToy create(@RequestBody CatToy catToy) {
         return catToyService.save(catToy);
     }
 
+    /**
+     * 식별자를 이용해 조회한 장난감 정보를 수정한다.
+     * @param id 고양이 장난감 식별자
+     * @param catToy 수정할 장난감 정보
+     * @return 수정된 고양이 장난감 상세 정보ㅗ
+     */
     @PatchMapping("/{id}")
     public CatToy updateCatToy(@PathVariable Long id, @RequestBody CatToy catToy) {
         return catToyService.updateCatToy(id, catToy);
     }
 
+    /**
+     * 식별자를 이용해 조회한 장난감 정보를 삭제한다.
+     * @param id 삭제할 고양이 장난감 식별자
+     */
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteCatToy(@PathVariable Long id) {
-        catToyService.deleteCatToy(id);
+        catToyService.deleteToy(id);
     }
 
 
