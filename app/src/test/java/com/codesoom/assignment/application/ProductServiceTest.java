3cff37f9f5075007b2bc563b5665332058e0f1be
pangnem <sprnd645@gmail.com>
2021-08-31 16:09:52 +0900

    test: 올바르지 않은 ID 테스트 케이스 추가, 계층형으로 수정

--- app/src/test/java/com/codesoom/assignment/application/ProductServiceTest.java
+++ app/src/test/java/com/codesoom/assignment/application/ProductServiceTest.java
@@ -1,20 +1,27 @@
 package com.codesoom.assignment.application;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 
 import com.codesoom.assignment.domain.Product;
 import com.codesoom.assignment.domain.ProductRepository;
+import com.codesoom.assignment.exceptions.ProductNotFoundException;
 import java.util.Collections;
 import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 public class ProductServiceTest {
+
+    public static final long ID = 1L;
+    public static final long INVALID_ID = 2L;
 
     private Product product;
     private ProductRepository productRepository;
@@ -26,11 +33,17 @@ public class ProductServiceTest {
         productRepository = mock(ProductRepository.class);
         productService = new ProductService(productRepository);
 
-        given(productRepository.save(product)).willReturn(product);
+        given(productRepository.save(product))
+            .willReturn(product);
 
-        given(productRepository.findAll()).willReturn(Collections.singletonList(product));
+        given(productRepository.findAll())
+            .willReturn(Collections.singletonList(product));
 
-        given(productRepository.findById(1L)).willReturn(Optional.of(product));
+        given(productRepository.findById(ID))
+            .willReturn(Optional.of(product));
+
+        given(productRepository.findById(INVALID_ID))
+            .willThrow(ProductNotFoundException.class);
     }
 
     @Test
@@ -53,38 +66,114 @@ public class ProductServiceTest {
         verify(productRepository).findAll();
     }
 
-    @Test
-    @DisplayName("존재하는 상품일 경우 상품 하나를 찾아 조회한다")
-    void getProduct() {
-        Product foundProduct = productService.getProduct(1L);
+    @Nested
+    @DisplayName("단일 조회 시")
+    class Describe_getProduct {
 
-        assertThat(foundProduct).isEqualTo(product);
+        @Nested
+        @DisplayName("존재하는 상품일 경우")
+        class Context_existProduct {
 
-        verify(productRepository).findById(1L);
+            @Test
+            @DisplayName("조회한다")
+            void it_read() {
+                Product foundProduct = productService.getProduct(ID);
+
+                assertThat(foundProduct).isEqualTo(product);
+
+                verify(productRepository).findById(ID);
+            }
+        }
+
+        @Nested
+        @DisplayName("존재하지 않는 상품일 경우")
+        class Context_notExistProduct {
+
+            @Test
+            @DisplayName("에러를 던진다")
+            void it_throws() {
+                assertThatThrownBy(() -> productService.getProduct(INVALID_ID))
+                    .isInstanceOf(ProductNotFoundException.class);
+
+                verify(productRepository).findById(INVALID_ID);
+            }
+        }
     }
 
-    @Test
-    @DisplayName("존재하는 상품일 경우 상품 하나를 찾아 수정한다")
-    void updateProduct() {
-        long id = 1L;
-        Product source = new Product("a", "b", 100L, "c");
 
-        Product updatedProduct = productService.updateProduct(id, source);
+    @Nested
+    @DisplayName("수정 시")
+    class Describe_updateProduct {
+        private Product source;
 
-        assertThat(source).isEqualTo(updatedProduct);
+        @BeforeEach
+        void setUp() {
+            source = new Product("a", "b", 100L, "c");
+        }
 
-        verify(productRepository).findById(id);
-        verify(productRepository).save(source);
+        @Nested
+        @DisplayName("존재하는 상품일 경우")
+        class Context_existProduct {
+
+            @Test
+            @DisplayName("수정한다")
+            void it_update() {
+                Product updatedProduct = productService.updateProduct(ID, source);
+
+                assertThat(source).isEqualTo(updatedProduct);
+
+                verify(productRepository).findById(ID);
+                verify(productRepository).save(source);
+            }
+        }
+
+        @Nested
+        @DisplayName("존재하지 않는 상품일 경우")
+        class Context_notExistProduct {
+
+            @Test
+            @DisplayName("에러를 던진다")
+            void it_throws() {
+                assertThatThrownBy(() -> productService.updateProduct(INVALID_ID, source))
+                    .isInstanceOf(ProductNotFoundException.class);
+
+                verify(productRepository).findById(INVALID_ID);
+                verify(productRepository, never()).save(source);
+            }
+        }
     }
 
-    @Test
-    @DisplayName("존재하는 상품일 경우 삭제한다")
-    void deleteProduct() {
-       long id = 1L;
+    @Nested
+    @DisplayName("삭제 시")
+    class Describe_deleteProduct {
 
-       productService.deleteProduct(id);
+        @Nested
+        @DisplayName("존재하는 상품일 경우")
+        class Context_existProduct {
 
-       verify(productRepository).findById(id);
-       verify(productRepository).delete(any(Product.class));
+            @Test
+            @DisplayName("삭제한다")
+            void deleteProduct() {
+                productService.deleteProduct(ID);
+
+                verify(productRepository).findById(ID);
+                verify(productRepository).delete(any(Product.class));
+            }
+        }
+
+        @Nested
+        @DisplayName("존재하는 상품일 경우")
+        class Context_notExistProduct {
+
+            @Test
+            @DisplayName("에러를 던진다")
+            void it_throws() {
+                assertThatThrownBy(() -> productService.deleteProduct(INVALID_ID))
+                    .isInstanceOf(ProductNotFoundException.class);
+
+                verify(productRepository).findById(INVALID_ID);
+                verify(productRepository, never()).delete(any(Product.class));
+            }
+        }
     }
 }
