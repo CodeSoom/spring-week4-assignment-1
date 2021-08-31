b463874a718e58eb519bbf5833b073fcbd720723
daeungkim <daeungkim93@gmail.com>
2021-08-31 11:28:58 +0900

    <title>
    WebTest 계층 구조 변경
    리소스 -> 엔드포인트 && 메서드 -> 리퀘스트 -> 쿼리 || 파라미터 || 바디 || 서버 내부 상태 -> 리스폰스
    -> 1. 어플리케이션에 대한 설명 -> 리소스에 대한 설명
    -> Product에 관한 테스트만 수행하므로 리소스로 표시하는것이
    -> 테스트의 범위를 좁힐 수 있다고 판단하였음

--- app/src/test/java/com/codesoom/assignment/Controller/ProductControllerWebTest.java
+++ app/src/test/java/com/codesoom/assignment/Controller/ProductControllerWebTest.java
@@ -12,25 +12,29 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest
-@DisplayName("고양이 장난감 가게 api 서버는")
+@DisplayName("Product 리소스")
 public class ProductControllerWebTest {
     private static final String EMPTY_LIST = "[]";
 
     @Autowired
     private MockMvc mockMvc;
-    
+
     @Nested
-    @DisplayName("장난감 목록을 요청받은 경우")
-    class Context_get_products {
+    @DisplayName("전체 목록 조회 엔드포인트는")
+    class Describe_read_products {
         @Nested
-        @DisplayName("저장된 장난감이 없다면")
-        class Context_product_empty {
-            @Test
-            @DisplayName("200과 함께 빈 목록을 리턴한다.")
-            void it_returns_a_200_status_code() throws Exception {
-                mockMvc.perform(get("/products"))
-                    .andExpect(status().isOk())
-                    .andExpect(content().string(EMPTY_LIST));
+        @DisplayName("장난감 목록을 요청 받은 경우")
+        class Context_get {
+            @Nested
+            @DisplayName("장난감이 없다면")
+            class Context_product_empty {
+                @Test
+                @DisplayName("200과 함께 빈 목록을 리턴한다.")
+                void it_returns_a_200_status_code() throws Exception {
+                    mockMvc.perform(get("/products"))
+                        .andExpect(status().isOk())
+                        .andExpect(content().string(EMPTY_LIST));
+                }
             }
         }
     }
