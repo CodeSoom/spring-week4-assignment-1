2f1015e9daf72ae645e7746b91c740252d9e6513
wjy5446 <wjy5446@gmail.com>
2021-08-31 22:14:46 +0900

    refac) CatToy를 Project 클래스에서 상속받아서 사용

--- app/src/main/java/com/codesoom/assignment/domain/CatToy.java
+++ app/src/main/java/com/codesoom/assignment/domain/CatToy.java
@@ -1,61 +1,13 @@
 package com.codesoom.assignment.domain;
 
-public class CatToy {
+public class CatToy extends Product{
 
-    private Long id;
-    private String name;
-    private String maker;
-    private Long price;
-    private String imageURI;
-
-    public CatToy() {}
+    public CatToy() {
+        super();
+    }
 
     public CatToy(Long id, String name, String maker, Long price, String imageURI) {
-        this.id = id;
-        this.name = name;
-        this.maker = maker;
-        this.price = price;
-        this.imageURI = imageURI;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getMaker() {
-        return maker;
-    }
-
-    public void setMaker(String maker) {
-        this.maker = maker;
-    }
-
-    public Long getPrice() {
-        return price;
-    }
-
-    public void setPrice(Long price) {
-        this.price = price;
-    }
-
-    public String getImageURI() {
-        return imageURI;
-    }
-
-    public void setImageURI(String imageURI) {
-        this.imageURI = imageURI;
+        super(id, name, maker, price, imageURI);
     }
 
 }
