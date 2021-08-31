f5880c5ba854793b8313ca5451e36f17169e1e34
pangnem <sprnd645@gmail.com>
2021-08-31 16:27:16 +0900

    fix: 에러 메시지 오타 수정

--- app/src/main/java/com/codesoom/assignment/exceptions/ProductNotFoundException.java
+++ app/src/main/java/com/codesoom/assignment/exceptions/ProductNotFoundException.java
@@ -1,7 +1,8 @@
 package com.codesoom.assignment.exceptions;
 
 public class ProductNotFoundException extends RuntimeException {
-    public static final String FORMAT = "id %s에 해당하는 id를 찾을 수 없습니다.";
+
+    public static final String FORMAT = "식별자 %s에 해당하는 상품을 찾을 수 없습니다.";
 
     public ProductNotFoundException(long id) {
         super(FORMAT.formatted(id));
