3d6292adb3a7e9277eae01ed24f37e57019c080d
wjy5446 <wjy5446@gmail.com>
2021-09-01 22:25:27 +0900

    feat) CatToyService 코드 추가

--- app/src/main/java/com/codesoom/assignment/applications/CatToyService.java
+++ app/src/main/java/com/codesoom/assignment/applications/CatToyService.java
@@ -1,2 +1,41 @@
-package com.codesoom.assignment.applications;public class CatToyService {
+package com.codesoom.assignment.applications;
+
+import com.codesoom.assignment.domain.CatToy;
+import com.codesoom.assignment.domain.CatToyRepository;
+import com.codesoom.assignment.dto.CatToyNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CatToyService {
+
+    private CatToyRepository catToyRepository;
+
+    public CatToyService(CatToyRepository catToyRepository) {
+        this.catToyRepository = catToyRepository;
+    }
+
+    CatToy save(CatToy catToy) {
+        return catToyRepository.save(catToy);
+    }
+
+    List<CatToy> findAll() {
+        return catToyRepository.findAll();
+    }
+
+    CatToy findById(Long id) {
+        return catToyRepository.findById(id)
+                .orElseThrow(()->new CatToyNotFoundException(id));
+    }
+
+    void update(Long id, CatToy catToy) {
+        catToyRepository.update(id, catToy);
+    }
+
+    void deleteById(Long id) {
+        catToyRepository.deleteById(id);
+    }
+
+
 }
