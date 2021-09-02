9aa02dd1fe286fb416c4dccee90b864cd5571dd7
Sungeheon <elon4856@gmail.com>
2021-09-02 20:32:49 +0900

    fix: CatProductRepository를 인터페이스로 파꾸세요

--- app/src/main/java/com/codesoom/assignment/domain/CatProductRepository.java
+++ app/src/main/java/com/codesoom/assignment/domain/CatProductRepository.java
@@ -4,7 +4,7 @@ import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public class CatProductRepository extends JpaRepository<CatProduct, Long> {
+public interface CatProductRepository extends JpaRepository<CatProduct, Long> {
 
 
   List<CatProduct> findAll();
