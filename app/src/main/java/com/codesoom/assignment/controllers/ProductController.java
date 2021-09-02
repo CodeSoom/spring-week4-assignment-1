c5bd00333514e7eb9dac77cc38007faba7ba6f90
wjy5446 <wjy5446@gmail.com>
2021-09-02 23:12:35 +0900

    feat) ProductContoller와 advice 추가

--- app/src/main/java/com/codesoom/assignment/controllers/ProductController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/ProductController.java
@@ -1,2 +1,46 @@
-package com.codesoom.assignment.controllers;public class ProductController {
+package com.codesoom.assignment.controllers;
+
+import com.codesoom.assignment.applications.ProductService;
+import com.codesoom.assignment.domain.Product;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/products")
+public class ProductController {
+    private ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public List<Product> findAll() {
+        return productService.findAll();
+    }
+
+    @GetMapping("{id}")
+    public Product findById(@PathVariable Long id) {
+        return productService.findById(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void save(@RequestBody Product product) {
+        productService.save(product);
+    }
+
+    @PatchMapping("{id}")
+    public void update(@PathVariable Long id, @RequestBody Product product) {
+        productService.update(id, product);
+    }
+
+    @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteById(@PathVariable Long id) {
+        productService.deleteById(id);
+    }
 }
