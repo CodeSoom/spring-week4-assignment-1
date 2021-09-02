e1954573cc1f40c1f75716ee0433fe5d52d75838
Sungeheon <elon4856@gmail.com>
2021-09-02 20:28:41 +0900

    feat: CatProductService getCatProducts를 추가하세요

--- app/src/main/java/com/codesoom/assignment/application/CatProductService.java
+++ app/src/main/java/com/codesoom/assignment/application/CatProductService.java
@@ -1,5 +1,20 @@
 package com.codesoom.assignment.application;
 
+import com.codesoom.assignment.domain.CatProduct;
+import com.codesoom.assignment.domain.CatProductRepository;
+import java.util.List;
+import javassist.NotFoundException;
+
 public class CatProductService {
+  private final CatProductRepository catProductRepository;
+
+  public CatProductService(CatProductRepository catProductRepository){
+    this.catProductRepository = catProductRepository;
+  }
+
+
+    public List<CatProduct> getCatProducts{
+    return catProductRepository.findAll();
+  }
 
 }
