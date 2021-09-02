b745d9b33c50a2d0b9f65112c63dc522acb4155b
ytkim4558@naver.com <R5vSP6djLOpfxXF>
2021-09-02 20:59:31 +0900

    catToy 과제 위한 클래스 생성

---
+++ app/src/test/java/com/codesoom/assignment/application/CatToyServiceTest.java
@@ -0,0 +1,12 @@
+package com.codesoom.assignment.application;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CatToyServiceTest {
+    // 1. list -> getCatToys
+    // 2. detail -> getCatToy (with ID)
+    // 3. create -> createCatToy(with source)
+    // 4. update -> updateCatToy (with ID, source)
+    // 5. delete -> deleteCatToy (with ID)
+
+}
