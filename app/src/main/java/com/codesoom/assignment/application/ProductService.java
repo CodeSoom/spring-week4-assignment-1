bd5be033dc25dfc672354d90462d6d6f7a4dd411
Sungeheon <elon4856@gmail.com>
2021-09-03 12:41:35 +0900

    fix: `@Service`를 추가하세요

--- app/src/main/java/com/codesoom/assignment/application/ProductService.java
+++ app/src/main/java/com/codesoom/assignment/application/ProductService.java
@@ -3,7 +3,9 @@ package com.codesoom.assignment.application;
 import com.codesoom.assignment.domain.Product;
 import com.codesoom.assignment.domain.ProductRepository;
 import java.util.List;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ProductService {
   private final ProductRepository productRepository;
 
