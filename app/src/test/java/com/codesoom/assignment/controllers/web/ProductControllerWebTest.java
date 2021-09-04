d99d06643bc8a982d14799bfc1955996520a5731
pangnem <sprnd645@gmail.com>
2021-09-04 19:40:01 +0900

    test: 누락된 웹 테스트 케이스 추가

--- app/src/test/java/com/codesoom/assignment/controllers/web/ProductControllerWebTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/web/ProductControllerWebTest.java
@@ -1,10 +1,21 @@
 package com.codesoom.assignment.controllers.web;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.codesoom.assignment.application.ProductService;
 import com.codesoom.assignment.domain.Product;
+import com.codesoom.assignment.exceptions.ProductNotFoundException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -20,6 +31,23 @@ public class ProductControllerWebTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    private ProductService productService;
+
+    private Product product;
+
+    @BeforeEach
+    void setUp() {
+        product = Product.builder()
+            .name("name")
+            .maker("maker")
+            .price(10000L)
+            .imageUrl("imageUrl")
+            .build();
+
+        productService.createProduct(product);
+    }
 
     @Nested
     @DisplayName("POST /products 요청은")
@@ -42,6 +70,7 @@ public class ProductControllerWebTest {
                 mockMvc.perform(post("/products")
                     .contentType(MediaType.APPLICATION_JSON)
                     .content(product.toString()))
+                    .andExpect(content().json(product.toString()))
                     .andExpect(status().isCreated());
             }
         }
@@ -67,12 +96,51 @@ public class ProductControllerWebTest {
         @DisplayName("존재하는 id일 경우")
         class Context_existId {
 
+            private Long existProductId;
+
+            @BeforeEach
+            void setUp() {
+                Long productId = product.getId();
+
+                checkHasId(productId);
+
+                existProductId = productId;
+            }
+
+            @Test
+            @DisplayName("찾은 상품을 반환하고 200을 응답한다")
+            void it_return_find_product_and_response_200() throws Exception {
+                mockMvc.perform(get("/products/" + existProductId))
+                    .andExpect(content().json(product.toString()))
+                    .andExpect(status().isOk());
+            }
         }
 
         @Nested
         @DisplayName("존재하지 않는 id일 경우")
         class Context_notExistId {
 
+            private Long notExistProductId;
+
+            @BeforeEach
+            void setUp() {
+                Long productId = product.getId();
+
+                productService.deleteProduct(productId);
+
+                assertThatThrownBy(() ->
+                    productService.getProduct(productId)
+                ).isInstanceOf(ProductNotFoundException.class);
+
+                notExistProductId = productId;
+            }
+
+            @Test
+            @DisplayName("404를 응답한다")
+            void it_response_404() throws Exception {
+                mockMvc.perform(get("/products/" + notExistProductId))
+                    .andExpect(status().isNotFound());
+            }
         }
     }
 
@@ -85,12 +153,62 @@ public class ProductControllerWebTest {
         @DisplayName("존재하는 id일 경우")
         class Context_existId {
 
+            private Long existProductId;
+
+            @BeforeEach
+            void setUp() {
+                Long productId = product.getId();
+
+                checkHasId(productId);
+
+                existProductId = productId;
+            }
+
+            @Test
+            @DisplayName("수정한 상품을 반환하고 200을 응답한다")
+            void it_return_updated_product_and_response_200() throws Exception {
+                Product source = Product.builder()
+                    .name("newName")
+                    .maker("newMaker")
+                    .price(100L)
+                    .imageUrl("newImageUrl")
+                    .build();
+
+                mockMvc.perform(patch("/products/" + existProductId)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(source.toString()))
+                    .andExpect(content().json(source.toString()))
+                    .andExpect(status().isOk());
+            }
         }
 
         @Nested
         @DisplayName("존재하지 않는 id일 경우")
         class Context_notExistId {
 
+            private Long notExistProductId;
+
+            @BeforeEach
+            void setUp() {
+                Long productId = product.getId();
+
+                productService.deleteProduct(productId);
+
+                assertThatThrownBy(() ->
+                    productService.getProduct(productId)
+                ).isInstanceOf(ProductNotFoundException.class);
+
+                notExistProductId = productId;
+            }
+
+            @Test
+            @DisplayName("404를 응답한다")
+            void it_response_404() throws Exception {
+                mockMvc.perform(patch("/products/" + notExistProductId)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(product.toString()))
+                    .andExpect(status().isNotFound());
+            }
         }
     }
 
@@ -103,12 +221,63 @@ public class ProductControllerWebTest {
         @DisplayName("존재하는 id일 경우")
         class Context_existId {
 
+            private Long existProductId;
+
+            @BeforeEach
+            void setUp() {
+                Long productId = product.getId();
+
+                checkHasId(productId);
+
+                existProductId = productId;
+            }
+
+            @Test
+            @DisplayName("상품을 삭제하고 204를 응답한다")
+            void it_delete_product_and_response_204() throws Exception {
+                mockMvc.perform(delete("/products/" + existProductId))
+                    .andExpect(status().isNoContent());
+            }
         }
 
         @Nested
         @DisplayName("존재하지 않는 id일 경우")
         class Context_notExistId {
 
+            private Long notExistProductId;
+
+            @BeforeEach
+            void setUp() {
+                Long productId = product.getId();
+
+                productService.deleteProduct(productId);
+
+                assertThatThrownBy(() ->
+                    productService.getProduct(productId)
+                ).isInstanceOf(ProductNotFoundException.class);
+
+                notExistProductId = productId;
+            }
+
+            @Test
+            @DisplayName("404를 응답한다")
+            void it_response_404() throws Exception {
+                mockMvc.perform(delete("/products/" + notExistProductId))
+                    .andExpect(status().isNotFound());
+            }
         }
+    }
+
+    private void checkHasId(Long productId) {
+        List<Product> products = new ArrayList<>();
+
+        Iterator<Product> iterator = productService.getAllProducts()
+            .iterator();
+        iterator.forEachRemaining(products::add);
+
+        boolean actual = products.stream()
+            .anyMatch(product -> productId.equals(product.getId()));
+
+        assertThat(actual).isTrue();
     }
 }
