b29e2add27f511870d40c36c3cf03792830e4004
ytkim4558@naver.com <R5vSP6djLOpfxXF>
2021-09-05 23:40:48 +0900

    CatToyTest

--- app/src/main/java/com/codesoom/assignment/application/CatToyService.java
+++ app/src/main/java/com/codesoom/assignment/application/CatToyService.java
@@ -6,30 +6,30 @@ import com.codesoom.assignment.domain.CatToyRepository;
 import java.util.List;
 
 public class CatToyService {
-    private final CatToyRepository catToyRepository;
+//    private final CatToyRepository catToyRepository;
 
-    public CatToyService(CatToyRepository catToyRepository) {
-        this.catToyRepository = catToyRepository;
-    }
-
-    public List<CatToy> getCatToys() {
-        return catToyRepository.findAll();
-    }
-
-    public CatToy getCatToy(Long id) {
-        return catToyRepository.findById(id)
-                .orElseThrow(() -> new CatToyNotFoundException(id));
-    }
-
-    public CatToy createCatToy(CatToy source) {
-        CatToy catToy = new CatToy();
-        catToy.setTitle(catToy.getTitle());
-
-        return catToyRepository.save(catToy);
-    }
-
-    public CatToy updateCatToy(Long id, CatToy source) {
-        CatToy catToy = catToyRepository.findById(id)
-                .orElseThrow(() -> new CatToyNotFoundException(id));
-    }
+//    public CatToyService(CatToyRepository catToyRepository) {
+//        this.catToyRepository = catToyRepository;
+//    }
+//
+//    public List<CatToy> getCatToys() {
+//        return catToyRepository.findAll();
+//    }
+//
+//    public CatToy getCatToy(Long id) {
+//        return catToyRepository.findById(id)
+//                .orElseThrow(() -> new CatToyNotFoundException(id));
+//    }
+//
+//    public CatToy createCatToy(CatToy source) {
+//        CatToy catToy = new CatToy();
+//        catToy.setTitle(catToy.getTitle());
+//
+//        return catToyRepository.save(catToy);
+//    }
+//
+//    public CatToy updateCatToy(Long id, CatToy source) {
+//        CatToy catToy = catToyRepository.findById(id)
+//                .orElseThrow(() -> new CatToyNotFoundException(id));
+//    }
 }
--- app/src/main/java/com/codesoom/assignment/domain/CatToy.java
+++ app/src/main/java/com/codesoom/assignment/domain/CatToy.java
@@ -1,4 +1,66 @@
 package com.codesoom.assignment.domain;
 
+// 이름 장난감 뱀
+// 메이커 애옹이네 장난감
+// 가격 5000
+// 이미지 url
 public class CatToy {
+    private Long id;
+    private String catToyName;
+    private String makerName;
+    private Double price;
+    private String imageUrl;
+
+    public CatToy(Long id, String catToyName, String makerName, Double price, String imageUrl) {
+        this.id = id;
+        this.catToyName = catToyName;
+        this.makerName = makerName;
+        this.price = price;
+        this.imageUrl = imageUrl;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public CatToy(String catToyName) {
+        this.catToyName = catToyName;
+    }
+
+    public String getCatToyName() {
+        return this.catToyName;
+    }
+
+
+    public void setCatToyName(String catToyName) {
+        this.catToyName = catToyName;
+    }
+
+    public String getMakerName() {
+        return makerName;
+    }
+
+    public void setMakerName(String makerName) {
+        this.makerName = makerName;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
 }
