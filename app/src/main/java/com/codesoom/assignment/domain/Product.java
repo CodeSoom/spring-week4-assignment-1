0cc5dd4c74f03e4bf4fabf973153b6dcff8c1eeb
wjy5446 <wjy5446@gmail.com>
2021-09-05 19:44:05 +0900

    fix) entity의 id 생성 전략 AUTO로 변경

--- app/src/main/java/com/codesoom/assignment/domain/Product.java
+++ app/src/main/java/com/codesoom/assignment/domain/Product.java
@@ -9,7 +9,7 @@ import javax.persistence.Id;
 public class Product {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String name;
     private String maker;
