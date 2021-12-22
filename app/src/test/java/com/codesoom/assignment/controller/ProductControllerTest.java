82d2658d348250abc3c6746281fef0601d939019
hyuk <pjh08190819@naver.com>
2021-12-23 01:55:59 +0900

    "/products" 전체 리스트 가져오기 위한 테스트

--- app/src/test/java/com/codesoom/assignment/controller/ProductControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controller/ProductControllerTest.java
@@ -1,7 +1,98 @@
 package com.codesoom.assignment.controller;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.codesoom.assignment.domain.Product;
+import com.codesoom.assignment.service.ProductService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@DisplayName("ProductController 클래스")
 class ProductControllerTest {
 
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    ProductService productService;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Nested
+    @DisplayName("list() 메소드는")
+    class Describe_list {
+        @Nested
+        @DisplayName("등록된 Product들이 존재하면")
+        class Context_has_product {
+            final int givenProductCnt = 5;
+
+            @BeforeEach
+            void prepare() {
+                List<Product> products = new ArrayList<>();
+                IntStream.range(0, givenProductCnt).forEach((i) -> products.add(getProduct()));
+
+                given(productService.getProducts()).willReturn(products);
+            }
+
+            @Test
+            @DisplayName("200(Ok)와 Product의 전체 리스트를 응답합니다.")
+            void it_return_ok_and_product() throws Exception {
+                mockMvc.perform(get("/products"))
+                        .andExpect(status().isOk())
+                        .andExpect(jsonPath("$", hasSize(givenProductCnt)))
+                        .andDo(print());
+            }
+        }
+
+        @Nested
+        @DisplayName("등록된 Product들이 없다면")
+        class Context_has_not_product {
+            final int givenProductCnt = 0;
+
+            @BeforeEach
+            void prepare() {
+                List<Product> products = new ArrayList<>();
+
+                given(productService.getProducts()).willReturn(products);
+            }
+
+            @Test
+            @DisplayName("빈 리스트를 리턴합니다.")
+            void it_return_products() throws Exception {
+                mockMvc.perform(get("/products"))
+                        .andExpect(status().isOk())
+                        .andExpect(jsonPath("$", hasSize(givenProductCnt)))
+                        .andDo(print());
+            }
+        }
+    }
+
+    private Product getProduct() {
+        return Product.builder()
+                .name("테스트 제품")
+                .maker("테스트 메이커")
+                .price(1000)
+                .image("http://test.com/test.jpg")
+                .build();
+    }
 }
--- app/src/test/java/com/codesoom/assignment/repository/ProductRepositoryTest.java
+++ app/src/test/java/com/codesoom/assignment/repository/ProductRepositoryTest.java
@@ -1,7 +1,51 @@
 package com.codesoom.assignment.repository;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.codesoom.assignment.domain.Product;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.stream.IntStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@DisplayName("ProductRepository 인터페이스")
 class ProductRepositoryTest {
 
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Nested
+    @DisplayName("findAll 메소드는")
+    class Describe_findAll {
+        @Nested
+        @DisplayName("등록된 Product가 있다면")
+        class Context_has_product {
+            private final int givenProductCnt = 5;
+
+            @BeforeEach
+            void prepare() {
+                IntStream.range(0, givenProductCnt).forEach((i) -> productRepository.save(getProduct()));
+            }
+
+            @Test
+            @DisplayName("Product의 전체 리스트를 리턴한다.")
+            void it_return_products() {
+                assertThat(productRepository.findAll()).hasSize(givenProductCnt);
+            }
+        }
+    }
+
+    private Product getProduct() {
+        return Product.builder()
+                .name("테스트 제품")
+                .maker("테스트 메이커")
+                .price(1000)
+                .image("http://test.com/test.jpg")
+                .build();
+    }
 }
