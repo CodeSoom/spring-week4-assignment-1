9d1a5db11ba8e7d87b0358deabdd84f341eabcaf
kyuwon53 <lkw2v@naver.com>
2021-12-22 22:52:24 +0900

    [Create] 상품 객체를 생성

---
+++ app/src/main/java/com/codesoom/domain/Product.java
@@ -0,0 +1,66 @@
+package com.codesoom.domain;
+
+import java.math.BigDecimal;
+
+public class Product {
+    private Long id;
+
+    private String name;
+
+    private String maker;
+
+    private BigDecimal price;
+
+    private String imageUrl;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMaker() {
+        return maker;
+    }
+
+    public void setMaker(String maker) {
+        this.maker = maker;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", maker='" + maker + '\'' +
+                ", price=" + price +
+                ", imageUrl='" + imageUrl + '\'' +
+                '}';
+    }
+}
