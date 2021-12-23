97ad5808472c4d4cc52408d16b5f8ac1fa51e2ff
Seungchul Jang <jangsc0000@gmail.com>
2021-12-23 14:06:38 +0900

    CatToy에 Entity anotation 추가

--- app/src/main/java/com/codesoom/assignment/domain/CatToy.java
+++ app/src/main/java/com/codesoom/assignment/domain/CatToy.java
@@ -1,6 +1,15 @@
 package com.codesoom.assignment.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class CatToy {
+    @Id
+    @GeneratedValue
+    private Long id;
+
     private String name;
     private String maker;
     private String image;
