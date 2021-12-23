8ba3ecdbc70694c369b385bba1cfd0a3a21f26a6
kyuwon53 <lkw2v@naver.com>
2021-12-24 00:57:45 +0900

    [Refactoring] ProductRepository로 책임을 분리하라

--- app/src/main/java/com/codesoom/application/ProductService.java
+++ app/src/main/java/com/codesoom/application/ProductService.java
@@ -1,58 +1,49 @@
 package com.codesoom.application;
 
 import com.codesoom.domain.Product;
+import com.codesoom.domain.ProductRepository;
 import com.codesoom.exception.ProductNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 
 @Service
 public class ProductService {
-    private final Map<Long, Product> products = new LinkedHashMap<>();
-    private Long newId = 0L;
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
 
     public List<Product> getProducts() {
-        return new ArrayList<Product>(products.values());
+        return productRepository.findAll();
     }
 
     public Optional<Product> getProduct(Long id) {
-        Optional<Product> product = findProduct(id);
-        if (!product.isPresent()) {
-            throw new ProductNotFoundException("요청한 " + id + "의 Product가 없습니다.");
-        }
-        return product;
+        return productRepository.find(id);
     }
 
     public Product createProduct(Product product) {
-        Long id = generateId();
-        product.setId(id);
-        products.put(id, product);
+
+        return productRepository.save(product);
+    }
+
+    public Optional<Product> updateProduct(Product source, Long id) {
+        Optional<Product> product = productRepository.find(id);
+
+        product.get().setName(source.getName());
+        product.get().setPrice(source.getPrice());
+        product.get().setMaker(source.getMaker());
+        product.get().setImageUrl(source.getImageUrl());
 
         return product;
     }
 
     public List<Product> deleteProduct(Long id) {
-        Optional<Product> product = findProduct(id);
-        if (!product.isPresent()) {
-            throw new ProductNotFoundException("요청한 " + id + "의 Product가 없습니다.");
-        }
-        products.remove(id);
+        Optional<Product> product = productRepository.find(id);
+
+        productRepository.remove(product);
         return getProducts();
-    }
-
-    private Optional<Product> findProduct(Long id) {
-        if (id == null) {
-            throw new IllegalArgumentException("id가 null이므로 Task를 찾을 수 없습니다.");
-        }
-        return Optional.ofNullable(products.get(id));
-    }
-
-    private synchronized Long generateId() {
-        newId += 1;
-        return newId;
     }
 }
