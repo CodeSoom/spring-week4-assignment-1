2a7f95a4ef22a40c8afd78f315347fda652a42e5
Hyunjune Lee <aphlox@naver.com>
2021-12-24 00:48:00 +0900

    test: ProductService getProducts 테스트 작성

---
+++ app/src/test/java/com/codesoom/assignment/application/ProductServiceTest.java
@@ -0,0 +1,61 @@
+package com.codesoom.assignment.application;
+
+import com.codesoom.assignment.domain.Product;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("Product Service")
+class ProductServiceTest {
+    //1. list   -> getProducts
+    //2. detail -> getProduct (with ID)
+    //3. create -> createProduct (with source)
+    //4. update -> updateProduct (with ID, source)
+    //5. delete -> deleteProduct (with ID)
+
+    private static final String NAME = "TestName ";
+    private static final String MAKER = "TestMaker";
+    private static final long PRICE = 1000;
+    private static final String IMAGE = "http://gdimg.gmarket.co.kr/1482334965/still/600?ver=1535083811";
+
+    private ProductService productService;
+
+
+    @BeforeEach
+    void setUp(){
+        productService = new ProductService();
+
+        Product product = new Product();
+        Product.setName(NAME);
+        Product.setMaker(MAKER);
+        Product.setPrice(PRICE);
+        Product.setImage(IMAGE);
+
+        productService.createProduct(product);
+    }
+
+    @Nested
+    @DisplayName("getProducts 메소드는")
+    class Describe_getProducts{
+        
+        @Test
+        @DisplayName("저장되어 있는 제품 리스트를 리턴합니다.")
+        void it_returns_products(){
+            List<Product> products = productService.getProducts();
+            assertThat(product).hasSize(1);
+
+            Product product = products.get(0);
+            assertThat(product.getName()).isEqualTo(NAME);
+            assertThat(product.getMaker()).isEqualTo(MAKER);
+            assertThat(product.getPrice()).isEqualTo(PRICE);
+            assertThat(product.getImage()).isEqualTo(IMAGE);
+        }
+
+
+    }
+}
