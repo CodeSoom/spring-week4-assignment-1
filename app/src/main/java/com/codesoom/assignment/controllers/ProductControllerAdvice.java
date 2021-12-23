715bd4ed5b5eff99e47d56e9db514de69b1175ee
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2021-12-23 22:40:16 +0900

    [추가] ProductControllerAdvice - javadoc 작성

--- app/src/main/java/com/codesoom/assignment/controllers/ProductControllerAdvice.java
+++ app/src/main/java/com/codesoom/assignment/controllers/ProductControllerAdvice.java
@@ -8,9 +8,17 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+/**
+ * 상품 컨트롤러에서 발생한 예외를 처리한다.
+ */
 @ControllerAdvice
 public class ProductControllerAdvice {
 
+    /**
+     * ProductNotFoundException 예외를 처리하여 에러응답 객체를 리턴한다.
+     * @param productNotFoundException 상품 컨트롤러에서 발생한 예외
+     * @return 에러 응답 객체
+     */
     @ResponseBody
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(ProductNotFoundException.class)
