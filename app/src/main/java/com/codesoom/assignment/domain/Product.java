1f18536f0f770c268991b62fddf425b6cb82cc68
Hyunjune Lee <aphlox@naver.com>
2021-12-24 00:59:38 +0900

    feat: Product Entity 구현

---
+++ app/src/main/java/com/codesoom/assignment/domain/Product.java
@@ -0,0 +1,59 @@
+package com.codesoom.assignment.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+    private String maker;
+    private Long price;
+    private String image;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMaker() {
+        return maker;
+    }
+
+    public void setMaker(String maker) {
+        this.maker = maker;
+    }
+
+    public Long getPrice() {
+        return price;
+    }
+
+    public void setPrice(Long price) {
+        this.price = price;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
+
