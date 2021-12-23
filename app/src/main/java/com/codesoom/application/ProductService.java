44d5495031086a4f07b5b82bb119f2ecae4eae86
kyuwon53 <lkw2v@naver.com>
2021-12-24 01:48:25 +0900

    [Fix] javadoc 주석을 달아라

--- app/src/main/java/com/codesoom/application/ProductService.java
+++ app/src/main/java/com/codesoom/application/ProductService.java
@@ -2,12 +2,14 @@ package com.codesoom.application;
 
 import com.codesoom.domain.Product;
 import com.codesoom.domain.ProductRepository;
-import com.codesoom.exception.ProductNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * 상품을 관리하기 위한 클래스
+ */
 @Service
 public class ProductService {
     private final ProductRepository productRepository;
@@ -16,19 +18,42 @@ public class ProductService {
         this.productRepository = productRepository;
     }
 
+    /**
+     * 상품 리스트를 반환합니다.
+     *
+     * @return 상품 리스트
+     */
     public List<Product> getProducts() {
         return productRepository.findAll();
     }
 
+    /**
+     * id에 해당하는 상품을 반환합니다.
+     *
+     * @param id 상품 id
+     * @return 해당하는 상품
+     */
     public Optional<Product> getProduct(Long id) {
         return productRepository.find(id);
     }
 
+    /**
+     * 상품을 저장하고 반환합니다.
+     *
+     * @param product 저장할 상품
+     * @return 저장된 상품
+     */
     public Product createProduct(Product product) {
 
         return productRepository.save(product);
     }
 
+    /**
+     * id에 해당하는 상품을 수정하고 반환합니다.
+     *
+     * @param source 수정할 상품
+     * @return 수정된 상품
+     */
     public Optional<Product> updateProduct(Product source, Long id) {
         Optional<Product> product = productRepository.find(id);
 
@@ -40,6 +65,12 @@ public class ProductService {
         return product;
     }
 
+    /**
+     * id에 해당하는 상품을 삭제하고 남은 목록을 반환합니다.
+     *
+     * @param id 삭제할 상품id
+     * @return 삭제 후 남은 상품 목록
+     */
     public List<Product> deleteProduct(Long id) {
         Optional<Product> product = productRepository.find(id);
 
