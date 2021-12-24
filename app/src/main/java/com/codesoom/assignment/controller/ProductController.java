df93854c7119f69ca95ec7c9a466a2dc8c7e6a2e
Hyunjune Lee <aphlox@naver.com>
2021-12-25 00:19:47 +0900

    feat: ProductController의 GET /products/{id} 요청시 해당하는 product 리턴하도록 구현 및 테스트 통과

--- app/src/main/java/com/codesoom/assignment/controller/ProductController.java
+++ app/src/main/java/com/codesoom/assignment/controller/ProductController.java
@@ -5,6 +5,7 @@ import com.codesoom.assignment.application.ProductService;
 import com.codesoom.assignment.domain.Product;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,6 +24,11 @@ public class ProductController {
     @GetMapping
     public List<Product> list() {
         return productService.getProducts();
+    }
+
+    @GetMapping("{id}")
+    public Product detail(@PathVariable Long id) {
+        return productService.getProductById(id);
     }
 
 }
