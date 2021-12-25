d53bb126090e944c7210548f617325aad0c506e1
kyuwon53 <lkw2v@naver.com>
2021-12-25 20:00:24 +0900

    [Create] 컨트롤러 web 테스트를 생성하라

---
+++ app/src/test/java/com/codesoom/controllers/ProductControllerWebTest.java
@@ -0,0 +1,287 @@
+package com.codesoom.controllers;
+
+import com.codesoom.application.ProductService;
+import com.codesoom.domain.Product;
+import com.codesoom.domain.ProductRepository;
+import com.codesoom.exception.ProductNotFoundException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebAppConfiguration
+@SpringBootTest(classes = ProductController.class)
+@AutoConfigureMockMvc
+@DisplayName("ProductController Web 테스트")
+class ProductControllerWebTest {
+    private static final String PRODUCT_NAME = "고양이 장남감";
+    private static final String UPDATE_POSTFIX = "new";
+    private static final String PRODUCT_MAKER = "코드숨";
+    private static final BigDecimal PRODUCT_PRICE = BigDecimal.valueOf(10000);
+    private static final BigDecimal NEW_PRICE = BigDecimal.valueOf(100);
+    private static final String PRODUCT_IMAGE_URL = "test.jpg";
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private ProductService productService;
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @BeforeEach
+    void setUp() {
+        productRepository = new ProductRepository();
+
+        productService = new ProductService(productRepository);
+    }
+
+    @Nested
+    @DisplayName("GET 요청은")
+    class Describe_get {
+        @Nested
+        @DisplayName("등록된 Product가 있다면")
+        class Context_has_product {
+            final int productCount = 3;
+
+            @BeforeEach
+            void setUp() {
+                List<Product> products = new ArrayList<>();
+
+                for (int i = 1; i < productCount; i++) {
+                    productService.createProduct(getProduct(i));
+                    products.add(getProduct(i));
+                }
+                given(productService.getProducts()).willReturn(products);
+            }
+
+            @Test
+            @DisplayName("전체 리스트를 리턴한다.")
+            void it_return_list() throws Exception {
+                mockMvc.perform(get("/products"))
+                        .andExpect(status().isOk());
+
+                //verify(productService.getProducts());
+            }
+        }
+
+        @Nested
+        @DisplayName("등록된 Product가 없다면")
+        class Context_none_product {
+            @BeforeEach
+            void setUp() {
+                List<Product> products = productService.getProducts();
+                products.forEach(product -> productService.deleteProduct(product.getId()));
+
+                given(null).willReturn(null);
+            }
+
+            @Test
+            @DisplayName("빈 리스트를 리턴한다.")
+            void it_return_list() throws Exception {
+                mockMvc.perform(get("/products"))
+                        .andExpect(status().isOk())
+                        .andExpect(content().string(containsString("[]")));
+
+                //verify(productService.getProducts());
+            }
+        }
+
+        @Nested
+        @DisplayName("등록된 id 값이 주어졌을때")
+        class Context_when_product_is_exist {
+            Product product;
+
+            @BeforeEach
+            void setUp() {
+                product = getProduct(0);
+
+                productService.createProduct(getProduct(1));
+                given(productService.getProduct(1L)).willReturn(Optional.of(product));
+            }
+
+            @Test
+            @DisplayName("등록된 Product 정보를 리턴한다.")
+            void it_return_product() throws Exception {
+
+                mockMvc.perform(get("/products/1"))
+                        .andExpect(status().isOk())
+                        .andExpect(content().string(containsString(PRODUCT_NAME + 1)))
+                        .andExpect(content().string(containsString(PRODUCT_MAKER + 1)))
+                        .andExpect(content().string(containsString(String.valueOf(PRODUCT_PRICE))))
+                        .andExpect(content().string(containsString(PRODUCT_IMAGE_URL)));
+
+                //verify(productService.getProduct(1L));
+            }
+        }
+
+        @Nested
+        @DisplayName("id에 해당하는 Product가 존재하지 않으면")
+        class Context_when_product_isnot_exist {
+
+            @BeforeEach
+            void setUp() {
+                given(productService.getProduct(0L))
+                        .willThrow(new ProductNotFoundException("요청한 0의 Product를 찾지 못했습니다."));
+            }
+
+            @Test
+            @DisplayName("Product를 찾을 수 없다는 예외를 던진다.")
+            void it_throw_ProductNotFoundException() throws Exception {
+                mockMvc.perform(get("/products/0"))
+                        .andExpect(status().isNotFound());
+
+                //verify(productService.getProduct(0L));
+            }
+        }
+    }
+
+    @Nested
+    @DisplayName("POST 요청은")
+    class Describe_post {
+        String product;
+
+        @Nested
+        @DisplayName("Product가 주어진다면")
+        class Context_with_new_product {
+            @BeforeEach
+            void setUp() throws JsonProcessingException {
+                product = objectMapper.writeValueAsString(getProduct(1));
+                given(productService.createProduct(getProduct(1)))
+                        .willReturn(getProduct(1));
+            }
+
+            @Test
+            @DisplayName("Product를 저장하고 Created를  응답한다.")
+            void it_return_status_created() throws Exception {
+
+                mockMvc.perform(post("/products")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(product))
+                        .andExpect(status().isCreated());
+
+                verify(productService).createProduct(any(Product.class));
+            }
+        }
+    }
+
+    @Nested
+    @DisplayName("PATCH 요청은")
+    class Describe_patch {
+        String product;
+
+        @BeforeEach
+        void setUp() throws JsonProcessingException {
+            productService.createProduct(getProduct(1));
+            product = objectMapper.writeValueAsString(getProduct(100));
+            given(productService.updateProduct(any(Product.class), eq(0L)))
+                    .willThrow(new ProductNotFoundException("요청한 0의 Product를 찾지 못했습니다."));
+        }
+
+        @Nested
+        @DisplayName("등록된 id가 주어진다면")
+        class Context_when_product_is_exist {
+            @Test
+            @DisplayName("id에 해당하는 Product 정보를 수정하고 리턴한다.")
+            void it_fix_product_return() throws Exception {
+                mockMvc.perform(patch("/products/1")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(product))
+                        .andExpect(status().isOk());
+
+                verify(productService).updateProduct(any(Product.class), eq(1L));
+            }
+        }
+
+        @Nested
+        @DisplayName("등록되지않은 id가 주어진다면")
+        class Context_when_product_isnot_exist {
+            @Test
+            @DisplayName("id에 해당하는 Product를 찾을 수 없어 수정할 수 없다고 예외를 던진다.")
+            void it_throw_ProductNotFoundException() throws Exception {
+                mockMvc.perform(patch("/products/0")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(product))
+                        .andExpect(status().isNotFound());
+
+                verify(productService).updateProduct(any(Product.class), eq(0L));
+            }
+        }
+    }
+
+    @Nested
+    @DisplayName("DELETE 요청은")
+    class Describe_delete {
+
+        @BeforeEach
+        void setUp() {
+            productService.createProduct(getProduct(1));
+            given(productService.deleteProduct(0L))
+                    .willThrow(new ProductNotFoundException("요청한 0의 Product를 찾지 못했습니다."));
+        }
+
+        @Nested
+        @DisplayName("등록된 id가 주어진다면")
+        class Context_when_product_is_exist {
+            @Test
+            @DisplayName("id에 해당하는 Product를 삭제한다.")
+            void it_return_products() throws Exception {
+                mockMvc.perform(delete("/products/1"))
+                        .andExpect(status().isNoContent());
+
+                verify(productService).deleteProduct(1L);
+            }
+        }
+
+        @Nested
+        @DisplayName("등록되지않은 id가 주어진다면")
+        class Context_when_product_isnot_exist {
+            @Test
+            @DisplayName("id에 해당하는 Product를 찾을 수 없어 삭제할 수 없다고 예외를 던진다.")
+            void it_return_products() throws Exception {
+                mockMvc.perform(delete("/products/0"))
+                        .andExpect(status().isNotFound());
+
+                verify(productService).deleteProduct(0L);
+            }
+        }
+    }
+
+    private Product getProduct(int number) {
+        Product product = new Product();
+
+        product.setName(PRODUCT_NAME + number);
+        product.setMaker(PRODUCT_MAKER + number);
+        product.setPrice(PRODUCT_PRICE);
+        product.setImageUrl(PRODUCT_IMAGE_URL);
+
+        return product;
+    }
+}
