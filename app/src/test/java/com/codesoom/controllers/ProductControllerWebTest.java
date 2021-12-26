0cc1997cc579fbaba9b039983facfa93fa7b2460
kyuwon53 <lkw2v@naver.com>
2021-12-26 15:04:21 +0900

    [Fix] 테스트 어노테이션 변경 @WebMvcTest는 MVC를 위한 테스트로 컨트롤러를 테스트하는 데 적합하여 @SpringBootTest 보다 다볍게 테스트할 수 있다

--- app/src/test/java/com/codesoom/controllers/ProductControllerWebTest.java
+++ app/src/test/java/com/codesoom/controllers/ProductControllerWebTest.java
@@ -10,7 +10,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
@@ -32,7 +34,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(ProductController.class)
+//@WebMvcTest(ProductController.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 @DisplayName("ProductController Web 테스트")
 class ProductControllerWebTest {
     @Autowired
