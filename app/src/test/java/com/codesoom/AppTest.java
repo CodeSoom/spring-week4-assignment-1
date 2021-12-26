7a0923b7ccded7369eb4adb0b620b4fb766959c5
kyuwon53 <lkw2v@naver.com>
2021-12-26 15:10:47 +0900

    [Remove] 폴더를 이동하라

---
+++ app/src/test/java/com/codesoom/AppTest.java
@@ -0,0 +1,16 @@
+package com.codesoom;
+
+import com.codesoom.App;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class AppTest {
+    @Test
+    void appHasAGreeting() {
+        App classUnderTest = new App();
+        assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
+        assertEquals(classUnderTest.getGreeting(), "Hello, world!");
+    }
+}
