180570b5d4ecc78e55f5ac9de93b612cf41ecf89
Hyunjune Lee <aphlox@naver.com>
2021-12-26 23:06:36 +0900

    refac: 테스트 시작할 때 productRepository 초기화하고 시작하도록 수정
    만약 끝날 때 초기화하면 혹시 테스트 실행 중에 오류로 초기화 로직이 실행 안되면 이후 테스트에 영향을 줄 수 있어서 수정함

--- app/src/test/java/com/codesoom/assignment/application/ProductServiceTest.java
+++ app/src/test/java/com/codesoom/assignment/application/ProductServiceTest.java
@@ -3,7 +3,6 @@ package com.codesoom.assignment.application;
 import com.codesoom.assignment.domain.Product;
 import com.codesoom.assignment.domain.ProductRepository;
 import com.codesoom.assignment.dto.ProductNotFoundException;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -31,18 +30,13 @@ class ProductServiceTest {
 
     @BeforeEach
     void setUp() {
-
+        productRepository.deleteAll();
         productService = new ProductService(productRepository);
 
         product1 = new Product(0L, "catTower", "samsung", 35900L, "https://thumbnail14.coupangcdn.com/thumbnails/remote/712x712ex/image/retail/images/451976858609946-e5186418-5f5e-4f4c-bccc-a59ac573d029.jpg");
         product2 = new Product(1L, "catBall", "love cat", 8000L, "http://mstatic1.e-himart.co.kr/contents/goods/00/05/96/13/20/0005961320__TB10__M_640_640.jpg");
     }
-
-    @AfterEach
-    public void afterEach() {
-        productRepository.deleteAll();
-    }
-
+    
     @Nested
     @DisplayName("getProducts 메소드는")
     class Describe_getProducts {
