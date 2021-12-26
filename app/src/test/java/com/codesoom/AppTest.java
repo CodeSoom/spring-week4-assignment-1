894ad093adfec0c43fbb06cb94bd0449098e59fd
kyuwon53 <lkw2v@naver.com>
2021-12-26 15:15:23 +0900

    [Refactoring] 이름 바꿔라

--- app/src/test/java/com/codesoom/AppTest.java
+++ app/src/test/java/com/codesoom/AppTest.java
@@ -9,8 +9,8 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 class AppTest {
     @Test
     void appHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
-        assertEquals(classUnderTest.getGreeting(), "Hello, world!");
+        App app = new App();
+        assertNotNull(app.getGreeting(), "app should have a greeting");
+        assertEquals(app.getGreeting(), "Hello, world!");
     }
 }
