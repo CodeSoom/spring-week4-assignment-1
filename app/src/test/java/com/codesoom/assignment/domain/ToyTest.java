9ab75cdb2a64b5f77dd971501c71db1c402fe998
SUBIN606 <gaemilee606@gmail.com>
2022-04-11 20:36:41 +0900

    test: Toy 엔티티 필수 값과 nullable한 값 분리 테스트

--- app/src/test/java/com/codesoom/assignment/domain/ToyTest.java
+++ app/src/test/java/com/codesoom/assignment/domain/ToyTest.java
@@ -9,6 +9,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class ToyTest {
 
+    private static final String TOY_NAME = "키위새";
+    private static final String TOY_MAKER = "어쩌구컴퍼니";
+    private static final BigDecimal TOY_PRICE = BigDecimal.valueOf(3000);
+    private static final String TOY_IMG_URL = "some url";
+
     @DisplayName("기본 생성자로 장난감 엔티티를 생성할 수 있다.")
     @Test
     void createNoArgsConstructorTest () {
@@ -18,28 +23,20 @@ public class ToyTest {
     @DisplayName("Builder 패턴으로 장난감 엔티티를 생성할 수 있다.")
     @Test
     void createWithBuilderTest() {
-        assertThat(new Toy.Builder(1L,
-                "키위새",
-                "어쩌구컴퍼니",
-                BigDecimal.valueOf(3000),
-                "https://somthing.com").build())
-        .isNotNull();
+        assertThat(new Toy.Builder(TOY_NAME, TOY_MAKER, TOY_PRICE).image(TOY_IMG_URL)
+                        .build()).isNotNull();
     }
 
     @DisplayName("getter는 각 필드의 값을 반환한다.")
     @Test
     void getterTest() {
-        Toy toy = new Toy.Builder(1L,
-                "키위새",
-                "어쩌구컴퍼니",
-                BigDecimal.valueOf(3000),
-                "https://somthing.com").build();
+        Toy toy = new Toy.Builder(TOY_NAME, TOY_MAKER, TOY_PRICE).image(TOY_IMG_URL).build();
 
         assertThat(toy.getId()).isEqualTo(1L);
-        assertThat(toy.getName()).isEqualTo("키위새");
-        assertThat(toy.getMaker()).isEqualTo("어쩌구컴퍼니");
-        assertThat(toy.getPrice()).isEqualTo(BigDecimal.valueOf(3000));
-        assertThat(toy.getImage()).isEqualTo("https://somthing.com");
+        assertThat(toy.getName()).isEqualTo(TOY_NAME);
+        assertThat(toy.getMaker()).isEqualTo(TOY_MAKER);
+        assertThat(toy.getPrice()).isEqualTo(TOY_PRICE);
+        assertThat(toy.getImage()).isEqualTo(TOY_IMG_URL);
     }
 
 }
