151886ff56f0c17442ff02bda654fd0989bfe3b2
shineoov <shineoov88@gmail.com>
2022-04-11 18:14:22 +0900

    feat: 고양이 장난감 엔티티, 리포지토리 작성

---
+++ app/src/main/java/com/codesoom/assignment/domain/CatToy.java
@@ -0,0 +1,22 @@
+package com.codesoom.assignment.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class CatToy {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String maker;
+
+    private Integer price;
+
+    private String imagePath;
+
+    public CatToy() {
+    }
+}
---
+++ app/src/main/java/com/codesoom/assignment/domain/CatToyRepository.java
@@ -0,0 +1,6 @@
+package com.codesoom.assignment.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CatToyRepository extends CrudRepository<CatToy, Long> {
+}
