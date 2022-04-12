2094fb23d0850bb3837ae92e95f6ecd5f64dcd40
pjkfckr <pjkfckr@gmail.com>
2022-04-12 12:49:52 +0900

    모든 Product를 조회하는 findAll 메소드에 대한 테스트

--- app/src/test/java/com/codesoom/assignment/domain/ProductRepositoryTest.java
+++ app/src/test/java/com/codesoom/assignment/domain/ProductRepositoryTest.java
@@ -3,10 +3,13 @@ package com.codesoom.assignment.domain;
 import com.codesoom.assignment.ProductNotFoundException;
 import com.codesoom.assignment.infra.InMemoryProductRepository;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -26,11 +29,77 @@ class ProductRepositoryTest {
     }
 
     @Nested
-    @DisplayName("상품을 조회할 때")
-    class Describe_read_of_product {
+    @DisplayName("findAll 메소드를 호출할 때")
+    class Describe_readAll_of_product {
 
         @Nested
         @DisplayName("상품이 존재할 경우")
+        class Context_with_products {
+            final long createProductSize = 4L;
+
+            @BeforeEach
+            void setUp() {
+                for (int i = 0; i < createProductSize; i++) {
+                    Product product = new Product();
+                    product.setName(PRODUCT_NAME);
+                    product.setMaker(PRODUCT_MAKER);
+                    product.setPrice(PRODUCT_PRICE);
+                    product.setImageUrl(PRODUCT_IMAGE_URL);
+                    productRepository.save(product);
+                }
+            }
+
+            @AfterEach
+            void tearDown() {
+                productRepository.deleteAll();
+            }
+
+            @Test
+            @DisplayName("상품이 존재하는 배열을 리턴합니다")
+            void it_returns_list_of_product() {
+                List<Product> products = productRepository.findAll();
+
+                assertThat(products).isNotEmpty();
+                assertThat(products).hasSize((int) createProductSize);
+            }
+        }
+
+        @Nested
+        @DisplayName("상품이 존재하지 않을 경우")
+        class Context_with_empty_list {
+            final long createProductSize = 3L;
+
+            @BeforeEach
+            void setUp() {
+                for (int i = 0; i < createProductSize; i++) {
+                    Product product = new Product();
+                    product.setName(PRODUCT_NAME);
+                    product.setMaker(PRODUCT_MAKER);
+                    product.setPrice(PRODUCT_PRICE);
+                    product.setImageUrl(PRODUCT_IMAGE_URL);
+                    productRepository.save(product);
+                }
+                productRepository.deleteAll();
+            }
+
+
+            @Test
+            @DisplayName("빈 배열을 반환합니다")
+            void it_return_empty_list() {
+                List<Product> products = productRepository.findAll();
+
+                assertThat(products).isEmpty();
+                assertThat(products).hasSize(0);
+            }
+        }
+    }
+
+   @Nested
+    @DisplayName("findById 메소드를 호출할 때")
+    class Describe_read_of_product {
+
+        @Nested
+        @DisplayName("Id 에 맞는 상품이 존재할 경우")
         class Context_with_product {
 
             @BeforeEach
@@ -49,7 +118,7 @@ class ProductRepositoryTest {
             }
 
             @Test
-            @DisplayName("ID 로 상품을 조회할수 있다")
+            @DisplayName("상품을 반환한다")
             void it_return_product() {
                 Product product = productRepository.findById(1L)
                         .orElseThrow(() -> new ProductNotFoundException(1L));
@@ -57,31 +126,30 @@ class ProductRepositoryTest {
                 assertThat(product).isNotNull();
             }
         }
-    }
+        @Nested
+        @DisplayName("Id 에 맞는 상품이 존재하지 않을 경우")
+        class Context_without_product {
+            final long createProductSize = 3L;
 
-    @Nested
-    @DisplayName("상품이 존재하지 않을 경우")
-    class Context_without_product {
-        final long createProductSize = 3L;
-
-        @BeforeEach
-        void setUp() {
-            for (long i = 0; i < createProductSize; i++) {
-                Product product = new Product();
-                product.setName(PRODUCT_NAME);
-                product.setMaker(PRODUCT_MAKER);
-                product.setPrice(PRODUCT_PRICE);
-                product.setImageUrl(PRODUCT_IMAGE_URL);
-                productRepository.save(product);
+            @BeforeEach
+            void setUp() {
+                for (long i = 0; i < createProductSize; i++) {
+                    Product product = new Product();
+                    product.setName(PRODUCT_NAME);
+                    product.setMaker(PRODUCT_MAKER);
+                    product.setPrice(PRODUCT_PRICE);
+                    product.setImageUrl(PRODUCT_IMAGE_URL);
+                    productRepository.save(product);
+                }
+                productRepository.deleteAll();
             }
-            productRepository.deleteAll();
-        }
 
-        @Test
-        @DisplayName("ProductNotFoundException을 던진다")
-        void it_throw_productNotFoundException() {
-            assertThatThrownBy(() -> productRepository.findById(1L))
-                    .isInstanceOf(ProductNotFoundException.class);
+            @Test
+            @DisplayName("ProductNotFoundException을 던진다")
+            void it_throw_productNotFoundException() {
+                assertThatThrownBy(() -> productRepository.findById(1L))
+                        .isInstanceOf(ProductNotFoundException.class);
+            }
         }
     }
 }
