687b5e8719d61b0886a000bb43c6de6cfb781b17
5Hanui <gkdud644@naver.com>
2022-04-13 16:29:58 +0900

    아이디로 Product를 조회하는 메소드 추가

--- app/src/main/java/com/codesoom/assignment/domain/ProductRepository.java
+++ app/src/main/java/com/codesoom/assignment/domain/ProductRepository.java
@@ -3,6 +3,9 @@ package com.codesoom.assignment.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    Optional<Product> findById(Long id);
 }
