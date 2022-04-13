38c97c6a7333edc77372d07f92f9031b1fc8a45e
shineoov <shineoov88@gmail.com>
2022-04-13 16:31:58 +0900

    feat: ProductController @ExceptionHandler 적용
    - @ExceptionHandler 별도로 관리하기 위해 ControllerAdvice 추가

---
+++ app/src/main/java/com/codesoom/assignment/advice/ProductControllerAdvice.java
@@ -0,0 +1,20 @@
+package com.codesoom.assignment.advice;
+
+import com.codesoom.assignment.controller.ProductController;
+import com.codesoom.assignment.exception.ProductNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * 상품에 대한 HTTP 요청 중 예외 발생 시 대신 응답합니다.
+ */
+@RestControllerAdvice(basePackageClasses = ProductController.class)
+public class ProductControllerAdvice {
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(ProductNotFoundException.class)
+    public void productNotFoundException() {
+    }
+}
