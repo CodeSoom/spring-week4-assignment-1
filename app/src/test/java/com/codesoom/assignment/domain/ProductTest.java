6a8348b14cc2684e02c18ffa8d44041ddbc25d91
5Hanui <gkdud644@naver.com>
2022-04-13 15:50:42 +0900

    modify 메소드 유닛 테스트를 작성

--- app/src/test/java/com/codesoom/assignment/domain/ProductTest.java
+++ app/src/test/java/com/codesoom/assignment/domain/ProductTest.java
@@ -1,5 +1,6 @@
 package com.codesoom.assignment.domain;
 
+import com.codesoom.assignment.dto.ProductDto;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,45 +13,69 @@ class ProductTest {
 
     private Product product;
 
+    private static final Long PRODUCT_TEST_ID = 1L;
+    private static final String TEST_PRODUCT_NAME = "NAME";
+    private static final String TEST_PRODUCT_MAKER = "MAKER";
+    private static final BigDecimal TEST_PRODUCT_PRICE = BigDecimal.TEN;
+    private static final String TEST_PRODUCT_IMAGE_URL = "/image.png";
+
     @BeforeEach
     void setUp() {
         product = new Product.ProductBuilder()
-                            .id(1L)
-                            .name("이름")
-                            .maker("메이커")
-                            .price(BigDecimal.valueOf(1000))
-                            .imageUrl("imgURL")
+                            .id(PRODUCT_TEST_ID)
+                            .name(TEST_PRODUCT_NAME)
+                            .maker(TEST_PRODUCT_MAKER)
+                            .price(TEST_PRODUCT_PRICE)
+                            .imageUrl(TEST_PRODUCT_IMAGE_URL)
                             .build();
     }
 
     @Test
     @DisplayName("getId는 상품의 아이디를 반환한다.")
     void getId() {
-        assertThat(product.getId()).isEqualTo(1L);
+        assertThat(product.getId()).isEqualTo(PRODUCT_TEST_ID);
     }
 
     @Test
     @DisplayName("getName는 상품의 이름을 반환한다.")
     void getName() {
-        assertThat(product.getName()).isEqualTo("이름");
+        assertThat(product.getName()).isEqualTo(TEST_PRODUCT_NAME);
     }
 
     @Test
     @DisplayName("getMaker는 상품의 제작자를 반환한다.")
     void getMaker() {
-        assertThat(product.getMaker()).isEqualTo("메이커");
+        assertThat(product.getMaker()).isEqualTo(TEST_PRODUCT_MAKER);
     }
 
     @Test
     @DisplayName("getPrice는 상품의 가격을 반환한다.")
     void getPrice() {
-        assertThat(product.getPrice()).isEqualTo(BigDecimal.valueOf(1000));
+        assertThat(product.getPrice()).isEqualTo(TEST_PRODUCT_PRICE);
     }
 
     @Test
     @DisplayName("getImageUrl는 상품의 이미지 주소를 반환한다.")
     void getImageUrl() {
-        assertThat(product.getImageUrl()).isEqualTo("imgURL");
+        assertThat(product.getImageUrl()).isEqualTo(TEST_PRODUCT_IMAGE_URL);
     }
 
+    @Test
+    @DisplayName("modify 메소드는 ProductDto를 받아 상품의 각 필드를 수정한다.")
+    void modify() {
+
+        ProductDto productDto = new ProductDto.ProductDtoBuilder()
+                .name("modify name")
+                .maker("modify maker")
+                .price(BigDecimal.ONE)
+                .imageUrl("/dir/test.png")
+                .build();
+
+        product.modify(productDto);
+
+        assertThat(product.getName()).isEqualTo(productDto.getName());
+        assertThat(product.getMaker()).isEqualTo(productDto.getMaker());
+        assertThat(product.getPrice()).isEqualTo(productDto.getPrice());
+        assertThat(product.getImageUrl()).isEqualTo(productDto.getImageUrl());
+    }
 }
