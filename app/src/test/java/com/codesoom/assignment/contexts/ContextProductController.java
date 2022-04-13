80d77149bf456519db8b770e5267c33aa3176b9b
minhee.son <minhee4735@gmail.com>
2022-04-14 05:18:03 +0900

    refactor Test를 위한 Context 들간 의존관계 설정 (Controller -> Service -> Repository -> Product)

--- app/src/test/java/com/codesoom/assignment/contexts/ContextProductController.java
+++ app/src/test/java/com/codesoom/assignment/contexts/ContextProductController.java
@@ -1,22 +1,15 @@
 package com.codesoom.assignment.contexts;
 
 import com.codesoom.assignment.controllers.ProductController;
-import com.codesoom.assignment.services.ProductService;
 import org.junit.jupiter.api.BeforeEach;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-@SpringBootTest
 @AutoConfigureMockMvc
-public abstract class ContextProductController {
+public abstract class ContextProductController extends ContextProductService {
 
     protected MockMvc mockMvc;
-
-    @Autowired
-    protected ProductService productService;
 
     @BeforeEach
     void setUp() {
--- app/src/test/java/com/codesoom/assignment/contexts/ContextProductService.java
+++ app/src/test/java/com/codesoom/assignment/contexts/ContextProductService.java
@@ -1,15 +1,9 @@
 package com.codesoom.assignment.contexts;
 
-import com.codesoom.assignment.repositories.ProductRepository;
 import com.codesoom.assignment.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-public abstract class ContextProductService extends ContextProduct {
-
-    @Autowired
-    private ProductRepository productRepository;
+public abstract class ContextProductService extends ContextProductRepository {
 
     @Autowired
     protected ProductService productService;
