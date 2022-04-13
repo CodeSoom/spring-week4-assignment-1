faedbcab81be1fb7f81ac9e95f3da04441cd31dc
5Hanui <gkdud644@naver.com>
2022-04-13 17:43:06 +0900

    상품의 수정, 삭제, 조회 등을 수행하는 ProductService 추가

--- app/src/main/java/com/codesoom/assignment/application/ProductService.java
+++ app/src/main/java/com/codesoom/assignment/application/ProductService.java
@@ -1,20 +1,14 @@
 package com.codesoom.assignment.application;
 
+import com.codesoom.assignment.common.exception.ProductNotFoundException;
 import com.codesoom.assignment.domain.Product;
 import com.codesoom.assignment.domain.ProductRepository;
+import com.codesoom.assignment.dto.ProductDto;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import java.util.List;
+import java.util.Optional;
 
-
-/** TODO
- 1. get all product
- 2. get product by id
- 3. create product
- 4. update product by id
- 5. delete product by id
-*/
 @Service
 @Transactional
 public class ProductService {
@@ -26,7 +20,30 @@ public class ProductService {
     }
 
     @Transactional(readOnly = true)
-    public List<Product> getList() {
+    public List<Product> getProducts() {
         return productRepository.findAll();
+    }
+
+    @Transactional(readOnly = true)
+    public Product getProductById(Long id) {
+        Optional<Product> foundProduct = productRepository.findById(id);
+        if(foundProduct.isEmpty()) throw new ProductNotFoundException(id);
+        return foundProduct.get();
+    }
+
+    public Product create(ProductDto productDto) {
+        Product newProduct = productDto.toEntity();
+        return productRepository.save(newProduct);
+    }
+
+    public Product updateProduct(Long id, ProductDto productDto) {
+        Product product = getProductById(id);
+        product.modify(productDto);
+        return productRepository.save(product);
+    }
+
+    public void deleteProduct(Long id) {
+        Product product = getProductById(id);
+        productRepository.delete(product);
     }
 }
