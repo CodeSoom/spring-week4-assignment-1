13b8d4305f72d073f27b40319a61444947d63790
shineoov <shineoov88@gmail.com>
2022-04-14 18:08:13 +0900

    feat: ProductCommandService replace, update 메소드 구현

--- app/src/main/java/com/codesoom/assignment/application/ProductCommandService.java
+++ app/src/main/java/com/codesoom/assignment/application/ProductCommandService.java
@@ -2,12 +2,15 @@ package com.codesoom.assignment.application;
 
 import com.codesoom.assignment.domain.Product;
 import com.codesoom.assignment.domain.ProductRepository;
+import com.codesoom.assignment.domain.ProductUpdateRequest;
 import com.codesoom.assignment.dto.ProductSaveRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * 상품 정보의 변경을 책임집니다.
  */
+@Transactional
 @Service
 public class ProductCommandService {
 
@@ -28,10 +31,34 @@ public class ProductCommandService {
     }
 
     /**
+     * 상품을 교체하고 리턴합니다.
+     * @param product 교체 대상 상품
+     * @param source 교체될 상품 데이터
+     */
+    public Product replaceProduct(final Product product, final ProductUpdateRequest source) {
+
+        product.replace(source);
+
+        return product;
+    }
+
+    /**
+     * 상품을 변경하고 리턴합니다.
+     * @param product 변경 대상 상품
+     * @param source 변경할 상품 데이터
+     */
+    public Product updateProduct(final Product product, final ProductUpdateRequest source) {
+
+        product.update(source);
+
+        return product;
+    }
+
+    /**
      * 상품을 삭제 합니다.
      * @param product 삭제할 상품
      */
-    public void deleteProduct(Product product) {
+    public void deleteProduct(final Product product) {
         productRepository.delete(product);
     }
 }
--- app/src/test/java/com/codesoom/assignment/application/ProductCommandServiceTest.java
+++ app/src/test/java/com/codesoom/assignment/application/ProductCommandServiceTest.java
@@ -2,6 +2,7 @@ package com.codesoom.assignment.application;
 
 import com.codesoom.assignment.domain.Product;
 import com.codesoom.assignment.domain.ProductRepository;
+import com.codesoom.assignment.domain.ProductUpdateRequest;
 import com.codesoom.assignment.dto.ProductSaveRequest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -23,6 +24,11 @@ class ProductCommandServiceTest {
     private static final String TEST_PRODUCT_MAKER = "MAKER";
     private static final Integer TEST_PRODUCT_PRICE = 1000;
     private static final String TEST_PRODUCT_IMAGE_PATH = "/image/test.jpg";
+
+    private final String TEST_PRODUCT_UPDATE_NAME = "고양이장난감-1";
+    private final String TEST_PRODUCT_UPDATE_MAKER = "APPLE-1";
+    private final Integer TEST_PRODUCT_UPDATE_PRICE = 11000;
+    private final String TEST_PRODUCT_UPDATE_IMAGE_PATH = "/images/toy-1.png";
 
     ProductCommandService productCommandService;
 
@@ -78,6 +84,123 @@ class ProductCommandServiceTest {
                         () -> assertThat(product.getMaker()).isEqualTo(TEST_PRODUCT_MAKER),
                         () -> assertThat(product.getPrice()).isEqualTo(TEST_PRODUCT_PRICE),
                         () -> assertThat(product.getImagePath()).isEqualTo(TEST_PRODUCT_IMAGE_PATH)
+                );
+            }
+        }
+    }
+
+    @Nested
+    @DisplayName("replaceProduct 메소드는")
+    class Describe_replaceProduct {
+
+        @Nested
+        @DisplayName("대체할 상품이 저장소에 있다면")
+        class Context_existsProduct {
+
+            final Product product = new Product(
+                    TEST_PRODUCT_NAME, TEST_PRODUCT_MAKER, TEST_PRODUCT_PRICE, TEST_PRODUCT_IMAGE_PATH);
+
+            final ProductUpdateRequest updateSource = new ProductUpdateRequest() {
+                @Override
+                public String getName() {
+                    return TEST_PRODUCT_UPDATE_NAME;
+                }
+
+                @Override
+                public String getMaker() {
+                    return TEST_PRODUCT_UPDATE_MAKER;
+                }
+
+                @Override
+                public Integer getPrice() {
+                    return TEST_PRODUCT_UPDATE_PRICE;
+                }
+
+                @Override
+                public String getImagePath() {
+                    return TEST_PRODUCT_UPDATE_IMAGE_PATH;
+                }
+            };
+
+            Long productId;
+
+            @BeforeEach
+            void setUp() {
+                productRepository.save(product);
+                productId = product.getId();
+            }
+
+            @Test
+            @DisplayName("상품 정보를 대체하고 리턴한다.")
+            void it_replace_and_return_product() {
+
+                final Product replacedProduct = productCommandService.replaceProduct(product, updateSource);
+
+                assertAll(
+                        () -> assertThat(replacedProduct.getId()).isEqualTo(productId),
+                        () -> assertThat(replacedProduct.getName()).isEqualTo(TEST_PRODUCT_UPDATE_NAME),
+                        () -> assertThat(replacedProduct.getMaker()).isEqualTo(TEST_PRODUCT_UPDATE_MAKER),
+                        () -> assertThat(replacedProduct.getPrice()).isEqualTo(TEST_PRODUCT_UPDATE_PRICE),
+                        () -> assertThat(replacedProduct.getImagePath()).isEqualTo(TEST_PRODUCT_UPDATE_IMAGE_PATH)
+                );
+            }
+        }
+    }
+
+
+    @Nested
+    @DisplayName("updateProduct 메소드는")
+    class Describe_updateProduct {
+
+        @Nested
+        @DisplayName("변경할 상품이 저장소에 있다면")
+        class Context_existsProduct {
+
+            final Product product = new Product(
+                    TEST_PRODUCT_NAME, TEST_PRODUCT_MAKER, TEST_PRODUCT_PRICE, TEST_PRODUCT_IMAGE_PATH);
+
+            final ProductUpdateRequest updateSource = new ProductUpdateRequest() {
+                @Override
+                public String getName() {
+                    return TEST_PRODUCT_UPDATE_NAME;
+                }
+
+                @Override
+                public String getMaker() {
+                    return TEST_PRODUCT_UPDATE_MAKER;
+                }
+
+                @Override
+                public Integer getPrice() {
+                    return null;
+                }
+
+                @Override
+                public String getImagePath() {
+                    return "";
+                }
+            };
+
+            Long productId;
+
+            @BeforeEach
+            void setUp() {
+                productRepository.save(product);
+                productId = product.getId();
+            }
+
+            @Test
+            @DisplayName("상품 정보를 변경하고 리턴한다.")
+            void it_update_and_return_product() {
+
+                final Product updatedProduct = productCommandService.updateProduct(product, updateSource);
+
+                assertAll(
+                        () -> assertThat(updatedProduct.getId()).isEqualTo(productId),
+                        () -> assertThat(updatedProduct.getName()).isEqualTo(TEST_PRODUCT_UPDATE_NAME),
+                        () -> assertThat(updatedProduct.getMaker()).isEqualTo(TEST_PRODUCT_UPDATE_MAKER),
+                        () -> assertThat(updatedProduct.getPrice()).isEqualTo(TEST_PRODUCT_PRICE),
+                        () -> assertThat(updatedProduct.getImagePath()).isEqualTo(TEST_PRODUCT_IMAGE_PATH)
                 );
             }
         }
