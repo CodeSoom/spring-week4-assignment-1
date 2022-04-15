db3e448802b0d20a2434789a78e37707b0c69b54
Jeon Gyedong <jeongyedong@gmail.com>
2022-04-15 22:05:05 +0900

    CLN: import한 클래스 중 쓰지 않는 것 삭제

--- app/src/main/java/com/codesoom/assignment/service/ProductService.java
+++ app/src/main/java/com/codesoom/assignment/service/ProductService.java
@@ -3,13 +3,10 @@ package com.codesoom.assignment.service;
 import com.codesoom.assignment.ProductNotFoundException;
 import com.codesoom.assignment.domain.Product;
 import com.codesoom.assignment.repository.ProductRepository;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.client.HttpServerErrorException;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 @Transactional
