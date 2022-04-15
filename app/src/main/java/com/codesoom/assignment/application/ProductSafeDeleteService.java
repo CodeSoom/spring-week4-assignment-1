4d9a413d4b0c65630f839d2ced01417a0b99c246
SUBIN606 <gaemilee606@gmail.com>
2022-04-15 22:48:11 +0900

    feat: 상품 삭제 컨트롤러
    id로 상품을 삭제할 수 있는 컨트롤러를 추가합니다.

--- app/src/main/java/com/codesoom/assignment/application/ProductSafeDeleteService.java
+++ app/src/main/java/com/codesoom/assignment/application/ProductSafeDeleteService.java
@@ -8,7 +8,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 
 @RequiredArgsConstructor
-@Service
 public class ProductSafeDeleteService implements ProductDeleteService {
 
     private final ProductRepository repository;
