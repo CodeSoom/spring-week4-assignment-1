08f2ef7e23b307d22c3a7b092047dea4f2375648
subin606 <gaemilee606@gmail.com>
2022-04-15 17:11:15 +0900

    feat: findAll 반환 형식을 List로 변경

--- app/src/main/java/com/codesoom/assignment/domain/ProductRepository.java
+++ app/src/main/java/com/codesoom/assignment/domain/ProductRepository.java
@@ -2,5 +2,11 @@ package com.codesoom.assignment.domain;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends CrudRepository<Product, Long> {
+
+    @Override
+    List<Product> findAll();
+
 }
