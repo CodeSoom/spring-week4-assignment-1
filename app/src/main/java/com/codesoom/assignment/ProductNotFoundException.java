53abf217a38ef05ff86b7461a633a1079712e585
Jeon Gyedong <jeongyedong@gmail.com>
2022-04-15 21:18:22 +0900

    ENH: ProductNotFoundException 클래스 추가

---
+++ app/src/main/java/com/codesoom/assignment/ProductNotFoundException.java
@@ -0,0 +1,7 @@
+package com.codesoom.assignment;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Product Not Found")
+public class ProductNotFoundException extends RuntimeException{ }
