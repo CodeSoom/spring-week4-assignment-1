3e83a6f82656754b52abe684f062e69e1fa6035b
n00nietzsche <n00nietzsche@gmail.com>
2022-04-16 18:12:45 +0900

    ProductControllerTest 생성

--- app/src/test/java/com/codesoom/assignment/controller/ProductControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controller/ProductControllerTest.java
@@ -1,15 +1,10 @@
 package com.codesoom.assignment.controller;
 
 import com.codesoom.assignment.application.ProductService;
+import org.springframework.stereotype.Controller;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProductControllerTest {
-
-    private final ProductService service;
-
-    public ProductControllerTest(ProductService service) {
-        this.service = service;
-    }
 
 }
