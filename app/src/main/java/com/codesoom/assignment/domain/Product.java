1a907505562319b4519b26595bf16d7d387946c4
n00nietzsche <n00nietzsche@gmail.com>
2022-04-16 16:15:53 +0900

    도메인 정의

--- app/src/main/java/com/codesoom/assignment/domain/Product.java
+++ app/src/main/java/com/codesoom/assignment/domain/Product.java
@@ -1,4 +1,20 @@
 package com.codesoom.assignment.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter @Setter
 public class Product {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+    private String maker;
+    private int price;
+    private String image;
 }
