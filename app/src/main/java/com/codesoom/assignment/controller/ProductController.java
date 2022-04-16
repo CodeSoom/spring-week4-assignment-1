83ebb96c7c4d6e2b49bddc7f6427d1601028c676
n00nietzsche <n00nietzsche@gmail.com>
2022-04-16 18:25:43 +0900

    TEST 를 충족시키는 controller 코드 작성

--- app/src/main/java/com/codesoom/assignment/controller/ProductController.java
+++ app/src/main/java/com/codesoom/assignment/controller/ProductController.java
@@ -1,8 +1,11 @@
 package com.codesoom.assignment.controller;
 
 import com.codesoom.assignment.application.ProductService;
+import com.codesoom.assignment.domain.Product;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/products")
@@ -11,5 +14,9 @@ public class ProductController {
 
     public ProductController(ProductService service) {
         this.service = service;
+    }
+
+    public List<Product> list() {
+        return service.getProducts();
     }
 }
