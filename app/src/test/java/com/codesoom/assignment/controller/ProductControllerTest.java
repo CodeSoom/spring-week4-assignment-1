3857d9de489261f49cf5c2eb145ca3910c59f374
n00nietzsche <n00nietzsche@gmail.com>
2022-04-16 18:35:11 +0900

    투두 목록 추가

--- app/src/test/java/com/codesoom/assignment/controller/ProductControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controller/ProductControllerTest.java
@@ -16,6 +16,12 @@ import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
+// DONE: GET /tasks 여러 건 조회
+// DONE: GET /task 단 건 조회
+// TODO: POST /task 등록
+// TODO: PATCH /task 수정
+// TODO: DELETE /task 삭제
+
 class ProductControllerTest {
 
     ProductService service;
