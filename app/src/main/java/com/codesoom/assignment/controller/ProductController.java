3bfd12c969636334c7bed3569da61329f6a15dbc
n00nietzsche <n00nietzsche@gmail.com>
2022-04-16 15:40:10 +0900

    기본 레이어 생성

---
+++ app/src/main/java/com/codesoom/assignment/controller/ProductController.java
@@ -0,0 +1,10 @@
+package com.codesoom.assignment.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+}
