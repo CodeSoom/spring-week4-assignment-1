9d276e09e3872700a00c82cfbcad2cb22c7c0dcc
n00nietzsche <n00nietzsche@gmail.com>
2022-04-17 11:16:08 +0900

    getGreeting() 등 초기에 샘플로 작성된 쓸모 없는 코드 제거

--- app/src/main/java/com/codesoom/assignment/App.java
+++ app/src/main/java/com/codesoom/assignment/App.java
@@ -1,14 +1,12 @@
 package com.codesoom.assignment;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class App {
-    public String getGreeting() {
-        return "Hello, world!";
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
