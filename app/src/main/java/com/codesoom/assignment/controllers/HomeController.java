27e98b132e1d7d9cf484e75cfa49539e3cfe90fa
MJbae <bmj5208@gmail.com>
2022-05-23 16:13:36 +0900

    feat: home("/") 경로에 대한 GET 요청을 처리하는 HomeController 구현

---
+++ app/src/main/java/com/codesoom/assignment/controllers/HomeController.java
@@ -0,0 +1,20 @@
+package com.codesoom.assignment.controllers;
+
+import com.codesoom.assignment.interfaces.Shop;
+import com.codesoom.assignment.models.ToyShop;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    private final Shop shop;
+
+    public HomeController() {
+        shop = new ToyShop("고양이 장난감 가게");
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return shop.name();
+    }
+}
