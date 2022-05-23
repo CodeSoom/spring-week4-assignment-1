0b6196f81247a91e81a700865bc99bf66c262290
caoyu-dev <choyumin01@gmail.com>
2022-05-24 01:11:15 +0900

    feat: Product Entity 생성

---
+++ app/src/main/java/com/codesoom/assignment/domain/Product.java
@@ -0,0 +1,67 @@
+package com.codesoom.assignment.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+    private String maker;
+    private Integer price;
+    private String imageURI;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMaker() {
+        return maker;
+    }
+
+    public void setMaker(String maker) {
+        this.maker = maker;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public String getImageURI() {
+        return imageURI;
+    }
+
+    public void setImageURI(String imageURI) {
+        this.imageURI = imageURI;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", maker='" + maker + '\'' +
+                ", price=" + price +
+                ", imageURI='" + imageURI + '\'' +
+                '}';
+    }
+}
