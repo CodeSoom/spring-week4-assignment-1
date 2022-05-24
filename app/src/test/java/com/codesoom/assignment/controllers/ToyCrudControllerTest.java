5e8899d790de470dc0cab4d9736eca4181868099
MJbae <bmj5208@gmail.com>
2022-05-24 11:28:15 +0900

    test: Repository에 의존하지 않고 Service를 mocking하도록 테스트 수정

--- app/src/test/java/com/codesoom/assignment/controllers/ToyCrudControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/ToyCrudControllerTest.java
@@ -1,25 +1,29 @@
 package com.codesoom.assignment.controllers;
 
-import com.codesoom.assignment.application.ToyCrudService;
 import com.codesoom.assignment.interfaces.CrudController;
 import com.codesoom.assignment.interfaces.Product;
 import com.codesoom.assignment.interfaces.CrudService;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.LinkedList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
 
+@ExtendWith(MockitoExtension.class)
 @DisplayName("ToyCrudController 클래스")
 class ToyCrudControllerTest {
+    @Mock
     private CrudService service;
     private CrudController controller;
     private final List<Product> toys = new LinkedList<>();
 
     @BeforeEach
     void setUp() {
-        service = new ToyCrudService();
         controller = new ToyCrudController(service);
     }
 
@@ -37,6 +41,7 @@ class ToyCrudControllerTest {
             @BeforeEach
             void setUp() {
                 toys.clear();
+                given(service.showAll()).willReturn(toys);
             }
 
             @Test
