b6b78672b73cbc8de9391405aa89ea7614aea35f
MJbae <bmj5208@gmail.com>
2022-05-24 12:35:05 +0900

    feat: ToyCrudService::showAll 구현

--- app/src/main/java/com/codesoom/assignment/application/ToyCrudService.java
+++ app/src/main/java/com/codesoom/assignment/application/ToyCrudService.java
@@ -2,16 +2,20 @@ package com.codesoom.assignment.application;
 
 import com.codesoom.assignment.interfaces.CrudService;
 import com.codesoom.assignment.interfaces.Product;
+import com.codesoom.assignment.interfaces.ProductRepository;
 import org.springframework.stereotype.Service;
 
-import java.util.LinkedList;
 import java.util.List;
 
 @Service
 public class ToyCrudService implements CrudService {
+    private final ProductRepository repository;
+    public ToyCrudService(ProductRepository repository){
+        this.repository = repository;
+    }
 
     @Override
     public List<Product> showAll() {
-        return new LinkedList<>();
+        return repository.findAll();
     }
 }
