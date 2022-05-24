bc6f9bbc8923aed4ac2c8abb59d57cf143faa106
seunglo <devseung9056@gmail.com>
2022-05-24 09:29:31 +0900

    Product 도메인을 생성을 구현해라

---
+++ app/src/main/java/com/codesoom/assignment/domain/Product.java
@@ -0,0 +1,32 @@
+package com.codesoom.assignment.domain;
+
+public class Product {
+
+    private String name;
+    private String maker;
+    private int price;
+    private String imageUrl;
+
+    public Product(String name, String maker, int price, String imageUrl) {
+        this.name = name;
+        this.maker = maker;
+        this.price = price;
+        this.imageUrl = imageUrl;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getMaker() {
+        return maker;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+}
