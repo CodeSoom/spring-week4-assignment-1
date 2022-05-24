d472808f8919bab650f7eb3bbc1a62116033238d
seunglo <devseung9056@gmail.com>
2022-05-24 10:23:18 +0900

    Product update 메서드 테스트를 작성해라

--- app/src/test/java/com/codesoom/assignment/domain/ProductTest.java
+++ app/src/test/java/com/codesoom/assignment/domain/ProductTest.java
@@ -1,6 +1,8 @@
 package com.codesoom.assignment.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -20,5 +22,31 @@ public class ProductTest {
         assertThat(product.getMaker()).isEqualTo(MAKER);
         assertThat(product.getPrice()).isEqualTo(PRICE);
         assertThat(product.getImageUrl()).isEqualTo(IMAGE_URL);
+    }
+
+    @Nested
+    @DisplayName("update 메소드는")
+    class Describe_update {
+        private Product product;
+        private static final String UPDATE_NAME = "캣닢";
+        private static final String UPDATE_MAKER = "캣드러그";
+        private static final int UPDATE_PRICE = 7000;
+        private static final String UPDATE_IMAGE_URL = "https://www.catdrug.com/images/1234567";
+
+        @BeforeEach
+        void setUp() {
+            product = new Product(NAME, MAKER, PRICE, IMAGE_URL);
+        }
+
+        @Test
+        @DisplayName("product의 값을 변경한다.")
+        void it_updates_product() {
+            product.update(UPDATE_NAME, UPDATE_MAKER, UPDATE_PRICE, UPDATE_IMAGE_URL);
+
+            assertThat(product.getName()).isEqualTo(UPDATE_NAME);
+            assertThat(product.getMaker()).isEqualTo(UPDATE_MAKER);
+            assertThat(product.getPrice()).isEqualTo(UPDATE_PRICE);
+            assertThat(product.getImageUrl()).isEqualTo(UPDATE_IMAGE_URL);
+        }
     }
 }
