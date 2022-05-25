ea07a1fe399b404e5a4d3a4e9229021a7efe4f02
minjoon_lee <minjoon1995@naver.com>
2022-05-25 23:44:14 +0900

    create : Product Entity 생성

---
+++ app/src/main/java/com/codesoom/assignment/model/Product.java
@@ -0,0 +1,48 @@
+package com.codesoom.assignment.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import com.codesoom.assignment.dto.ProductDTO;
+
+import lombok.Getter;
+
+@Entity
+@Getter
+public class Product {
+	@Id
+	@GeneratedValue
+	private int id;
+
+	@Column(name = "name")
+	private String name;
+
+	@Column(name = "price")
+	private int price;
+
+	@Column(name = "image_url")
+	private String imageUrl;
+
+	@Column(name = "maker")
+	private String maker;
+
+	public Product() {
+	}
+
+	public Product(ProductDTO.CreateProduct CreateProduct) {
+		this.name = CreateProduct.getName();
+		this.price = CreateProduct.getPrice();
+		this.imageUrl = CreateProduct.getImageUrl();
+		this.maker = CreateProduct.getMaker();
+	}
+
+	public void updateProduct(ProductDTO.UpdateProduct updateProduct) {
+		this.name = updateProduct.getName();
+		this.price = updateProduct.getPrice();
+		this.imageUrl = updateProduct.getImageUrl();
+		this.maker = updateProduct.getMaker();
+	}
+}
+
