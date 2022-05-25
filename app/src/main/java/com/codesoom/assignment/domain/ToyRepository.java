6875d7e7a2a7f72d6f4dfb399cc371e07272a89e
caoyu-dev <choyumin01@gmail.com>
2022-05-25 17:22:40 +0900

    feat: ToyRepository 생성

---
+++ app/src/main/java/com/codesoom/assignment/domain/ToyRepository.java
@@ -0,0 +1,41 @@
+package com.codesoom.assignment.domain;
+
+import com.codesoom.assignment.interfaces.ProductRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class ToyRepository implements ProductRepository {
+    private final List<Product> products = new ArrayList<>();
+    private Long newId = 0L;
+
+    @Override
+    public List<Product> findAll() {
+        return products;
+    }
+
+    @Override
+    public Optional<Product> findById(Long id) {
+        return products.stream()
+                .filter(product -> product.getId().equals(id))
+                .findFirst();
+    }
+
+    @Override
+    public Product save(Product product) {
+        return null;
+    }
+
+    @Override
+    public Product update(Long id, Product product) {
+        return null;
+    }
+
+    @Override
+    public Product delete(Product product) {
+        return null;
+    }
+}
