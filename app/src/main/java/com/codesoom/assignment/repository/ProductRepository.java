7c763b9a59007c1b0aba08fc7e567d4ede6fcd89
minjoon_lee <minjoon1995@naver.com>
2022-05-25 23:44:31 +0900

    create : Product Repository 생성

---
+++ app/src/main/java/com/codesoom/assignment/repository/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.codesoom.assignment.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.codesoom.assignment.model.Product;
+
+public interface ProductRepository extends JpaRepository<Product,Integer> {
+}
