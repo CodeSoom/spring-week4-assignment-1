4976df878c1f35763056b615cd3be70abac83843
yongjunleeme <chesidyong@gmail.com>
2022-05-25 18:41:56 +0900

    Add getProducts in ProductService

--- app/src/test/java/com/codesoom/assignment/service/ProductServiceTest.java
+++ app/src/test/java/com/codesoom/assignment/service/ProductServiceTest.java
@@ -2,24 +2,30 @@ package com.codesoom.assignment.service;
 
 import com.codesoom.assignment.domain.Product;
 import com.codesoom.assignment.dto.ProductRequest;
+import com.codesoom.assignment.dto.ProductResponse;
 import com.codesoom.assignment.repository.ProductRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 class ProductServiceTest {
 
     private ProductRepository productRepository;
     private ProductService productService;
-
     private ProductRequest productRequest;
 
     private Product product1;
     private Product product2;
+    private List<Product> products;
 
     @BeforeEach
     void setUp() {
@@ -45,6 +51,8 @@ class ProductServiceTest {
         productRequest.setMaker("maker3");
         productRequest.setPrice(3);
         productRequest.setImageUrl("url3");
+
+        products = new ArrayList<>();
     }
 
     @Test
@@ -54,6 +62,23 @@ class ProductServiceTest {
                 .will(invocation -> invocation.getArgument(0));
 
         productService.createProduct(productRequest);
+
+        verify(productRepository).save(any(Product.class));
+    }
+
+    @Test
+    @DisplayName("모든 상품을 조회한다.")
+    void getProducts() {
+        products.add(product1);
+        products.add(product2);
+
+        given(productRepository.findAll()).willReturn(products);
+
+        List<ProductResponse> products = productService.getProducts();
+
+        verify(productRepository).findAll();
+
+        assertThat(products).hasSize(2);
     }
 
 }
