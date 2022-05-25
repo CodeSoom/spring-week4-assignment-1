ee1de5ce648ff891f5b2430b2d289e0bfc60a499
minjoon_lee <minjoon1995@naver.com>
2022-05-25 23:44:04 +0900

    create : Product 관련 static nested class DTO 들을 전부 담는 ProductDTO 생성

---
+++ app/src/main/java/com/codesoom/assignment/dto/ProductDTO.java
@@ -0,0 +1,47 @@
+package com.codesoom.assignment.dto;
+
+import com.codesoom.assignment.model.Product;
+
+import lombok.Getter;
+
+public class ProductDTO {
+
+	@Getter
+	public static class CreateProduct {
+		private String name;
+		private String maker;
+		private int price;
+		private String imageUrl;
+	}
+
+	@Getter
+	public static class UpdateProduct {
+		private String name;
+		private String maker;
+		private int price;
+		private String imageUrl;
+	}
+
+	@Getter
+	public static class Response {
+		private final int id;
+		private final String name;
+		private final String maker;
+		private final int price;
+		private final String imageUrl;
+
+		public Response(int id, String name, String maker, int price, String imageUrl) {
+			this.id = id;
+			this.name = name;
+			this.maker = maker;
+			this.price = price;
+			this.imageUrl = imageUrl;
+		}
+
+		public static Response of(Product product) {
+			return new Response(product.getId(), product.getName(), product.getMaker(), product.getPrice(),
+				product.getImageUrl());
+		}
+	}
+
+}
