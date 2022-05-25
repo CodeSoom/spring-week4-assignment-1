0587f665675f70af0055437d51d29148dca2ba0c
caoyu-dev <choyumin01@gmail.com>
2022-05-25 17:16:28 +0900

    feat: service interface

---
+++ app/src/main/java/com/codesoom/assignment/interfaces/ProductService.java
@@ -0,0 +1,19 @@
+package com.codesoom.assignment.interfaces;
+
+import com.codesoom.assignment.domain.Product;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductService {
+
+    List<Product> findProducts();
+
+    Product findProduct(Long id);
+
+    Product createProduct(Product product);
+
+    Product updateProduct(Long id, Product product);
+
+    Product deleteProduct(Product product);
+}
