4f019dacbf3e2ce2eef5116afab90de7c91dd39e
caoyu-dev <choyumin01@gmail.com>
2022-05-25 17:15:06 +0900

    feat: controller interface

---
+++ app/src/main/java/com/codesoom/assignment/interfaces/ProductController.java
@@ -0,0 +1,12 @@
+package com.codesoom.assignment.interfaces;
+
+import com.codesoom.assignment.domain.Product;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductController {
+    List<Product> getProducts();
+
+    Optional<Product> getProduct(Long id);
+}
