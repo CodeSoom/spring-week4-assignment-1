cd431beab612cd615f202039948438f8c0e77b85
10M <endlesslhk@gmail.com>
2022-05-26 16:21:18 +0900

    feat(ToyStoreControllerTest): 장난감 목록 조회 테스트 케이스 추가

--- app/src/test/java/com/codesoom/assignment/controller/ToyStoreControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controller/ToyStoreControllerTest.java
@@ -4,6 +4,7 @@ import static org.hamcrest.Matchers.containsString;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -13,6 +14,8 @@ import com.codesoom.assignment.model.Product;
 import com.codesoom.assignment.service.ToyStoreService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -54,6 +57,25 @@ public class ToyStoreControllerTest {
                 .andDo(print());
 
         verify(toyStoreService).save(any(Product.class));
+    }
+
+    @DisplayName("장난감 목록 조회 후 반환")
+    @Test
+    void getProductsTest() throws Exception {
+
+        List<Product> productsSource = new ArrayList<>();
+        productsSource.add(new Product(1L, "name", "maker", 10000, "abcdefg.jpg"));
+        productsSource.add(new Product(2L, "name", "maker", 10000, "abcdefg.jpg"));
+        productsSource.add(new Product(3L, "name", "maker", 10000, "abcdefg.jpg"));
+
+        given(toyStoreService.getProducts()).willReturn(productsSource);
+
+        mockMvc.perform(get("/products"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("1")))
+                .andDo(print());
+
+        verify(toyStoreService).getProducts();
     }
 
     private String productToString(Object source) throws JsonProcessingException {
