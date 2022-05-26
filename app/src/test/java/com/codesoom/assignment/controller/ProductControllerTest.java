c44b5424608d2963312e24d77fa4e002c57b5e14
hy43 <hyde0401@gmail.com>
2022-05-26 14:13:34 +0900

    test : 요구 사항을 위한 카테고리 작성(list, detail, regist, modify, delete)

---
+++ app/src/test/java/com/codesoom/assignment/controller/ProductControllerTest.java
@@ -0,0 +1,25 @@
+package com.codesoom.assignment.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("ProductController Test")
+class ProductControllerTest {
+
+    @Nested
+    @DisplayName("list")
+
+    @Nested
+    @DisplayName("detail")
+
+    @Nested
+    @DisplayName("regist")
+
+    @Nested
+    @DisplayName("modify")
+
+    @Nested
+    @DisplayName("delete")
+}
