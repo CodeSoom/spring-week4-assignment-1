e5d9e91b018ed0fa32d3c84c67f4cf39227671fc
minjoon_lee <minjoon1995@naver.com>
2022-05-26 17:33:25 +0900

    feature : ProductControllerTest 에서 postProductTest 코드 성공하도록 구현

--- app/src/main/java/com/codesoom/assignment/controller/ProductController.java
+++ app/src/main/java/com/codesoom/assignment/controller/ProductController.java
@@ -1,7 +1,11 @@
 package com.codesoom.assignment.controller;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.codesoom.assignment.dto.ProductDTO;
 import com.codesoom.assignment.service.ProductService;
 
 @RestController
@@ -11,5 +15,9 @@ public class ProductController {
 
 	public ProductController(ProductService productService) {
 		this.productService = productService;
+	}
+
+	public ResponseEntity<List<ProductDTO.Response>> getProducts() {
+		return ResponseEntity.ok(productService.getProducts());
 	}
 }
