e18f564ea21187f48ce0a8a0c40eb0697d70aadc
10M <endlesslhk@gmail.com>
2022-05-26 15:37:48 +0900

    feat(AppTest): App 테스트 및 Product모델 equals 테스트 추가

--- app/src/test/java/com/codesoom/assignment/AppTest.java
+++ app/src/test/java/com/codesoom/assignment/AppTest.java
@@ -6,6 +6,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class AppTest {
+
+    @Test
+    void appStartTest() {
+        App.main(new String[]{});
+    }
+
     @Test
     void appHasAGreeting() {
         App classUnderTest = new App();
--- app/src/test/java/com/codesoom/assignment/model/ProductTest.java
+++ app/src/test/java/com/codesoom/assignment/model/ProductTest.java
@@ -9,7 +9,19 @@ public class ProductTest {
     @Test
     void initializeProductTest() {
         Product product = new Product("name", "maker", 5000, "abc.jpg");
+        Product productWithNoParameter = new Product();
 
         assertThat(product).isNotNull();
+        assertThat(productWithNoParameter).isNotNull();
+    }
+
+    @Test
+    void equalsAndHashCodeTest() {
+        Product product = new Product("name", "maker", 5000, "abc.jpg");
+        Product product2 = new Product("name", "maker", 5000, "abc.jpg");
+
+        assertThat(product.equals(product)).isTrue();
+        assertThat(product.equals("test")).isFalse();
+        assertThat(product.hashCode()).isEqualTo(product2.hashCode());
     }
 }
