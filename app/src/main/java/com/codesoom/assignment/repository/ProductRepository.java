e6453b1506d4da289fc6c80098ccbbcb0b4445cd
hy43 <hyde0401@gmail.com>
2022-05-26 15:54:10 +0900

    feat : ProductRepository 인터페이스 작성

---
+++ app/src/main/java/com/codesoom/assignment/repository/ProductRepository.java
@@ -0,0 +1,17 @@
+package com.codesoom.assignment.repository;
+
+import com.codesoom.assignment.domain.Product;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductRepository {
+
+    List<Product> findAll();
+
+    Optional<Product> findById(Long id);
+
+    Product save(Product product);
+
+    void delete(Long id);
+}
