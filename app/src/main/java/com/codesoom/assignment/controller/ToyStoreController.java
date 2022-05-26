09ea186b3bd502ee16d92021bedfae772f1f595d
10M <endlesslhk@gmail.com>
2022-05-26 18:08:25 +0900

    feat(ToyStoreController): CrossOrigin 추가

--- app/src/main/java/com/codesoom/assignment/controller/ToyStoreController.java
+++ app/src/main/java/com/codesoom/assignment/controller/ToyStoreController.java
@@ -4,6 +4,7 @@ import com.codesoom.assignment.model.Product;
 import com.codesoom.assignment.service.ToyStoreService;
 import java.util.List;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/products")
 public class ToyStoreController {
