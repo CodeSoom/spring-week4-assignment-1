4dc4bf3893ecc6c46036233158395263a4fe2a8d
minjoon_lee <minjoon1995@naver.com>
2022-05-26 18:46:30 +0900

    fix : method 관련 cosr 에러 수정

--- app/src/main/java/com/codesoom/assignment/config/WebConfig.java
+++ app/src/main/java/com/codesoom/assignment/config/WebConfig.java
@@ -10,6 +10,6 @@ public class WebConfig implements WebMvcConfigurer {
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
 			.allowedOrigins("*")
-			.allowedMethods("*");
+			.allowedMethods("POST","GET","PATCH","UPDATE","DELETE");
 	}
 }
