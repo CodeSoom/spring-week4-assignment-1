ab1a4a99d71567923e0d52e806f1cb80e0f5a887
hy43 <hyde0401@gmail.com>
2022-05-26 14:28:51 +0900

    test : 성공 케이스에 대한 http 반환 테스트 틀 작성

--- app/src/test/java/com/codesoom/assignment/controller/ProductControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controller/ProductControllerTest.java
@@ -2,6 +2,7 @@ package com.codesoom.assignment.controller;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -9,17 +10,83 @@ import static org.junit.jupiter.api.Assertions.*;
 class ProductControllerTest {
 
     @Nested
-    @DisplayName("list")
+    @DisplayName("GET /products")
+    class list {
+
+        @Nested
+        @DisplayName("고양이 장난감 목록을 조회하면")
+        class when_list {
+
+            @Test
+            @DisplayName("200 OK를 응답합니다..")
+            void httpStatus_200() {
+
+            }
+        }
+    }
 
     @Nested
-    @DisplayName("detail")
+    @DisplayName("GET /products/{id}")
+    class detail {
+
+        @Nested
+        @DisplayName("id로 고양이 장난감 목록을 조회하면")
+        class when_list_with_id {
+
+            @Test
+            @DisplayName("200 OK를 응답합니다.")
+            void httpStatus_200() {
+
+            }
+        }
+    }
 
     @Nested
-    @DisplayName("regist")
+    @DisplayName("POST /products")
+    class regist {
+
+        @Nested
+        @DisplayName("고양이 장난감을 등록하면")
+        class when_list_with_id {
+
+            @Test
+            @DisplayName("201 CREATED를 응답합니다.")
+            void httpStatus_201() {
+
+            }
+        }
+    }
 
     @Nested
-    @DisplayName("modify")
+    @DisplayName("PATCH /products/{id}")
+    class modify {
+
+        @Nested
+        @DisplayName("고양이 장난감을 수정하면")
+        class when_modify_with_id {
+
+            @Test
+            @DisplayName("200 CREATED를 응답합니다.")
+            void httpStatus_200() {
+
+            }
+        }
+    }
 
     @Nested
-    @DisplayName("delete")
+    @DisplayName("DELETE /products/{id}")
+    class delete {
+
+        @Nested
+        @DisplayName("고양이 장난감을 삭제하면")
+        class when_delete_with_id {
+
+            @Test
+            @DisplayName("204 NO CONTENT를 응답합니다.")
+            void httpStatus_204() {
+
+            }
+        }
+    }
+
 }
