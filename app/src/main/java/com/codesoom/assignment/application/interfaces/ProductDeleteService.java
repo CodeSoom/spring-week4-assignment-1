b36e65b735b92df307a7e34dab5ee0d019653425
MJbae <bmj5208@gmail.com>
2022-05-27 20:57:54 +0900

    feat: ProductDeleteService 인터페이스 정의

---
+++ app/src/main/java/com/codesoom/assignment/application/interfaces/ProductDeleteService.java
@@ -0,0 +1,23 @@
+package com.codesoom.assignment.application.interfaces;
+
+import com.codesoom.assignment.domain.Toy;
+
+import java.util.List;
+
+/**
+ * Product 타입 삭제에 대한 비지니스 로직
+ * <p>
+ * All Known Implementing Classes:
+ * ToyDeleteService
+ * </p>
+ */
+public interface ProductDeleteService {
+    /**
+     * 매개변수로 전달 받은 id에 해당하는 Toy 엔티티를 반환
+     * <p>
+     * @param id Toy 엔티티의 Id에 해당
+     * @return Toy 엔티티
+     * </p>
+     */
+    Toy deleteBy(Long id);
+}
