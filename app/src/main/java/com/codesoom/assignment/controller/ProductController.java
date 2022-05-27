4bd8b1de052a216aa2e6f8f00b2442603c8a1270
MinJoonLee <52944973+mj950425@users.noreply.github.com>
2022-05-28 00:10:08 +0900

    refactoring : status code 반환을 @ResponseStatus 로 반환

--- app/src/main/java/com/codesoom/assignment/controller/ProductController.java
+++ app/src/main/java/com/codesoom/assignment/controller/ProductController.java
@@ -1,56 +1,51 @@
 package com.codesoom.assignment.controller;
 
-import java.net.URI;
-import java.util.List;
-
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.codesoom.assignment.dto.ProductDTO;
 import com.codesoom.assignment.service.ProductService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("/products")
 public class ProductController {
 
-	private final ProductService productService;
+    private final ProductService productService;
 
-	public ProductController(ProductService productService) {
-		this.productService = productService;
-	}
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
 
-	@GetMapping
-	public ResponseEntity<List<ProductDTO.Response>> getProducts() {
-		return ResponseEntity.ok(productService.getProducts());
-	}
+    @GetMapping
+    @ResponseStatus(value = HttpStatus.OK)
+    public List<ProductDTO.Response> getProducts() {
+        return productService.getProducts();
+    }
 
-	@DeleteMapping("/{id}")
-	public ResponseEntity<?> deleteProduct(@PathVariable("id") int id) {
-		productService.deleteProduct(id);
-		return ResponseEntity.noContent().build();
-	}
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteProduct(@PathVariable("id") int id) {
+        productService.deleteProduct(id);
+        return ResponseEntity.noContent().build();
+    }
 
-	@GetMapping("/{id}")
-	public ResponseEntity<ProductDTO.Response> getProduct(@PathVariable("id") int id) {
-		return ResponseEntity.ok(productService.getProduct(id));
-	}
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductDTO.Response> getProduct(@PathVariable("id") int id) {
+        return ResponseEntity.ok(productService.getProduct(id));
+    }
 
-	@PostMapping
-	public ResponseEntity<ProductDTO.Response> createProduct(@RequestBody ProductDTO.CreateProduct createProduct) {
-		URI uri = URI.create("http://localhost:8080/products");
-		return ResponseEntity.created(uri).body(productService.createProduct(createProduct));
-	}
+    @PostMapping
+    public ResponseEntity<ProductDTO.Response> createProduct(@RequestBody ProductDTO.CreateProduct createProduct) {
+        ProductDTO.Response response = productService.createProduct(createProduct);
+        URI uri = URI.create("http://localhost:8080/products/" + response.getId());
+        return ResponseEntity.created(uri).body(response);
+    }
 
-	@PatchMapping("/{id}")
-	public ResponseEntity<ProductDTO.Response> updateProduct(@PathVariable("id") int id,
-		@RequestBody ProductDTO.UpdateProduct updateProduct) {
-		return ResponseEntity.ok(productService.updateProduct(id, updateProduct));
-	}
+    @PatchMapping("/{id}")
+    public ResponseEntity<ProductDTO.Response> updateProduct(@PathVariable("id") int id,
+                                                             @RequestBody ProductDTO.UpdateProduct updateProduct) {
+        return ResponseEntity.ok(productService.updateProduct(id, updateProduct));
+    }
 }
