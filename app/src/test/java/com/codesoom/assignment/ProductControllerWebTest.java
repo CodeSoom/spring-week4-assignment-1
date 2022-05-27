b83699c818495df402f55734d466e278ac6395b5
minjoon_lee <minjoon1995@naver.com>
2022-05-27 17:39:34 +0900

    create : ProductControllerWebTest

---
+++ app/src/test/java/com/codesoom/assignment/ProductControllerWebTest.java
@@ -0,0 +1,11 @@
+package com.codesoom.assignment;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureWebMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@AutoConfigureWebMvc
+
+
+public class ProductControllerWebTest {
+}
