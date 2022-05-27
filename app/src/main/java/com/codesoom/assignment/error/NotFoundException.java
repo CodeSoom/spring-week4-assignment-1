50225f37daf4294f2fc10c93a6bebd4bf372e1df
caoyu-dev <choyumin01@gmail.com>
2022-05-27 23:43:54 +0900

    refactor: NotFoundException 에 ResponseStatus 설정 해당 설정이 안되어 있어서 계속 테스트에 오류가 있었다.

--- app/src/main/java/com/codesoom/assignment/error/NotFoundException.java
+++ app/src/main/java/com/codesoom/assignment/error/NotFoundException.java
@@ -1,5 +1,9 @@
 package com.codesoom.assignment.error;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class NotFoundException extends RuntimeException {
     public NotFoundException(Long id) {
         super("Product not found: " + id);
