a2ec8c69254fd0241559376a7540315bd2c97cc9
MJbae <bmj5208@gmail.com>
2022-05-27 10:32:18 +0900

    feat: ToyUpdateController 구현

---
+++ app/src/main/java/com/codesoom/assignment/controllers/ToyUpdateController.java
@@ -0,0 +1,28 @@
+package com.codesoom.assignment.controllers;
+
+import com.codesoom.assignment.application.interfaces.ProductUpdateService;
+import com.codesoom.assignment.controllers.dtos.ToyRequestDto;
+import com.codesoom.assignment.controllers.dtos.ToyResponseDto;
+import com.codesoom.assignment.controllers.interfaces.ProductUpdateController;
+import com.codesoom.assignment.domain.Toy;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/tasks")
+@CrossOrigin(origins = "http://localhost:3000")
+public class ToyUpdateController implements ProductUpdateController {
+    private final ProductUpdateService service;
+
+    public ToyUpdateController(ProductUpdateService service) {
+        this.service = service;
+    }
+
+    @Override
+    public ToyResponseDto update(Long id, ToyRequestDto requestDto) {
+        Toy toy = service.update(id, requestDto.toEntity());
+        return new ToyResponseDto(toy);
+    }
+}
