6dd0ba0c27706660a7ce71636599f0cafb6cda99
MinJoonLee <52944973+mj950425@users.noreply.github.com>
2022-05-28 00:09:46 +0900

    refactoring : Product 생성자를 this 를 활용하여 변경

--- app/src/main/java/com/codesoom/assignment/model/Product.java
+++ app/src/main/java/com/codesoom/assignment/model/Product.java
@@ -1,56 +1,52 @@
 package com.codesoom.assignment.model;
 
+import com.codesoom.assignment.dto.ProductDTO;
+import lombok.Getter;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
-import com.codesoom.assignment.dto.ProductDTO;
-
-import lombok.Getter;
-
 @Entity
 @Getter
 public class Product {
-	@Id
-	@GeneratedValue
-	private int id;
+    @Id
+    @GeneratedValue
+    private int id;
 
-	@Column(name = "name")
-	private String name;
+    @Column(name = "name")
+    private String name;
 
-	@Column(name = "price")
-	private int price;
+    @Column(name = "price")
+    private int price;
 
-	@Column(name = "image_url")
-	private String imageUrl;
+    @Column(name = "image_url")
+    private String imageUrl;
 
-	@Column(name = "maker")
-	private String maker;
+    @Column(name = "maker")
+    private String maker;
 
-	public Product() {
-	}
+    public Product() {
+    }
 
-	public Product(String name, int price, String imageUrl, String maker) {
-		this.name = name;
-		this.price = price;
-		this.imageUrl = imageUrl;
-		this.maker = maker;
-	}
+    public Product(String name, int price, String imageUrl, String maker) {
+        this.name = name;
+        this.price = price;
+        this.imageUrl = imageUrl;
+        this.maker = maker;
+    }
 
-	public Product(ProductDTO.CreateProduct createProduct) {
-		this.name = createProduct.getName();
-		this.price = createProduct.getPrice();
-		this.imageUrl = createProduct.getImageUrl();
-		this.maker = createProduct.getMaker();
-	}
+    public Product(ProductDTO.CreateProduct createProduct) {
+		this(createProduct.getName(), createProduct.getPrice(), createProduct.getImageUrl(), createProduct.getMaker());
+    }
 
-	public Product updateProduct(ProductDTO.UpdateProduct updateProduct) {
-		this.name = updateProduct.getName();
-		this.price = updateProduct.getPrice();
-		this.imageUrl = updateProduct.getImageUrl();
-		this.maker = updateProduct.getMaker();
-		return this;
-	}
+    public Product updateProduct(ProductDTO.UpdateProduct updateProduct) {
+        this.name = updateProduct.getName();
+        this.price = updateProduct.getPrice();
+        this.imageUrl = updateProduct.getImageUrl();
+        this.maker = updateProduct.getMaker();
+        return this;
+    }
 }
 
