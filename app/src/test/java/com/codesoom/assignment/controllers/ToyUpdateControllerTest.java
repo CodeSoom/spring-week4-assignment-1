ce5b39bb0f0a4df3da5400b6267b8a468f428710
MJbae <bmj5208@gmail.com>
2022-05-27 10:33:43 +0900

    test: ToyUpdateController 테스트 내 불필요한 변수 제거

--- app/src/test/java/com/codesoom/assignment/controllers/ToyUpdateControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/ToyUpdateControllerTest.java
@@ -1,7 +1,9 @@
 package com.codesoom.assignment.controllers;
 
+import com.codesoom.assignment.application.interfaces.ProductUpdateService;
 import com.codesoom.assignment.controllers.dtos.ToyRequestDto;
 import com.codesoom.assignment.controllers.dtos.ToyResponseDto;
+import com.codesoom.assignment.controllers.interfaces.ProductUpdateController;
 import com.codesoom.assignment.domain.Toy;
 import com.codesoom.assignment.domain.ToyProducer;
 import org.junit.jupiter.api.*;
@@ -10,8 +12,6 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.math.BigDecimal;
-import java.util.LinkedList;
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.given;
@@ -22,21 +22,18 @@ class ToyUpdateControllerTest {
     @Mock
     private ProductUpdateService service;
     private ProductUpdateController controller;
-    private final List<Toy> toys = new LinkedList<>();
+    private ToyRequestDto requestDto;
 
     private final Long TOY_ID = 2L;
     private final String TOY_NAME = "Test Toy";
+    private final String TOY_NAME_UPDATING = "Updated Toy";
     private final String PRODUCER_NAME = "Test Producer";
     private final BigDecimal WON_VALUE = new BigDecimal(1000);
 
     @BeforeEach
     void setUp() {
+        requestDto = new ToyRequestDto(TOY_NAME_UPDATING, new ToyProducer(PRODUCER_NAME), WON_VALUE);
         controller = new ToyUpdateController(service);
-    }
-
-    @AfterEach
-    void clear() {
-        toys.clear();
     }
 
 
@@ -44,14 +41,13 @@ class ToyUpdateControllerTest {
     @DisplayName("update 메소드는")
     class Describe_update {
         private ToyResponseDto subject() {
-            ToyRequestDto requestDto = new ToyRequestDto(TOY_NAME, new ToyProducer(PRODUCER_NAME), WON_VALUE);
-            return controller.update(requestDto);
+            return controller.update(TOY_ID, requestDto);
         }
 
         @BeforeEach
         void setUp() {
-            Toy toyUpdating = new Toy(TOY_ID, TOY_NAME + "Updating", new ToyProducer(PRODUCER_NAME), WON_VALUE);
-            given(service.update(TOY_ID, toyUpdating)).willReturn(toyUpdating);
+            Toy toyUpdating = new Toy(TOY_ID, TOY_NAME_UPDATING, new ToyProducer(PRODUCER_NAME), WON_VALUE);
+            given(service.update(TOY_ID, requestDto.toEntity())).willReturn(toyUpdating);
         }
 
         @Test
