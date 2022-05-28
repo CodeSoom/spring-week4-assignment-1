4276c522c0a0a453ab9493ea560806eb982cec35
yongjunleeme <chesidyong@gmail.com>
2022-05-28 16:19:21 +0900

    Delete readOnly = true in ProductService

--- app/src/main/java/com/codesoom/assignment/service/ProductService.java
+++ app/src/main/java/com/codesoom/assignment/service/ProductService.java
@@ -13,7 +13,7 @@ import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
-@Transactional(readOnly = true)
+@Transactional
 public class ProductService {
     private final ProductRepository productRepository;
 
