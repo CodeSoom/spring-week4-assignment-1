cca502b0c810f72c1992facb3bc33480d227e468
Jinu Noh <yes.jinu@gmail.com>
2022-06-28 20:54:37 +0900

    test: add tests for `ToyService::register`

--- app/src/test/java/com/codesoom/assignment/ToyServiceTest.java
+++ app/src/test/java/com/codesoom/assignment/ToyServiceTest.java
@@ -10,6 +10,8 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -132,6 +134,51 @@ public class ToyServiceTest {
                 long ID_NOT_EXISTS = Long.MAX_VALUE;
                 assertThatThrownBy(() -> toyService.getToyById(ID_NOT_EXISTS))
                         .isInstanceOf(ToyNotFoundException.class);
+            }
+        }
+    }
+
+    @Nested
+    @DisplayName("register 메소드는")
+    class Describe_register {
+        @Nested
+        @DisplayName("정상적인 상황에서")
+        class Context_normal {
+            private Toy toy;
+
+            @BeforeEach
+            void setUp() {
+                toy = randomToy();
+            }
+
+            @Test
+            @DisplayName("인자로 넘겨진 Toy를 등록하고, 등록된 Toy를 리턴한다")
+            void it_returns_registered_toy() {
+                Toy registeredToy = toyService.register(toy);
+
+                assertThat(registeredToy).isEqualTo(toy);
+            }
+        }
+
+        @Nested
+        @DisplayName("여러 태스크를 추가해도")
+        class Context_multiple_tasks {
+            void registerMultipleToys(int testCase) {
+                for (int i = 0; i < testCase; i++) {
+                    Toy randomToy = randomToy();
+                    toyService.register(randomToy);
+                }
+            }
+
+            @DisplayName("모두 다른 ID를 부여한다")
+            @ParameterizedTest
+            @ValueSource(ints = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20})
+            void it_returns_all_different_ids(int testCase) {
+                registerMultipleToys(testCase);
+                List<Toy> toys = toyService.getToys();
+
+                Set<Long> ids = toys.stream().map(Toy::getId).collect(Collectors.toSet());
+                assertThat(ids).hasSize(testCase);
             }
         }
     }
