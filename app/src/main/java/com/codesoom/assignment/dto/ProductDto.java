1a2eeca65bcde9d52caac459caa70f3d44f44b91
unajun <bjyoon513@gmail.com>
2022-06-30 02:01:13 +0900

    feat: implement dto and getter

--- app/src/main/java/com/codesoom/assignment/dto/ProductDto.java
+++ app/src/main/java/com/codesoom/assignment/dto/ProductDto.java
@@ -1,4 +1,31 @@
 package com.codesoom.assignment.dto;
 
 public class ProductDto {
+    private final String name;
+    private final String maker;
+    private final Long price;
+    private final String imageURL;
+
+    public ProductDto(String productName, String makerName, Long productPrice, String imageURL) {
+        this.name = productName;
+        this.maker = makerName;
+        this.price = productPrice;
+        this.imageURL = imageURL;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getMaker() {
+        return maker;
+    }
+
+    public Long getPrice() {
+        return price;
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
 }
