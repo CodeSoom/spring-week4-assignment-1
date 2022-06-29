94f557f1848a4df22fc401748d7fa231fa9dfc6d
unajun <bjyoon513@gmail.com>
2022-06-30 02:02:22 +0900

    feat: implement ProductRepository by extending CrudRepository

--- app/src/main/java/com/codesoom/assignment/domain/ProductRepository.java
+++ app/src/main/java/com/codesoom/assignment/domain/ProductRepository.java
@@ -1,4 +1,12 @@
 package com.codesoom.assignment.domain;
 
-public class ProductRepository {
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProductRepository extends CrudRepository<Product, Long> {
+    @Override
+    List<Product> findAll();
 }
