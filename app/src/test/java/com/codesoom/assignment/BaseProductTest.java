de7c791dfd5aa898d4726c1c6b7cd594a34a366f
unajun <bjyoon513@gmail.com>
2022-06-30 01:54:33 +0900

    test: implement Base for testing

---
+++ app/src/test/java/com/codesoom/assignment/BaseProductTest.java
@@ -0,0 +1,28 @@
+package com.codesoom.assignment;
+
+
+import com.codesoom.assignment.domain.Product;
+import com.codesoom.assignment.dto.ProductDto;
+
+public abstract class BaseProductTest {
+
+    protected static final Long PRODUCT_ID = 1L;
+    protected static final String PRODUCT_NAME = "sample cat";
+
+    protected static final Long PRODUCT_PRICE = 300L;
+    protected static final String IMAGE_URL = "test url";
+    protected static final String MAKER_NAME = "sample maker";
+    protected static final String ERROR_MSG = "Product not found";
+
+    protected Product supplyDummyTask() {
+
+        ProductDto productDto = new ProductDto(PRODUCT_NAME, MAKER_NAME, PRODUCT_PRICE, IMAGE_URL);
+        Product product = new Product(productDto);
+
+        return product;
+    }
+
+    protected String supplyErrorMSG(Long id){
+        return ERROR_MSG + ": " + id;
+    }
+}
--- app/src/test/java/com/codesoom/assignment/domain/ProductTest.java
+++
@@ -1,7 +0,0 @@
-package com.codesoom.assignment.domain;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class ProductTest {
-
-}
