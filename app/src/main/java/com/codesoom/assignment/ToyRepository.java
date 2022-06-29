edfc85efbb57fc0e8ef22dd8a93d6deb80bd21d9
Jinu Noh <yes.jinu@gmail.com>
2022-06-29 19:35:41 +0900

    refactor: replace List structure into Map structure for faster search

--- app/src/main/java/com/codesoom/assignment/ToyRepository.java
+++ app/src/main/java/com/codesoom/assignment/ToyRepository.java
@@ -2,40 +2,30 @@ package com.codesoom.assignment;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class ToyRepository {
-    List<Toy> toys = new ArrayList<>();
+    Map<Long, Toy> toys = new ConcurrentHashMap();
 
     public Optional<Toy> findById(long id) {
-        for (Toy toy : toys) {
-            if (toy.getId() == id) {
-                return Optional.of(toy);
-            }
-        }
-
-        return Optional.empty();
+        return Optional.ofNullable(toys.get(id));
     }
 
     public Toy save(Toy toy) {
         Toy toyEntity = new Toy(nextId(), toy.getName(), toy.getMaker(), toy.getPrice(), toy.getImageUrl());
-        toys.add(toyEntity);
+        toys.put(toyEntity.getId(), toyEntity);
 
         return toyEntity;
     }
 
     public List<Toy> findAll() {
-        return toys;
+        return new ArrayList<>(toys.values());
     }
 
     public Optional<Toy> deleteById(Long id) {
-        for (Toy toy : toys) {
-            if (toy.getId() == id) {
-                return Optional.of(toy);
-            }
-        }
-
-        return Optional.empty();
+        return Optional.ofNullable(toys.remove(id));
     }
 
     private long nextId() {
