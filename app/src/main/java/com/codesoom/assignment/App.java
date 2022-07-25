a5bf5913aa9e187e808bbc5aef4bc07def790e04
BEOMKING <qjawlsqjacks@naver.com>
2022-07-25 22:29:58 +0900

    불필요한 코드 제거

--- app/src/main/java/com/codesoom/assignment/App.java
+++ app/src/main/java/com/codesoom/assignment/App.java
@@ -5,10 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class App {
-    public String getGreeting() {
-        return "안녕하세요! 잘 부탁드립니다.";
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
