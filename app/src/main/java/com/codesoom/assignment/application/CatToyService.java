0cee9419b679bb814e0213227b3bd72c54ec6f4c
eunice0035 <eunice7357@gmail.com>
2022-07-26 21:00:47 +0900

    CatToyService 주석추가

--- app/src/main/java/com/codesoom/assignment/application/CatToyService.java
+++ app/src/main/java/com/codesoom/assignment/application/CatToyService.java
@@ -1,2 +1,44 @@
-package com.codesoom.assignment.application;public class CatToyService {
+package com.codesoom.assignment.application;
+
+import com.codesoom.assignment.domain.CatToy;
+import com.codesoom.assignment.domain.CatToyRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CatToyService {
+
+    private final CatToyRepository catToyRepository;
+
+    public CatToyService(CatToyRepository catToyRepository) {
+        this.catToyRepository = catToyRepository;
+    }
+
+    public List<CatToy> getCatToys(){
+        return catToyRepository.findAll();
+    }
+
+    public CatToy getToys(Long id){
+        return catToyRepository.find(id);
+    }
+
+    public CatToy createToy(CatToy source){
+        CatToy toy = new CatToy(source.getName(), source.getMaker(), source.getPrice(), source.getImg_url());
+        return catToyRepository.save(toy);
+    }
+
+    //추가적인 구현이 필요합니다.
+    public CatToy updateToy(Long id, CatToy source){
+
+        CatToy toy = getToys(id);
+        toy.setName(source.getName());
+
+        return catToyRepository.save(toy);
+    }
+
+    public CatToy deleteToy(Long id){
+        return catToyRepository.delete(getToys(id));
+    }
+
 }
