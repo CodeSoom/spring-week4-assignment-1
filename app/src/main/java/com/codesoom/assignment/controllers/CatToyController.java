cebf34d99a8f40bb8a07998952bdef0d1b00952c
eunice0035 <eunice7357@gmail.com>
2022-07-26 21:08:10 +0900

    CatToyControllers 구현

--- app/src/main/java/com/codesoom/assignment/controllers/CatToyController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/CatToyController.java
@@ -1,2 +1,51 @@
-package com.codesoom.assignment.controllers;public class CatToyController {
+package com.codesoom.assignment.controllers;
+
+import com.codesoom.assignment.application.CatToyService;
+import com.codesoom.assignment.domain.CatToy;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/cattoys")
+@CrossOrigin
+public class CatToyController {
+    private CatToyService catToyService;
+
+    public CatToyController(CatToyService catToyService){
+        this.catToyService = catToyService;
+    }
+
+    @GetMapping
+    public List<CatToy> list(){
+        return catToyService.getCatToys();
+    }
+
+    @GetMapping("{id}")
+    public CatToy detail(@PathVariable Long id){
+        return catToyService.getToys(id);
+    }
+
+    @PostMapping()
+    @ResponseStatus(HttpStatus.CREATED)
+    public CatToy update(@RequestBody CatToy source){
+        return catToyService.createToy(source);
+    }
+
+    @PutMapping("{id}")
+    public CatToy update(@PathVariable Long id, @RequestBody CatToy source){
+        return catToyService.updateToy(id, source);
+    }
+
+    @PatchMapping("{id}")
+    public CatToy patch(@PathVariable Long id, @RequestBody CatToy source){
+        return catToyService.updateToy(id, source);
+    }
+
+    @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id){
+        catToyService.deleteToy(id);
+    }
 }
