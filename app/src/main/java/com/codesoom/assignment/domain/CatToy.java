4ae077bd22e6d316ed178f2fc383e36397b2a443
BEOMKING <qjawlsqjacks@naver.com>
2022-07-26 21:24:32 +0900

    CatToy 객체에 기본키를 name -> id 를 추가 후 변경

--- app/src/main/java/com/codesoom/assignment/domain/CatToy.java
+++ app/src/main/java/com/codesoom/assignment/domain/CatToy.java
@@ -1,7 +1,5 @@
 package com.codesoom.assignment.domain;
 
-import lombok.Getter;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -9,9 +7,10 @@ import javax.persistence.Id;
 import java.util.Objects;
 
 @Entity
-@Getter
 public class CatToy {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String name;
     private String maker;
     private Integer price;
@@ -20,7 +19,8 @@ public class CatToy {
     protected CatToy() {
     }
 
-    public CatToy(String name, String maker, Integer price, String url) {
+    public CatToy(Long id, String name, String maker, Integer price, String url) {
+        this.id = id;
         this.name = name;
         this.maker = maker;
         this.price = price;
@@ -36,11 +36,11 @@ public class CatToy {
             return false;
         }
         CatToy catToy = (CatToy) o;
-        return Objects.equals(name, catToy.name) && Objects.equals(maker, catToy.maker) && Objects.equals(price, catToy.price) && Objects.equals(url, catToy.url);
+        return Objects.equals(id, catToy.id) && Objects.equals(name, catToy.name) && Objects.equals(maker, catToy.maker) && Objects.equals(price, catToy.price) && Objects.equals(url, catToy.url);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(name, maker, price, url);
+        return Objects.hash(id, name, maker, price, url);
     }
 }
