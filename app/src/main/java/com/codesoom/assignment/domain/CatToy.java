a472b193afdbda72e928503f8748d373d8c6933e
Ernest Hong <hsh3592@gmail.com>
2022-07-27 23:05:46 +0900

    refactor: CatToy 주석 추가

--- app/src/main/java/com/codesoom/assignment/domain/CatToy.java
+++ app/src/main/java/com/codesoom/assignment/domain/CatToy.java
@@ -1,9 +1,14 @@
 package com.codesoom.assignment.domain;
 
+import com.google.common.annotations.VisibleForTesting;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+/**
+ * 고양이 장난감 정보를 담는 역할을 합니다.
+ */
 @Entity
 public class CatToy {
     @Id
@@ -16,7 +21,25 @@ public class CatToy {
 
     private int price;
 
+    /**
+     * 고양이 장난감 정보를 생성합니다.
+     * @param name 장난감 이름
+     * @param maker 장난감 메이커
+     * @param price 장난감 가격
+     */
     public CatToy(String name, String maker, int price) {
+        this(null, name, maker, price);
+    }
+
+    /**
+     * 고양이 장난감 정보를 지정된 Id로 생성합니다.
+     * @param id 식별자
+     * @param name 장난감 이름
+     * @param maker 장난감 메이커
+     * @param price 장난감 가격
+     */
+    public CatToy(Long id, String name, String maker, int price) {
+        this.id = id;
         this.name = name;
         this.maker = maker;
         this.price = price;
