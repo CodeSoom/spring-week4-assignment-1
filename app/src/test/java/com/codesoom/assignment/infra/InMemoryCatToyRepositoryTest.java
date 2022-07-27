51daeefde98a0e5459cafac199e0341910a34be7
Ernest Hong <hsh3592@gmail.com>
2022-07-27 20:29:43 +0900

    feat: InMemoryCatToyRepository::getList 테스트 추가

---
+++ app/src/test/java/com/codesoom/assignment/infra/InMemoryCatToyRepositoryTest.java
@@ -0,0 +1,38 @@
+package com.codesoom.assignment.infra;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("InMemoryCatToyRepositoryTest 테스트")
+class InMemoryCatToyRepositoryTest {
+
+    private CatToyRepository repository;
+
+    @BeforeEach
+    void prepare() {
+        repository = new InMemoryCatToyRepository();
+    }
+
+    @Nested
+    @DisplayName("getList 메소드는")
+    class Describe_getList {
+        @Nested
+        @DisplayName("생성된 고양이 장난감이 없을 때")
+        class Context_didNotCreateCatToy {
+            @BeforeEach
+            void prepare() {
+                repository.removeAll();
+            }
+
+            @Test
+            @DisplayName("빈 목록을 반환한다")
+            void it_returnsEmptyList() {
+                assertThat(repository.getList()).isEmpty();
+            }
+        }
+    }
+}
