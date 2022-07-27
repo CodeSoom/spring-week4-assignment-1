0d51dbfb3600627b4e62eacb521dee59309b7588
Ernest Hong <hsh3592@gmail.com>
2022-07-27 21:40:09 +0900

    feat: GET /products 테스트 추가

---
+++ app/src/test/java/com/codesoom/assignment/application/CatToyControllerWebTest.java
@@ -0,0 +1,52 @@
+package com.codesoom.assignment.application;
+
+import com.codesoom.assignment.domain.CatToyRepository;
+import com.codesoom.assignment.infra.InMemoryCatToyRepository;
+import com.codesoom.assignment.service.CatToyService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@DisplayName("CatToyController Web")
+public class CatToyControllerWebTest {
+    private CatToyRepository repository;
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    void setup() {
+        repository = new InMemoryCatToyRepository();
+        final CatToyService service = new CatToyService(repository);
+        final CatToyController controller = new CatToyController(service);
+        mockMvc = MockMvcBuilders
+                .standaloneSetup(controller)
+                .build();
+    }
+
+    @Nested
+    @DisplayName("GET /products 요청은")
+    class Describe_getProducts {
+        @Nested
+        @DisplayName("생성된 고양이 장난감이 없을 때")
+        class Context_didNotCreateCatToy {
+            @BeforeEach
+            void prepare() {
+                repository.deleteAll();
+            }
+        }
+
+        @Test
+        @DisplayName("OK status, 빈 목록을 반환한다")
+        void it_returnsOkStatusAndEmptyList() throws Exception {
+            mockMvc.perform(get("/products"))
+                    .andExpect(status().isOk())
+                    .andExpect(content().string("[]"));
+        }
+    }
+}
