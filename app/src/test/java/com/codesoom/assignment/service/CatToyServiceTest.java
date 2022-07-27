bf60f8e25a4ca45b3cf599db7be8200404d7bece
박범진 <qjawlsqjacks@fasoo.com>
2022-07-27 09:41:32 +0900

    서비스 로직을 통합 테스트로 변경

--- app/src/test/java/com/codesoom/assignment/service/CatToyServiceTest.java
+++ app/src/test/java/com/codesoom/assignment/service/CatToyServiceTest.java
@@ -3,28 +3,33 @@ package com.codesoom.assignment.service;
 import com.codesoom.assignment.domain.CatToy;
 import com.codesoom.assignment.domain.CatToyDto;
 import com.codesoom.assignment.domain.CatToyRepository;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 
-@ExtendWith(MockitoExtension.class)
+@DataJpaTest
 @DisplayName("CatToyService 클래스의")
 public class CatToyServiceTest {
     public static final long GIVEN_ID = 1L;
+
     private ToyService toyService;
 
-    @Mock
+    @Autowired
     private CatToyRepository catToyRepository;
 
     @BeforeEach
@@ -38,22 +43,16 @@ public class CatToyServiceTest {
         @Nested
         @DisplayName("장난감이 주어지면")
         class Context_with_catToy {
-            CatToyDto prepareRequest() {
-                return new CatToyDto("뱀", "아디다스", 3000, "url");
-            }
-
-            CatToy prepareResult() {
-                return new CatToy(GIVEN_ID, "뱀", "아디다스", 3000, "url");
+            CatToy prepare() {
+                return toyService.create(new CatToyDto("뱀", "아디다스", 3000, "url"));
             }
 
             @Test
             @DisplayName("장난감을 저장하고 리턴한다")
             void It_returns_catToy_and_save() {
-                given(catToyRepository.save(prepareRequest().toCatToy())).willReturn(prepareResult());
+                CatToy given = prepare();
 
-                assertThat(toyService.create(prepareRequest())).isEqualTo(prepareResult());
-
-                verify(catToyRepository).save(prepareRequest().toCatToy());
+                assertThat(given).isEqualTo(new CatToy(given.getId(), "뱀", "아디다스", 3000, "url"));
             }
         }
     }
@@ -64,18 +63,16 @@ public class CatToyServiceTest {
         @Nested
         @DisplayName("주어진 식별자를 가진 장난감이 있다면")
         class Context_with_toy {
-            Optional<CatToy> prepareResult() {
-                return Optional.of(new CatToy(GIVEN_ID, "뱀", "아디다스", 3000, "url"));
+            CatToy prepare() {
+                return toyService.create(new CatToyDto("뱀", "아디다스", 3000, "url"));
             }
 
             @Test
             @DisplayName("장난감을 리턴한다")
             void It_returns_toy() {
-                given(catToyRepository.findById(GIVEN_ID)).willReturn(prepareResult());
+                CatToy given = prepare();
 
-                assertThat(toyService.findById(GIVEN_ID)).isEqualTo(prepareResult().get());
-
-                verify(catToyRepository).findById(GIVEN_ID);
+                assertThat(toyService.findById(given.getId())).isEqualTo(new CatToy(given.getId(), "뱀", "아디다스", 3000, "url"));
             }
         }
     }
