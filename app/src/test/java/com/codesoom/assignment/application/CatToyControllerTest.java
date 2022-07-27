ae6ed818b00732c4087780f457afe9ac6acffd1e
Ernest Hong <hsh3592@gmail.com>
2022-07-27 21:00:02 +0900

    feat: CatToyController::getList 테스트 추가

---
+++ app/src/test/java/com/codesoom/assignment/application/CatToyControllerTest.java
@@ -0,0 +1,43 @@
+package com.codesoom.assignment.application;
+
+import com.codesoom.assignment.domain.CatToyRepository;
+import com.codesoom.assignment.infra.InMemoryCatToyRepository;
+import com.codesoom.assignment.service.CatToyService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("CatToyController 테스트")
+public class CatToyControllerTest {
+    private CatToyRepository repository;
+    private CatToyController controller;
+
+    @BeforeEach
+    void setup() {
+        repository = new InMemoryCatToyRepository();
+        CatToyService service = new CatToyService(repository);
+        controller = new CatToyController(service);
+    }
+
+    @Nested
+    @DisplayName("getList 메소드는")
+    class Describe_getList {
+        @Nested
+        @DisplayName("생성된 고양이 장난감이 없을 때")
+        class Context_didNotCreateCatToy {
+            @BeforeEach
+            void prepare() {
+                repository.removeAll();
+            }
+
+            @Test
+            @DisplayName("빈 목록을 반환한다")
+            void it_returnsEmptyList() {
+                assertThat(controller.getList()).isEmpty();
+            }
+        }
+    }
+}
