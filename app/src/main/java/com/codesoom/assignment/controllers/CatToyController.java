82ed56814ae1f8c30e3af0922fcca8c51262f181
eunice0035 <eunice7357@gmail.com>
2022-07-28 18:25:43 +0900

    import 수정

--- app/src/main/java/com/codesoom/assignment/controllers/CatToyController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/CatToyController.java
@@ -3,7 +3,23 @@ package com.codesoom.assignment.controllers;
 import com.codesoom.assignment.application.CatToyService;
 import com.codesoom.assignment.domain.CatToy;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+
+
+
+
 
 import java.util.List;
 
@@ -29,7 +45,7 @@ public class CatToyController {
 
     @PostMapping()
     @ResponseStatus(HttpStatus.CREATED)
-    public CatToy update(@RequestBody CatToy source){
+    public CatToy create(@RequestBody CatToy source){
         return catToyService.createToy(source);
     }
 
