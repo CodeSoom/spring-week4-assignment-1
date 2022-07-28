f65dc720c7c4e53866a7d6d8df820851e65826d9
Ernest Hong <hsh3592@gmail.com>
2022-07-28 20:10:07 +0900

    refactor: InMemoryCatToyRepositoryTest findAll 리팩토링

--- app/src/test/java/com/codesoom/assignment/infra/InMemoryCatToyRepositoryTest.java
+++ app/src/test/java/com/codesoom/assignment/infra/InMemoryCatToyRepositoryTest.java
@@ -8,11 +8,15 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("InMemoryCatToyRepositoryTest 테스트")
 class InMemoryCatToyRepositoryTest {
+    private final String FIXTURE_NAME = "name";
+    private final String FIXTURE_MAKER = "maker";
+    private final int FIXTURE_PRICE = 10000;
 
     private CatToyRepository repository;
 
@@ -22,8 +26,8 @@ class InMemoryCatToyRepositoryTest {
     }
 
     @Nested
-    @DisplayName("getList 메소드는")
-    class Describe_getList {
+    @DisplayName("findAll 메소드는")
+    class Describe_findAll {
         @Nested
         @DisplayName("저장된 고양이 장난감이 없을 때")
         class Context_didNotSaveCatToy {
@@ -42,25 +46,32 @@ class InMemoryCatToyRepositoryTest {
         @Nested
         @DisplayName("저장된 고양이 장난감이 있을 때")
         class Context_didSaveCatToy {
-            final int NUMBER_OF_TOY_LIST = 2;
+            final int NUMBER_OF_TOY_LIST = 3;
 
             @BeforeEach
             void prepare() {
-                for (int i = 0; i < NUMBER_OF_TOY_LIST; i++) {
-                    repository.save(new CatToy("name", "maker", 999999));
-                }
+                IntStream.rangeClosed(1, NUMBER_OF_TOY_LIST)
+                        .mapToObj(value -> {
+                            return new CatToy(FIXTURE_NAME + value, FIXTURE_MAKER + value, FIXTURE_PRICE + value);
+                        })
+                        .forEach(catToy -> {
+                            repository.save(catToy);
+                        });
             }
 
             @Test
             @DisplayName("저장된 고양이 장난감 목록을 반환한다")
             void it_returnsSavedToyList() {
                 List<CatToy> result = repository.findAll();
-                CatToy toy1 = result.get(0);
-                CatToy toy2 = result.get(1);
-
                 assertThat(result).hasSize(NUMBER_OF_TOY_LIST);
-                assertThat(toy1.getId()).isEqualTo(1L);
-                assertThat(toy2.getId()).isEqualTo(2L);
+
+                IntStream.rangeClosed(1, NUMBER_OF_TOY_LIST)
+                        .forEach(index -> {
+                            CatToy catToy = result.get(index - 1);
+                            assertThat(catToy.getName()).isEqualTo(FIXTURE_NAME + index);
+                            assertThat(catToy.getMaker()).isEqualTo(FIXTURE_MAKER + index);
+                            assertThat(catToy.getPrice()).isEqualTo(FIXTURE_PRICE + index);
+                        });
             }
         }
     }
