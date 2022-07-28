cc7bf178fdc43a21d2a935fe526415865022bd7b
Ernest Hong <hsh3592@gmail.com>
2022-07-28 21:06:02 +0900

    feat: InMemoryCatToyRepository::save 테스트 추가

--- app/src/test/java/com/codesoom/assignment/infra/InMemoryCatToyRepositoryTest.java
+++ app/src/test/java/com/codesoom/assignment/infra/InMemoryCatToyRepositoryTest.java
@@ -8,11 +8,12 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@DisplayName("InMemoryCatToyRepositoryTest 테스트")
+@DisplayName("InMemoryCatToyRepository 클래스")
 class InMemoryCatToyRepositoryTest {
     private final String FIXTURE_NAME = "name";
     private final String FIXTURE_MAKER = "maker";
@@ -74,5 +75,57 @@ class InMemoryCatToyRepositoryTest {
                         });
             }
         }
+    }
+
+    @Nested
+    @DisplayName("save 메소드는")
+    class Describe_save {
+        @Nested
+        @DisplayName("고양이 장난감 정보를 전달했을 때")
+        class Context_withCatToy {
+            CatToy catToy = new CatToy(FIXTURE_NAME, FIXTURE_MAKER, FIXTURE_PRICE);
+
+            @Test
+            @DisplayName("저장에 성공한 장난감을 반환한다")
+            void it_returnsSavedToy() {
+                CatToy result = repository.save(catToy);
+
+                assertThat(result.getId()).isEqualTo(1L);
+                assertThat(result.getName()).isEqualTo(FIXTURE_NAME);
+                assertThat(result.getMaker()).isEqualTo(FIXTURE_MAKER);
+                assertThat(result.getPrice()).isEqualTo(FIXTURE_PRICE);
+            }
+        }
+
+        @Nested
+        @DisplayName("고양이 장난감을 담아서 여러번 호출했을 때")
+        class Context_calledMultipleWithCatToy {
+            private final int NUMBER_OF_TOY_LIST = 3;
+            private final List<CatToy> requestToys = IntStream.rangeClosed(1, NUMBER_OF_TOY_LIST)
+                    .mapToObj(value -> {
+                        return new CatToy(FIXTURE_NAME + value, FIXTURE_MAKER + value, FIXTURE_PRICE + value);
+                    })
+                    .collect(Collectors.toList());
+
+            @Test
+            @DisplayName("저장에 성공한 장난감을 반환한다")
+            void it_returnsSavedToy() {
+                 List<CatToy> result = requestToys.stream()
+                         .map(catToy -> {
+                             return repository.save(catToy);
+                         })
+                         .collect(Collectors.toList());
+
+                IntStream.rangeClosed(1, NUMBER_OF_TOY_LIST)
+                                .forEach(index -> {
+                                    CatToy catToy = result.get(index - 1);
+                                    assertThat(catToy.getId()).isEqualTo(index);
+                                    assertThat(catToy.getName()).isEqualTo(FIXTURE_NAME + index);
+                                    assertThat(catToy.getMaker()).isEqualTo(FIXTURE_MAKER + index);
+                                    assertThat(catToy.getPrice()).isEqualTo(FIXTURE_PRICE + index);
+                                });
+            }
+        }
+
     }
 }
