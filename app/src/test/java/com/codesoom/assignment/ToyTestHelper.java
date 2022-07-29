9443b5f36c6567fa45eca6a4ef2cf8652bad3e56
BEOMKING <qjawlsqjacks@naver.com>
2022-07-29 20:31:47 +0900

    TestHelper 준비된 데이터 추가 및 정리

--- app/src/test/java/com/codesoom/assignment/ToyTestHelper.java
+++ app/src/test/java/com/codesoom/assignment/ToyTestHelper.java
@@ -4,22 +4,25 @@ import com.codesoom.assignment.domain.CatToy;
 import com.codesoom.assignment.domain.CatToyDto;
 
 public class ToyTestHelper {
-    public final String givenToyName = "낚시물고기";
-    public final String givenMaker = "나이키";
-    public final Integer givenPrice = 5000;
-    public final String givenUrl = "url";
+    private static final String givenToyName = "낚시물고기";
+    private static final String givenMaker = "나이키";
+    private static final Integer givenPrice = 5000;
+    private static final String givenUrl = "url";
 
-    public final String changedToyName = "변경된 낚시물고기";
-    public final String changedMaker = "변경된 나이키";
-    public final Integer changedPrice = 10;
-    public final String changedUrl = "변경된 url";
+    private static final String changedToyName = "변경된 낚시물고기";
+    private static final String changedMaker = "변경된 나이키";
+    private static final Integer changedPrice = 10;
+    private static final String changedUrl = "변경된 url";
 
-    public final CatToyDto givenToyDto = new CatToyDto(givenToyName, givenMaker, givenPrice, givenUrl);
+    public static final CatToyDto toyToCreateDto = new CatToyDto(givenToyName, givenMaker, givenPrice, givenUrl);
 
-    public final CatToyDto givenToyToChangeDto = new CatToyDto(changedToyName, changedMaker, changedPrice, changedUrl);
+    public static final CatToyDto toyToChangeDto = new CatToyDto(changedToyName, changedMaker, changedPrice, changedUrl);
+
+    public CatToy createdToy(Long id) {
+        return new CatToy(id, changedToyName, changedMaker, changedPrice, changedUrl);
+    }
 
     public CatToy changedToy(Long id) {
         return new CatToy(id, changedToyName, changedMaker, changedPrice, changedUrl);
     }
-
 }
