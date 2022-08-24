2acf43a6a0023f1cc0059bd698d6420b339fc7ab
정현준 <jeongdalma@gmail.com>
2022-08-24 13:32:07 +0900

    Product DTO 추가

--- app/src/main/java/com/codesoom/assignment/controller/ProductController.java
+++ app/src/main/java/com/codesoom/assignment/controller/ProductController.java
@@ -1,8 +1,14 @@
 package com.codesoom.assignment.controller;
 
+import com.codesoom.assignment.dto.ProductDTO;
 import com.codesoom.assignment.exception.ResourceNotFoundException;
 import com.codesoom.assignment.service.ProductService;
 import com.codesoom.assignment.domain.Product;
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -14,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/products")
 public class ProductController {
@@ -35,8 +42,8 @@ public class ProductController {
     }
 
     @PostMapping
-    public Product createProduct(Product product){
-        return service.save(product);
+    public Product createProduct(@RequestBody ProductDTO productDTO) {
+        return service.save(productDTO.toProduct());
     }
 
     @PatchMapping("{id}")
