3430a2c9988560ed8f6170a879fd40bcd4ec4058
정현준 <jeongdalma@gmail.com>
2022-08-24 10:26:27 +0900

    ProductService 주석 추가

--- app/src/main/java/com/codesoom/assignment/service/ProductService.java
+++ app/src/main/java/com/codesoom/assignment/service/ProductService.java
@@ -19,19 +19,45 @@ public class ProductService{
         this.repository = repository;
     }
 
+    /**
+     * 모든 엔티티를 반환합니다
+     *
+     * @return List<엔티티>
+     */
     public List<Product> findAll(){
         return (List<Product>) repository.findAll();
     }
 
+    /**
+     * 식별자와 같은 엔티티를 찾아 반환합니다
+     *
+     * @param   id
+     * @return  id에 해당하는 자원
+     * @throws  ResourceNotFoundException
+     *          id에 해당하는 자원이 존재하지 않을 때 던져진다
+     */
     public Product findById(Long id){
         return repository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("상품이 존재하지 않습니다. id : " + id));
     }
 
+    /**
+     * 주어진 엔티티를 저장합니다
+     *
+     * @param   product
+     * @return  저장된 product
+     */
     public Product save(Product product){
         return repository.save(product);
     }
 
+    /**
+     * id에 해당하는 엔티티의 정보를 넘겨받은 엔티티로 대체한다
+     * id는 변경되지 않는다
+     * @param id 엔티티의 식별자
+     * @param product 대체할 엔티티의 정보
+     * @return 수정된 product
+     */
     public Product update(Long id , Product product){
         // TODO
         // 수정 정보 적용
@@ -39,6 +65,10 @@ public class ProductService{
         return product;
     }
 
+    /**
+     * id에 해당하는 엔티티를 삭제한다
+     * @param id 엔티티의 식별자
+     */
     public void deleteById(Long id){
         repository.deleteById(id);
     }
