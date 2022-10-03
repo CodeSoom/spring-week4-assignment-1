80beb3f99f5ea69521aef1ae285857dee4394919
luckycw87 <blissful.pcw@gmail.com>
2022-10-03 20:54:13 +0900

    Feat: ProductService Interface 생성

---
+++ app/src/main/java/com/codesoom/assignment/domain/ProductCommand.java
@@ -0,0 +1,34 @@
+package com.codesoom.assignment.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+public class ProductCommand {
+
+    @Getter
+    @Builder
+    @ToString
+    public static class Register {
+        private final Long id;
+
+        private final String name;
+
+        private final String maker;
+
+        private final Long price;
+
+        private final String imageUrl;
+
+        public Product toEntity() {
+            return Product.builder()
+                    .id(id)
+                    .name(name)
+                    .maker(maker)
+                    .price(price)
+                    .imageUrl(imageUrl)
+                    .build();
+        }
+
+    }
+}
