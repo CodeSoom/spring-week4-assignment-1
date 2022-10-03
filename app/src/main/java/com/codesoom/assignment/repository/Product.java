0ff5ddf864392bffc572708bd80dda59992a2bd7
kckoh <kyungcheolkoh@gmail.com>
2022-10-02 17:50:20 -0700

    스프링 4주차 - 고양이 장난감가게 만들기
    이번 주도 잘 부탁드립니다!
    Product 클라스 테스트 케이스 작성

--- app/src/main/java/com/codesoom/assignment/repository/Product.java
+++ app/src/main/java/com/codesoom/assignment/repository/Product.java
@@ -1,10 +1,13 @@
 package com.codesoom.assignment.repository;
 
+import lombok.Getter;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@Getter
 @Entity
 public class Product {
     @Id
