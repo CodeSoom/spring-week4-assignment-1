dc397e5da27ba8c9e4dc15616f002f4dc84c4c45
Eunha Jeong <eunhaay@gmail.com>
2022-10-03 20:14:40 +0900

    feat: 장난감 도메인 추가

--- app/src/main/java/com/codesoom/assignment/domain/Product.java
+++ app/src/main/java/com/codesoom/assignment/domain/Product.java
@@ -1,4 +1,55 @@
 package com.codesoom.assignment.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Product {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    /** 장난감 이름 **/
+    private String name;
+
+    /** 가게 **/
+    private String maker;
+
+    /** 가격 **/
+    private int price;
+
+    /** 이미지 **/
+    private String imageUrl;
+
+    public Product() {
+    }
+
+    public Product(String name, String maker, int price, String imageUrl) {
+        this.name = name;
+        this.maker = maker;
+        this.price = price;
+        this.imageUrl = imageUrl;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getMaker() {
+        return maker;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
 }
