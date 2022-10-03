e6f49ea09938cb09734c1357b5640ca87ad98474
Eunha Jeong <eunhaay@gmail.com>
2022-10-03 20:13:47 +0900

    fix: type, id 정의 순서 수정

--- app/src/main/java/com/codesoom/assignment/repository/ProductRepository.java
+++ app/src/main/java/com/codesoom/assignment/repository/ProductRepository.java
@@ -3,5 +3,5 @@ package com.codesoom.assignment.repository;
 import com.codesoom.assignment.domain.Product;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ProductRepository extends CrudRepository<Long, Product> {
+public interface ProductRepository extends CrudRepository<Product, Long> {
 }
