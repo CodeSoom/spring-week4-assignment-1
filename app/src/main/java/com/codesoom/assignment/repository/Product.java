25bfffb374e6185ded38d5eaa8a6e5a8ef294f06
kckoh <kyungcheolkoh@gmail.com>
2022-10-02 18:07:07 -0700

    add: implement Product class
    Product 클라스 빌더/필드 구현

--- app/src/main/java/com/codesoom/assignment/repository/Product.java
+++ app/src/main/java/com/codesoom/assignment/repository/Product.java
@@ -1,6 +1,8 @@
 package com.codesoom.assignment.repository;
 
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -8,11 +10,28 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Getter
+@NoArgsConstructor
 @Entity
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private String name;
+
+    private String maker;
+
+    private Long price;
+
+    private String imageUrl;
+
+    @Builder
+    public Product(String name, String maker, Long price, String imageUrl) {
+        this.name = name;
+        this.maker = maker;
+        this.price = price;
+        this.imageUrl = imageUrl;
+    }
 
 
 }
