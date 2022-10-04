08cfbc68a17d1c98677b57e9853242a28ca9d648
hautblis <hautblis@gmail.com>
2022-10-04 20:24:44 +0900

    도메인 구현
    ProductRepository, Product

---
+++ app/src/main/java/com/codesoom/assignment/domain/Product.java
@@ -0,0 +1,52 @@
+package com.codesoom.assignment.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String maker;
+
+    private int price;
+
+    private String imageUrl;
+
+    public Product() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getMaker() {
+        return maker;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public Product(Long id, String name, String maker, int price, String imageUrl) {
+        this.id = id;
+        this.name = name;
+        this.maker = maker;
+        this.price = price;
+        this.imageUrl = imageUrl;
+    }
+}
