918a9c927df25633f5a8ea4adfd0b02b4053ffd7
hautblis <hautblis@gmail.com>
2022-10-04 20:26:36 +0900

    ProductService에 findAll(), save(), deleteAll() 구현

---
+++ app/src/main/java/com/codesoom/assignment/application/ProductService.java
@@ -0,0 +1,30 @@
+package com.codesoom.assignment.application;
+
+import com.codesoom.assignment.domain.Product;
+import com.codesoom.assignment.domain.ProductRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class ProductService {
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public List<Product> findAll() {
+        return productRepository.findAll();
+    }
+
+    public Product save(Product product) {
+        return productRepository.save(product);
+    }
+
+    public void deleteAll() {
+        productRepository.deleteAll();
+    }
+}
