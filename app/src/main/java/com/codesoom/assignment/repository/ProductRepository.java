d8aacd11e43e7ad8bc0225b70e25ff73a7a0207d
Eunha Jeong <eunhaay@gmail.com>
2022-10-04 15:08:28 +0900

    feat: findAll 메소드 반환값 List로 변경

--- app/src/main/java/com/codesoom/assignment/repository/ProductRepository.java
+++ app/src/main/java/com/codesoom/assignment/repository/ProductRepository.java
@@ -3,5 +3,8 @@ package com.codesoom.assignment.repository;
 import com.codesoom.assignment.domain.Product;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends CrudRepository<Product, Long> {
+    List<Product> findAll();
 }
