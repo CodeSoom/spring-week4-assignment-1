16d96593a2e56fadf9228f356d08d01297d6c148
yewon9609 <dodnjs1241@gmail.com>
2022-10-05 21:27:59 +0900

    product로 이름변경

--- app/src/main/java/com/codesoom/assignment/controllers/ProductController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/ProductController.java
@@ -1,12 +1,12 @@
 package com.codesoom.assignment.controllers;
 
-import com.codesoom.assignment.application.ToyService;
+import com.codesoom.assignment.application.ProductService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/products")
-public class ToyController {
+public class ProductController {
 
 //    * 고양이 장난감 목록 얻기 - `GET /products`
 //    * 고양이 장난감 상세 조회하기 - `GET /products/{id}`
@@ -14,9 +14,9 @@ public class ToyController {
 //    * 고양이 장난감 수정하기 - `PATCH /products/{id}`
 //    * 고양이 장난감 삭제하기 - `DELETE /products/{id}`
 
-    private final ToyService toyService;
+    private final ProductService toyService;
 
-    public ToyController(ToyService toyService) {
+    public ProductController(ProductService toyService) {
         this.toyService = toyService;
     }
 
