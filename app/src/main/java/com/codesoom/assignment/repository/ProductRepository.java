7e95796748ed0423a3150be37213788c24e46b07
yewon9609 <dodnjs1241@gmail.com>
2022-10-05 21:29:27 +0900

    product로 이름변경

--- app/src/main/java/com/codesoom/assignment/repository/ProductRepository.java
+++ app/src/main/java/com/codesoom/assignment/repository/ProductRepository.java
@@ -1,7 +1,7 @@
 package com.codesoom.assignment.repository;
 
-import com.codesoom.assignment.entity.Toy;
+import com.codesoom.assignment.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ToyRepository extends JpaRepository<Toy, Long> {
+public interface ProductRepository extends JpaRepository<Product, Long> {
 }
--- app/src/test/java/com/codesoom/assignment/repository/ProductRepositoryTest.java
+++ app/src/test/java/com/codesoom/assignment/repository/ProductRepositoryTest.java
@@ -1,7 +1,14 @@
 package com.codesoom.assignment.repository;
 
-import com.codesoom.assignment.entity.Toy;
-import org.junit.jupiter.api.*;
+import com.codesoom.assignment.entity.Product;
+
+import java.util.List;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
@@ -10,34 +17,35 @@ import java.util.Optional;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
-public class ToyRepositoryTest {
+@DisplayName("ProductRepository")
+public class ProductRepositoryTest {
 
     @Autowired
-    private ToyRepository toyRepository;
+    private ProductRepository productRepository;
 
     @Nested
     @DisplayName("findAll 메소드는")
     class Describe_find_all {
 
         @BeforeEach
-        void prepare() {
-            toyRepository.deleteAll();
+        void setUp() {
+            productRepository.deleteAll();
         }
 
         @Nested
-        @DisplayName("저장된 toy가 있다면")
-        class Context_have_toy {
+        @DisplayName("저장된 product가 있다면")
+        class Context_have_product {
 
             @BeforeEach
-            void setUp() {
-                Toy toy = new Toy(1L, "테스트1", 2000, "");
-                toyRepository.save(toy);
+            void prepare() {
+                Product toy = new Product(1L, "테스트1", 2000, "");
+                productRepository.save(toy);
             }
 
             @Test
-            @DisplayName("toyList를 리턴한다")
-            void it_returns_toyList() {
-                assertThat(toyRepository.findAll()).hasSize(1);
+            @DisplayName("ProductList를 리턴한다")
+            void it_returns_productList() {
+                assertThat(productRepository.findAll()).hasSize(1);
             }
         }
     }
@@ -47,18 +55,30 @@ public class ToyRepositoryTest {
     class Describe_find_by_id {
 
         @BeforeEach
-        void prepare() {
-            toyRepository.deleteAll();
+        void setUp() {
+            productRepository.deleteAll();
         }
 
         @Nested
         @DisplayName("id가 존재하지 않는다면")
         class Context_non_existent_id {
 
+            @BeforeEach
+            void prepare() {
+                Product source = new Product(1L, "테스트", 2000, "");
+                productRepository.save(source);
+                List<Product> all = productRepository.findAll();
+                for(Product p : all) {
+                    if (p.getId().equals(1L)){
+                        productRepository.deleteById(1L);
+                    }
+                }
+            }
+
             @Test
             @DisplayName("비어있는 Optional을 리턴한다")
-            void it_returns_empty_toyList() {
-                assertThat(toyRepository.findById(100L)).isEqualTo(Optional.empty());
+            void it_returns_empty_productList() {
+                assertThat(productRepository.findById(1L)).isEqualTo(Optional.empty());
             }
         }
 
@@ -66,16 +86,21 @@ public class ToyRepositoryTest {
         @DisplayName("id가 존재한다면")
         class Context_existent_id {
 
+            Product source;
+            Product saved;
+
             @BeforeEach
             void givenId() {
-                Toy toy = new Toy(1L, "테스트1", 2000, "");
-                toyRepository.save(toy);
+                source = new Product(1L, "테스트1", 2000, "");
+                saved = productRepository.save(source);
             }
 
             @Test
-            @DisplayName("해당 id를 가진 toy객체를 리턴한다")
-            void it_returns_toyList() {
-                assertThat(toyRepository.findById(1L).get()).isEqualTo(new Toy(1L, "테스트1", 2000, ""));
+            @DisplayName("해당 id를 가진 Product객체를 리턴한다")
+            void it_returns_productList() {
+                Product findProduct = productRepository.findById(saved.getId()).get();
+                Assertions.assertEquals(findProduct.getBrand(), source.getBrand());
+                Assertions.assertEquals(findProduct.getPrice(), source.getPrice());
             }
         }
     }
@@ -85,18 +110,21 @@ public class ToyRepositoryTest {
     class Describe_save {
 
         @BeforeEach
-        void prepare() {
-            toyRepository.deleteAll();
+        void setUp() {
+            productRepository.deleteAll();
         }
 
         @Nested
-        @DisplayName("Toy객체가 주어질 때")
-        class Context_existent_toy {
+        @DisplayName("Product객체가 주어질 때")
+        class Context_existent_product {
+
+            Product product = new Product(1L, "테스트1", 2000, "");
 
             @Test
-            @DisplayName("toy객체를 리턴한다")
-            void it_returns_toy() {
-                assertThat(toyRepository.save(new Toy(2L, "테스트2", 2000, "")).getBrand()).isEqualTo("테스트2");
+            @DisplayName("Product객체를 저장 후 리턴한다")
+            void it_returns_product() {
+                Product saved = productRepository.save(product);
+                assertThat(saved.getBrand()).isEqualTo(product.getBrand());
             }
         }
     }
@@ -106,19 +134,25 @@ public class ToyRepositoryTest {
     class Describe_delete_by_id {
 
         @BeforeEach
-        void prepare() {
-            toyRepository.deleteAll();
+        void setUp() {
+            productRepository.deleteAll();
         }
 
         @Nested
         @DisplayName("id가 존재한다면")
         class Context_existent_id {
 
+            @BeforeEach
+            void givenProduct() {
+                Product product = new Product(1L, "테스트1", 2000, "");
+                productRepository.save(product);
+            }
+
             @Test
-            @DisplayName("해당 id를 가진 toy객체를 삭제한다")
-            void it_returns_toy() {
-                toyRepository.deleteById(1L);
-                assertThat(toyRepository.findAll()).hasSize(0);
+            @DisplayName("해당 id를 가진 Product객체를 삭제한다")
+            void it_returns_product() {
+                productRepository.deleteById(1L);
+                assertThat(productRepository.findAll()).hasSize(0);
             }
         }
     }
