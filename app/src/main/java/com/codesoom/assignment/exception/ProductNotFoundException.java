c284b2e6f05ed2065cdfd240c9a5e57458c2a5ce
Eunha Jeong <eunhaay@gmail.com>
2022-10-05 19:15:59 +0900

    doc: ProductNotFoundException 설명 변경
    Co-authored-by: JohnGrib <johngrib82@gmail.com>

--- app/src/main/java/com/codesoom/assignment/exception/ProductNotFoundException.java
+++ app/src/main/java/com/codesoom/assignment/exception/ProductNotFoundException.java
@@ -1,7 +1,7 @@
 package com.codesoom.assignment.exception;
 
 /**
- * 장난감을 찾지 못했을 때 발생하는 예외
+ * 상품을 찾지 못했을 때 던집니다.
  */
 public class ProductNotFoundException extends RuntimeException {
     public ProductNotFoundException() {
