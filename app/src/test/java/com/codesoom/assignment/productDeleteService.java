8fe97e8ef581cba0aa6325aa1e6a83f78eee611a
yewon9609 <dodnjs1241@gmail.com>
2022-10-06 21:55:45 +0900

    refacotr:테스트 코드에서만 사용하는delete메소드 클래스로 분리

--- app/src/test/java/com/codesoom/assignment/productDeleteService.java
+++ app/src/test/java/com/codesoom/assignment/productDeleteService.java
@@ -1,2 +1,20 @@
-package com.codesoom.assignment;public class productDeleteService {
+package com.codesoom.assignment;
+
+import com.codesoom.assignment.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * The type Product delete service.
+ */
+public class productDeleteService {
+
+  @Autowired
+  private ProductRepository productRepository;
+
+  /**
+   * Delete all.
+   */
+  public void deleteAll() {
+    productRepository.deleteAll();
+  }
 }
