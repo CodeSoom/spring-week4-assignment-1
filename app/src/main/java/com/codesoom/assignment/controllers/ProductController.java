be097fd658a43383319e95a4d9cb3d3bf1ed198a
hautblis <hautblis@gmail.com>
2022-10-06 19:58:21 +0900

    feat : GET /products 에 대한 구현

---
+++ app/src/main/java/com/codesoom/assignment/controllers/ProductController.java
@@ -0,0 +1,26 @@
+package com.codesoom.assignment.controllers;
+
+import com.codesoom.assignment.application.ProductService;
+import com.codesoom.assignment.domain.Product;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public List<Product> getProducts() {
+        return productService.getProducts();
+    }
+}
