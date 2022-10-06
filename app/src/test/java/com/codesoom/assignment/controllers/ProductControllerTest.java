808af4754360dc69d55bb66599582b58a345e993
yewon9609 <dodnjs1241@gmail.com>
2022-10-06 21:58:32 +0900

    controllerTest클래스 생성

--- app/src/test/java/com/codesoom/assignment/controllers/ProductControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/ProductControllerTest.java
@@ -1,2 +1,32 @@
-package com.codesoom.assignment.controllers;public class ProductControllerTest {
+package com.codesoom.assignment.controllers;
+
+import com.codesoom.assignment.entity.Product;
+import org.hibernate.engine.transaction.jta.platform.internal.AtomikosJtaPlatform;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@DataJpaTest
+@WebMvcTest
+public class ProductControllerTest {
+
+  @Autowired
+  MockMvc mockMvc;
+
+  @Autowired
+  private ProductController productController;
+
+
+
 }
