9c3a4a87b5fb20f4b6ab00e20d17b964fc4bbef0
yewon9609 <dodnjs1241@gmail.com>
2022-10-07 20:38:07 +0900

    feat:테스트 코드에서만 사용하는 deleteAll클래스 생성

--- app/src/test/java/com/codesoom/assignment/ProductDeleteService.java
+++ app/src/test/java/com/codesoom/assignment/ProductDeleteService.java
@@ -2,19 +2,24 @@ package com.codesoom.assignment;
 
 import com.codesoom.assignment.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 
 /**
- * The type Product delete service.
+ * 테스트 코드에서만 사용하는 productDeleteService
  */
-public class productDeleteService {
+@Service
+public class ProductDeleteService {
 
   @Autowired
-  private ProductRepository productRepository;
+  private final ProductRepository productRepository;
 
-  /**
-   * Delete all.
-   */
+  public ProductDeleteService(ProductRepository productRepository) {
+    this.productRepository = productRepository;
+  }
+
   public void deleteAll() {
     productRepository.deleteAll();
   }
+
 }
