482ca2f58bc57f53c744ce9225f72a0d1f307b29
yewon9609 <dodnjs1241@gmail.com>
2022-10-07 20:40:59 +0900

    feat:ProductControllerTest클래스생성

--- app/src/test/java/com/codesoom/assignment/controllers/ProductControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/ProductControllerTest.java
@@ -1,32 +1,42 @@
 package com.codesoom.assignment.controllers;
 
-import com.codesoom.assignment.entity.Product;
-import org.hibernate.engine.transaction.jta.platform.internal.AtomikosJtaPlatform;
+import com.codesoom.assignment.application.ProductService;
+import com.codesoom.assignment.repository.ProductRepository;
+
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.hamcrest.Matchers.containsString;
-import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@DataJpaTest
-@WebMvcTest
+@SpringBootTest
+@AutoConfigureMockMvc
 public class ProductControllerTest {
 
   @Autowired
   MockMvc mockMvc;
 
+  private ProductController controller;
+  private ProductService service;
+
   @Autowired
-  private ProductController productController;
+  private ProductRepository repository;
 
+  @BeforeEach
+  void setUp() throws Exception {
+    service = new ProductService(repository);
+    controller = new ProductController(service);
 
+  }
+
+  @Test
+  void getList() throws Exception {
+    mockMvc.perform(get("/products"))
+        .andExpect(status().isOk());
+  }
 
 }
