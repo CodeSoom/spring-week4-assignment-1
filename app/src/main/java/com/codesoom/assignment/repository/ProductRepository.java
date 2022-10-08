e00c470157fb0cd76510de1aa411fe54f6b0a8c4
yewon9609 <dodnjs1241@gmail.com>
2022-10-08 21:42:04 +0900

    docs:javaDoc한글 수정

--- app/src/main/java/com/codesoom/assignment/repository/ProductRepository.java
+++ app/src/main/java/com/codesoom/assignment/repository/ProductRepository.java
@@ -4,7 +4,7 @@ import com.codesoom.assignment.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
- * The interface Product repository.
+ * 상품 저장소를 추상화합니다.
  */
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
