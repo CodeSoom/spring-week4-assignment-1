248207c8b35db354764bc193dee0649bc50af4c1
yewon9609 <dodnjs1241@gmail.com>
2022-10-08 21:38:52 +0900

    fead:update로직 변경 docs:javaDoc한글로 수정

--- app/src/main/java/com/codesoom/assignment/application/ProductService.java
+++ app/src/main/java/com/codesoom/assignment/application/ProductService.java
@@ -18,10 +18,10 @@ public class ProductService {
     }
 
     /**
-     * 매개변수로 받은 product 객체를 db에 저장한다
+     * 새로운 상품을 생성한다.
      *
      * @param product the product
-     * @return 저장한 product 를 리턴한다
+     * @return 저장한 상품
      */
     public Product create(Product product) {
        return productRepository.save(product);
@@ -31,25 +31,26 @@ public class ProductService {
      * id로 product 객체를 찾아 리턴한다
      *
      * @param id the id
-     * @return id로 찾은 product객체를 리턴한다
+     * @return 찾은 상품
      */
     public Product findById(Long id) {
         return productRepository.findById(id).orElseThrow(() -> new ProductNotFoundException("해당 id를 가진 상품이 존재하지 않습니다 id:" + id));
     }
 
     /**
+     * 모든 상품의 목록을 리턴합니다.
      *
-     * @return productList를 리턴한다
+     * @return 모든 상품
      */
     public List<Product> getList() {
         return productRepository.findAll();
     }
 
     /**
-     * id로 찾은 product 를 삭제한다
+     * 상품을 삭제하고, 삭제한 정보를 리턴합니다
      *
-     * @param id the id
-     * @return the product
+     * @param id 삭제할 상품의 식별자
+     * @return 삭제한 상품 정보
      */
     public Product remove(Long id) {
         Product product = productRepository.findById(id)
@@ -68,8 +69,21 @@ public class ProductService {
      */
     public Product update(Long id, Product product){
         return productRepository.findById(id)
-            .map(i->i.updateProduct(product))
+            .map(this::detailUpdate)
             .orElseThrow(() -> new ProductNotFoundException("해당 id를 가진 상품이 존재하지 않습니다 id:" + id));
+    }
+
+    public Product detailUpdate(Product product) {
+        if(product.getBrand() != null){
+            product.updateBrand(product.getBrand());
+        }
+        if(product.getPrice() != null) {
+            product.updatePrice(product.getPrice());
+        }
+        if(product.getImageUrl() != null) {
+            product.updateImageUrl(product.getImageUrl());
+        }
+        return product;
     }
 
 }
