306b6d5abda67843ac04d6386d57b4b196a8108e
yewon9609 <dodnjs1241@gmail.com>
2022-10-08 21:40:53 +0900

    style:ResponseStatus추가

--- app/src/main/java/com/codesoom/assignment/exception/ProductNotFoundException.java
+++ app/src/main/java/com/codesoom/assignment/exception/ProductNotFoundException.java
@@ -1,8 +1,12 @@
 package com.codesoom.assignment.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 /**
- * findById로 product를 찾지 못했을 때 던지는 exception
+ * 상품을 찾지 못했을 때 던집니다.
  */
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ProductNotFoundException extends RuntimeException{
 
   public ProductNotFoundException(String message) {
