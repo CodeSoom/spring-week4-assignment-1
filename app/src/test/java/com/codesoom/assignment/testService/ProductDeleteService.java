86fbc40dd6cc517c73dba90620fec3e31cc1bbba
yewon9609 <dodnjs1241@gmail.com>
2022-10-09 12:09:09 +0900

    docs:javaDoc수정

--- app/src/test/java/com/codesoom/assignment/testService/ProductDeleteService.java
+++ app/src/test/java/com/codesoom/assignment/testService/ProductDeleteService.java
@@ -1,4 +1,4 @@
-package com.codesoom.assignment.TestService;
+package com.codesoom.assignment.testService;
 
 import com.codesoom.assignment.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 
 
 /**
- * 테스트 코드에서만 사용하는 productDeleteService
+ * 테스트 코드에서만 사용합니다.
  */
 @Service
 public class ProductDeleteService implements TestOnly {
