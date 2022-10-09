beaf13a20f56ae7148423d0147ee6d5983bd4a68
yewon9609 <dodnjs1241@gmail.com>
2022-10-09 12:03:29 +0900

    docs:javaDoc수정

--- app/src/test/java/com/codesoom/assignment/TestService/TestOnly.java
+++
@@ -1,5 +0,0 @@
-package com.codesoom.assignment.TestService;
-
-public interface TestOnly {
-
-}
---
+++ app/src/test/java/com/codesoom/assignment/testService/TestOnly.java
@@ -0,0 +1,9 @@
+package com.codesoom.assignment.testService;
+
+
+/**
+ * testService 내 클래스들이 상속받는 마커인터페이스.
+ */
+public interface TestOnly {
+
+}
