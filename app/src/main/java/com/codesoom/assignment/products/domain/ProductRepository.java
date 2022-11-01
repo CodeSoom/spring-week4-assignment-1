a8ca7a2692dd7787572b4db1c146b2e42ea50c5b
giibeom <kpmyung@gmail.com>
2022-11-01 14:15:16 +0900

    feat: 상품의 id로 장난감 정보를 조회하는 ProductRepository의 findById() 테스트 완성

--- app/src/main/java/com/codesoom/assignment/products/domain/ProductRepository.java
+++ app/src/main/java/com/codesoom/assignment/products/domain/ProductRepository.java
@@ -1,11 +1,8 @@
 package com.codesoom.assignment.products.domain;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
-public interface ProductRepository extends CrudRepository<Product, Long> {
-    List<Product> findAll();
+public interface ProductRepository extends JpaRepository<Product, Long> {
 }
--- app/src/test/java/com/codesoom/assignment/products/domain/ProductRepositoryTest.java
+++ app/src/test/java/com/codesoom/assignment/products/domain/ProductRepositoryTest.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.util.List;
+import java.util.Optional;
 
 import static com.codesoom.assignment.support.ProductFixture.TOY_1;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -18,8 +19,18 @@ import static org.assertj.core.api.Assertions.assertThat;
 @DataJpaTest
 @DisplayName("Product Repository 유닛 테스트")
 class ProductRepositoryTest {
+    private static final Long TEST_ID = 1L;
     @Autowired
     private ProductRepository productRepository;
+
+    /**
+     * 계층 구조 테스트 특성상 테스트 인스턴스의 생명주기가 공유됩니다.
+     * 따라서 매 테스트마다 모든 fixture 데이터 삭제를 진행합니다.
+     */
+    @AfterEach
+    void setUpDeleteFixture() {
+        productRepository.deleteAll();
+    }
 
     @Nested
     @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
@@ -27,7 +38,7 @@ class ProductRepositoryTest {
 
         @Nested
         @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
-        class 등록된_장난감이_없을_경우 {
+        class 등록된_장난감이_없을_때 {
             @Test
             @DisplayName("빈 리스트를 리턴한다")
             void it_returns_empty_list() {
@@ -38,18 +49,10 @@ class ProductRepositoryTest {
 
         @Nested
         @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
-        class 등록된_장난감이_있을_경우 {
-            private Product productSource;
-
+        class 등록된_장난감이_있을_때 {
             @BeforeEach
             void setUpCreate() {
-                productSource = TOY_1.생성();
-                productRepository.save(productSource);
-            }
-
-            @AfterEach
-            void setUpDelete() {
-                productRepository.delete(productSource);
+                productRepository.save(TOY_1.생성());
             }
 
             @Test
@@ -59,6 +62,40 @@ class ProductRepositoryTest {
 
                 assertThat(products).isNotEmpty();
                 assertThat(products).hasSize(1);
+            }
+        }
+    }
+
+    @Nested
+    @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+    class findById_메서드는 {
+
+        @Nested
+        @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+        class 찾을_수_없는_id가_주어지면 {
+            @Test
+            @DisplayName("빈 값을 리턴한다")
+            void it_returns_empty_product() {
+                assertThat(productRepository.findById(TEST_ID))
+                        .isEmpty();
+            }
+        }
+
+        @Nested
+        @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+        class 찾을_수_있는_id가_주어지면 {
+            @BeforeEach
+            void setUpCreateFixture() {
+                productRepository.save(TOY_1.생성(TEST_ID));
+            }
+
+            @Test
+            @DisplayName("해당 id의 장난감 정보를 리턴한다")
+            void it_returns_product() {
+                Optional<Product> product = productRepository.findById(TEST_ID);
+
+                assertThat(product).isNotEmpty();
+                assertThat(product.get().getId()).isEqualTo(TEST_ID);
             }
         }
     }
