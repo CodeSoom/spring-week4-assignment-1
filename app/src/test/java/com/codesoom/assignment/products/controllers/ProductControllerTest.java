ccb4e9cb83f10e1ed2f53f48cad533847a7835a7
giibeom <kpmyung@gmail.com>
2022-11-03 17:53:47 +0900

    refactor: ProductController 테스트 내 mock 전부 제거 작업 (실제 객체 사용)

--- app/src/test/java/com/codesoom/assignment/products/controllers/ProductControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/products/controllers/ProductControllerTest.java
@@ -3,6 +3,7 @@ package com.codesoom.assignment.products.controllers;
 
 import com.codesoom.assignment.exception.products.ProductNotFoundException;
 import com.codesoom.assignment.products.application.ProductService;
+import com.codesoom.assignment.products.domain.FakeProductRepository;
 import com.codesoom.assignment.products.domain.Product;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -10,28 +11,34 @@ import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import static com.codesoom.assignment.support.ProductFieldFixture.TEST_EXIST;
 import static com.codesoom.assignment.support.ProductFieldFixture.TEST_NOT_EXIST;
 import static com.codesoom.assignment.support.ProductFixture.TOY_1;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
 
+@SpringBootTest
 @DisplayName("ProductController 유닛 테스트")
 class ProductControllerTest {
-    private ProductController productController;
-    private ProductService productService;
+    private final ProductController productController;
+    private final ProductService productService;
+    private final FakeProductRepository fakeProductRepository;
 
+    @Autowired
+    ProductControllerTest(ProductController productController, ProductService productService, FakeProductRepository fakeProductRepository) {
+        this.productController = productController;
+        this.productService = productService;
+        this.fakeProductRepository = fakeProductRepository;
+    }
+
+    // Service의 delete 기능이 개발되기 전까지 대체합니다.
     @BeforeEach
-    void setUpVariable() {
-        productService = mock(ProductService.class);
-        productController = new ProductController(productService);
+    void setUpDeleteFixture() {
+        fakeProductRepository.deleteAllInBatch();
     }
 
     @Nested
@@ -56,9 +63,7 @@ class ProductControllerTest {
             @Test
             @DisplayName("비어있지 않은 리스트를 리턴한다")
             void it_returns_list() {
-                List<Product> products = new ArrayList<>();
-                products.add(TOY_1.생성());
-                given(productService.getProducts()).willReturn(products);
+                productService.createProduct(TOY_1.요청_데이터_생성());
 
                 assertThat(productController.list()).isNotEmpty();
             }
@@ -75,13 +80,8 @@ class ProductControllerTest {
             @Test
             @DisplayName("예외를 던진다")
             void it_returns_exception() {
-                given(productService.getProduct(TEST_NOT_EXIST.ID()))
-                        .willThrow(ProductNotFoundException.class);
-
                 assertThatThrownBy(() -> productController.detail(TEST_NOT_EXIST.ID()))
                         .isInstanceOf(ProductNotFoundException.class);
-
-                verify(productService).getProduct(TEST_NOT_EXIST.ID());
             }
         }
 
@@ -91,15 +91,15 @@ class ProductControllerTest {
             @Test
             @DisplayName("해당 id의 장난감 정보를 리턴한다")
             void it_returns_product() {
-                given(productService.getProduct(TEST_EXIST.ID()))
-                        .willReturn(TOY_1.생성(TEST_EXIST.ID()));
+                Product productSource = productService.createProduct(TOY_1.요청_데이터_생성());
 
-                Product product = productController.detail(TEST_EXIST.ID());
-
-                verify(productService).getProduct(TEST_EXIST.ID());
+                Product product = productController.detail(productSource.getId());
 
                 assertThat(product).isNotNull();
-                assertThat(product.getId()).isEqualTo(TEST_EXIST.ID());
+                assertThat(product.getName()).isEqualTo(TOY_1.NAME());
+                assertThat(product.getMaker()).isEqualTo(TOY_1.MAKER());
+                assertThat(product.getPrice()).isEqualTo(TOY_1.PRICE());
+                assertThat(product.getImgUrl()).isEqualTo(TOY_1.IMAGE());
             }
         }
     }
