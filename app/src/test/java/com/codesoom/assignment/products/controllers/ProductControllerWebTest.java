952cecc270021350fbb8233382f2196adb641868
giibeom <kpmyung@gmail.com>
2022-11-04 19:13:43 +0900

    feat: ProductController 통합 웹 테스트 - 찾을 수 있는 id로 상품 수정할 시 200 코드 반환 테스트 추가

--- app/src/test/java/com/codesoom/assignment/products/controllers/ProductControllerWebTest.java
+++ app/src/test/java/com/codesoom/assignment/products/controllers/ProductControllerWebTest.java
@@ -1,8 +1,8 @@
 package com.codesoom.assignment.products.controllers;
 
-import com.codesoom.assignment.products.application.ProductService;
 import com.codesoom.assignment.products.domain.Product;
 import com.codesoom.assignment.util.JsonUtil;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
@@ -16,8 +16,10 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import static com.codesoom.assignment.support.ProductFieldFixture.TEST_NOT_EXIST;
 import static com.codesoom.assignment.support.ProductFixture.TOY_1;
+import static com.codesoom.assignment.support.ProductFixture.TOY_2;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -28,7 +30,7 @@ public class ProductControllerWebTest {
     private MockMvc mockMvc;
 
     @Autowired
-    private ProductService productService;
+    private ProductController productController;
 
     @Nested
     @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
@@ -53,7 +55,7 @@ public class ProductControllerWebTest {
             @Test
             @DisplayName("200 코드를 반환한다")
             void it_responses_200() throws Exception {
-                Product productSource = productService.createProduct(TOY_1.등록_요청_데이터_생성());
+                Product productSource = productController.create(TOY_1.등록_요청_데이터_생성());
 
                 mockMvc.perform(
                                 get("/products/" + productSource.getId())
@@ -81,13 +83,51 @@ public class ProductControllerWebTest {
     class create_메서드는 {
         @Test
         @DisplayName("201 코드를 반환한다")
-        void it_returns_201() throws Exception {
+        void it_responses_201() throws Exception {
             mockMvc.perform(
                             post("/products")
                                     .contentType(MediaType.APPLICATION_JSON)
                                     .content(JsonUtil.writeValue(TOY_1.등록_요청_데이터_생성()))
                     )
                     .andExpect(status().isCreated());
+        }
+    }
+
+    @Nested
+    @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+    class update_메서드는 {
+        @Nested
+        @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+        class 찾을_수_있는_id가_주어지면 {
+
+            private Long fixtureId;
+
+            @BeforeEach
+            void setUpCreateFixture() {
+                Product productSource = productController.create(TOY_1.등록_요청_데이터_생성());
+                fixtureId = productSource.getId();
+            }
+
+            @Test
+            @DisplayName("200 코드를 반환한다")
+            void it_responses_200() throws Exception {
+                mockMvc.perform(
+                                put("/products/" + fixtureId)
+                                        .contentType(MediaType.APPLICATION_JSON)
+                                        .content(JsonUtil.writeValue(TOY_2.수정_요청_데이터_생성()))
+                        )
+                        .andExpect(status().isOk());
+            }
+        }
+
+        @Nested
+        @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+        class 찾을_수_없는_id가_주어지면 {
+            @Test
+            @DisplayName("404 코드를 반환한다")
+            void it_responses_404() {
+
+            }
         }
     }
 }
