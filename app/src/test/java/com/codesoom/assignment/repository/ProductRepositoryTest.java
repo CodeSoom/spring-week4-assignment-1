a3b4337144c184fa9542c97589813bf5b915d9c7
EuijeongHwang_Mac <euijeong.hwang97@gmail.com>
2022-11-30 21:01:53 +0900

    test: ProductRepository 테스트 코드 작성
    CRUD 테스트 코드 작성함. 매 테스트 코드 실행 후 ID 초기화하는 방법을 찾아야 함.

--- app/src/test/java/com/codesoom/assignment/repository/ProductRepositoryTest.java
+++ app/src/test/java/com/codesoom/assignment/repository/ProductRepositoryTest.java
@@ -1,23 +1,32 @@
 package com.codesoom.assignment.repository;
 
+import com.codesoom.assignment.ProductNotFoundException;
 import com.codesoom.assignment.domain.Product;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.mock.mockito.MockBean;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
-@AutoConfigureMockMvc
+@Transactional
 @DisplayName("ProductRepository 클래스 테스트")
 class ProductRepositoryTest {
 
-    @MockBean
+    @Autowired
     private ProductRepository repository;
+
+    private static final String PRODUCT_NAME = "춘식이 고구마 장난감";
+    private static final String MAKER        = "카카오";
+    private static final Long PRICE          = 39000L;
+    private static final String IMAGE_URL    = "http://localhost:8080/snake";
 
     @Nested
     @DisplayName("save 메소드는")
@@ -28,26 +37,36 @@ class ProductRepositoryTest {
             @Test
             @DisplayName("상품을 새로 만들어 리턴한다")
             void it_return_new_product() {
-                Product product = new Product("춘식이 고구마 장난감", "kakao", 39000L, "");
+                Product product = new Product(PRODUCT_NAME, MAKER, PRICE, IMAGE_URL);
 
-                repository.save(product);
+                Product toy = repository.save(product);
 
-                assertThat(repository.findById(1L)).isNotNull();
+                assertThat(toy.getProductName()).isEqualTo(PRODUCT_NAME);
+                assertThat(repository.findAll().size()).isEqualTo(1);
             }
         }
 
         @Nested
         @DisplayName("id가 존재할 때")
         class Context_with_id {
-            @BeforeEach
-            void setUp() {
-
-            }
-
             @Test
             @DisplayName("상품을 업데이트한다")
             void it_update_product() {
+                //given
+                Product product = new Product(PRODUCT_NAME, MAKER, PRICE, IMAGE_URL);
 
+                repository.save(product);
+
+                assertThat(product.getProductName()).isEqualTo(PRODUCT_NAME);
+
+                //when
+                product.setProductName("춘식이 감자 장난감");
+                product.setPrice(29000L);
+
+                repository.save(product);
+
+                //then
+                assertThat(product.getProductName()).isEqualTo("춘식이 감자 장난감");
             }
         }
     }
@@ -55,10 +74,23 @@ class ProductRepositoryTest {
     @Nested
     @DisplayName("findAll 메소드는")
     class Describe_findAll {
+        @BeforeEach
+        void setUp() {
+            Product product  = new Product(PRODUCT_NAME, MAKER, PRICE, IMAGE_URL);
+            Product product2 = new Product(PRODUCT_NAME, MAKER, PRICE, IMAGE_URL);
+            Product product3 = new Product(PRODUCT_NAME, MAKER, PRICE, IMAGE_URL);
+
+            repository.save(product);
+            repository.save(product2);
+            repository.save(product3);
+        }
+
         @Test
         @DisplayName("모든 Product 리스트를 리턴한다")
         void it_return_products() {
+            List<Product> products = repository.findAll();
 
+            assertThat(products.size()).isEqualTo(3);
         }
     }
 
@@ -68,17 +100,37 @@ class ProductRepositoryTest {
         @Test
         @DisplayName("해당하는 id의 Product를 리턴한다")
         void it_return_product() {
+            //given
+            Product product  = new Product(PRODUCT_NAME, MAKER, PRICE, IMAGE_URL);
 
+            repository.save(product);
+
+            //when
+            Optional<Product> findProduct = repository.findById(1L)
+                                                        .stream()
+                                                        .findFirst();
+
+            //then
+            assertThat(findProduct).isPresent();
         }
     }
 
     @Nested
     @DisplayName("deleteById 메소드는")
     class Describe_deleteById {
+        @BeforeEach
+        void setUp() {
+            Product product  = new Product(PRODUCT_NAME, MAKER, PRICE, IMAGE_URL);
+
+            repository.save(product);
+        }
+
         @Test
         @DisplayName("해당하는 id의 Product를 삭제한다")
         void it_remove_product() {
+            repository.deleteById(1L);
 
+            assertThat(repository.existsById(1L)).isFalse();
         }
     }
 }
