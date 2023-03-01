bc079c6a86f94f9425086490f78a049d1ded84e8
tlatmsrud <tlatmsrud@naver.com>
2023-03-01 23:38:46 +0900

    CatToyStoreRepository에 대한 테스트코드를 작성하라

--- app/src/test/java/com/codesoom/assignment/repository/CatToyStoreRepositoryTest.java
+++ app/src/test/java/com/codesoom/assignment/repository/CatToyStoreRepositoryTest.java
@@ -1,4 +1,28 @@
+package com.codesoom.assignment.repository;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class CatToyStoreRepositoryTest {
-  
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void existsById() {
+    }
+
+    @Test
+    void deleteById() {
+    }
 }
