4b16a280b54d2507f753eed4d57a193b454f31c1
tlatmsrud <tlatmsrud@naver.com>
2023-03-07 00:39:22 +0900

    테스트 오류에 대한 피드백 반영

--- app/src/test/java/com/codesoom/assignment/repository/CatToyStoreRepositoryTest.java
+++ app/src/test/java/com/codesoom/assignment/repository/CatToyStoreRepositoryTest.java
@@ -22,7 +22,7 @@ class CatToyStoreRepositoryTest {
 
     @Autowired
     private CatToyStoreRepository catToyStoreRepository;
-    private static final Long DEFAULT_ID = 1L;
+    private Long savedId;
     private static final String DEFAULT_NAME = "첫번째 고양이 장난감";
     private static final Long INVALID_ID = 100L;
     private static final Long CREATE_ID = 2L;
@@ -32,22 +32,25 @@ class CatToyStoreRepositoryTest {
     @BeforeEach
     void setUp(){
         CatToy catToy = new CatToy();
-        catToy.setId(DEFAULT_ID);
         catToy.setName(DEFAULT_NAME);
         catToy.setPrice(1000);
         catToy.setMaker("첫번째 브랜드");
         catToy.setImageUrl("https://avatars.githubusercontent.com/u/9374562?s=400&v=4");
 
-        catToyStoreRepository.save(catToy);
+        CatToy savedCatToy =  catToyStoreRepository.save(catToy);
+        savedId = savedCatToy.getId();
     }
 
-
+    @AfterEach
+    void end(){
+        catToyStoreRepository.deleteAll();
+    }
     @Test
     @DisplayName("리스트 조회 > 1개 리스트 조회 및 상품에 대한 id, name 일치")
     void findAll() {
         List<CatToy> list = catToyStoreRepository.findAll();
         assertThat(list).hasSize(1);
-        assertThat(list.get(0).getId()).isEqualTo(DEFAULT_ID);
+        assertThat(list.get(0).getId()).isEqualTo(savedId);
         assertThat(list.get(0).getName()).isEqualTo(DEFAULT_NAME);
 
     }
@@ -55,9 +58,9 @@ class CatToyStoreRepositoryTest {
     @Test
     @DisplayName("유효 ID에 대한 상세조회 > 조회 상품에 대한 id, name 일치")
     void findByIdWithValidId() {
-        CatToy catToy = catToyStoreRepository.findById(DEFAULT_ID).get();
+        CatToy catToy = catToyStoreRepository.findById(savedId).get();
 
-        assertThat(catToy.getId()).isEqualTo(DEFAULT_ID);
+        assertThat(catToy.getId()).isEqualTo(savedId);
         assertThat(catToy.getName()).isEqualTo(DEFAULT_NAME);
     }
 
@@ -72,7 +75,6 @@ class CatToyStoreRepositoryTest {
     @DisplayName("저장 > 1 증가된 리스트 개수 확인 (예상 : 2)")
     void save() {
         CatToy catToy = new CatToy();
-        catToy.setId(CREATE_ID);
         catToy.setName(CREATE_NAME);
         catToy.setPrice(2000);
         catToy.setMaker("두번째 브랜드");
@@ -86,8 +88,8 @@ class CatToyStoreRepositoryTest {
     @Test
     @DisplayName("삭제 > 삭제한 ID에 대한 상세 조회 결과가 빈 Optional 인지 확인")
     void deleteById() {
-        catToyStoreRepository.deleteById(DEFAULT_ID);
-        assertThat(catToyStoreRepository.findById(DEFAULT_ID)).isEmpty();
+        catToyStoreRepository.deleteById(savedId);
+        assertThat(catToyStoreRepository.findById(savedId)).isEmpty();
     }
 
 
