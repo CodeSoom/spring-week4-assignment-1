18b76ebe7b8d9d91908972a3e43a10bada0fb08f
yushin <kkad45@naver.com>
2023-03-29 13:29:56 +0900

    feat 고양이 생성 로직 테스트 코드 작성
    - Cat.create() 메소드 테스트 코드 작성

---
+++ app/src/test/java/com/codesoom/assignment/domain/CatTest.java
@@ -0,0 +1,40 @@
+package com.codesoom.assignment.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("Cat Class")
+class CatTest {
+
+    @Nested
+    @DisplayName("createMethod는")
+    class create {
+
+        @Nested
+        @DisplayName("모든 파라미터가 있을 경우")
+        class parameter_all {
+
+            @Test
+            @DisplayName("모든 데이터가 들어있는 고양이를 생성한다.")
+            public void create_all_parameter() {
+                String name = "고양이이름";
+                String maker = "아디다스";
+                Long price = 5000L;
+                String imgUrl = "test.jpg";
+
+                Cat cat = Cat.create(name, maker, price, imgUrl);
+                assertAll(() -> assertThat(cat.getName()).isEqualTo(name));
+                assertAll(() -> assertThat(cat.getMaker()).isEqualTo(maker));
+                assertAll(() -> assertThat(cat.getPrice()).isEqualTo(price));
+                assertAll(() -> assertThat(cat.getImgUrl).isEqualTo(imgUrl));
+            }
+        }
+    }
+
+
+}
