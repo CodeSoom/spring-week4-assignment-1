88acfe3477734a05d4fae5995d1dd688dab41d8e
EunjinJwa <ejjwa0563@gmail.com>
2023-05-27 20:04:29 +0900

    JPA Entity 설정

--- app/src/main/java/com/codesoom/assignment/controller/ProductController.java
+++ app/src/main/java/com/codesoom/assignment/controller/ProductController.java
@@ -28,6 +28,7 @@ public class ProductController {
 	}
 
 	@PostMapping()
+	@ResponseStatus(HttpStatus.CREATED)
 	public Product create(@RequestBody Product product) {
 		return productService.createProduct(product);
 	}
--- app/src/main/java/com/codesoom/assignment/domain/Product.java
+++ app/src/main/java/com/codesoom/assignment/domain/Product.java
@@ -1,16 +1,22 @@
 package com.codesoom.assignment.domain;
 
 import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
 import lombok.ToString;
 
-@Getter @Setter
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 @ToString
 @EqualsAndHashCode(exclude = {"id"})
 public class Product {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
+
 	private String name;
 	private String maker;
 	private long price;
@@ -23,5 +29,45 @@ public class Product {
 		this.imageUrl = product.getImageUrl();
 
 		return this;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getMaker() {
+		return maker;
+	}
+
+	public void setMaker(String maker) {
+		this.maker = maker;
+	}
+
+	public long getPrice() {
+		return price;
+	}
+
+	public void setPrice(long price) {
+		this.price = price;
+	}
+
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
 	}
 }
