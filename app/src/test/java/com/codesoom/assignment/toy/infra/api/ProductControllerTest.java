c89696c0b3e31e8ab4de5b9496a5f8fe59001b99
tmxhsk99 <tmxhsk99@naver.com>
2023-07-24 14:50:08 +0900

    test : 상품리스트 조회 테스트 작성

--- app/src/test/java/com/codesoom/assignment/toy/infra/api/ProductControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/toy/infra/api/ProductControllerTest.java
@@ -1,7 +1,10 @@
 package com.codesoom.assignment.toy.infra.api;
 
+import com.codesoom.assignment.toy.domain.Product;
 import com.codesoom.assignment.toy.domain.dto.ProductRequest;
+import com.codesoom.assignment.toy.infra.persistence.ProductRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +13,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -23,7 +27,15 @@ class ProductControllerTest {
     MockMvc mockMvc;
 
     @Autowired
+    ProductRepository productRepository;
+
+    @Autowired
     ObjectMapper objectMapper;
+
+    @BeforeEach
+    void setUp() {
+        productRepository.deleteAll();
+    }
 
     @Test
     @DisplayName("상품을 등록 후 생성된 상품정보를 반환한다.")
@@ -44,4 +56,26 @@ class ProductControllerTest {
                 .andDo(print());
     }
 
+    @Test
+    @DisplayName("상품을 리스트를 조회한다")
+    void getProducts() throws Exception {
+        // given
+        Product product = Product.builder()
+                .name("catToy1")
+                .price(2000)
+                .maker("maker1")
+                .imageUrl("test/img1.jpg")
+                .build();
+
+        productRepository.save(product);
+
+        // expected
+        mockMvc.perform(get("/products"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("[0].name").value("catToy1"))
+                .andExpect(jsonPath("[0].maker").value("maker1"))
+                .andExpect(jsonPath("[0].price").value(2000))
+                .andExpect(jsonPath("[0].imageUrl").value("test/img1.jpg"))
+                .andDo(print());
+    }
 }
