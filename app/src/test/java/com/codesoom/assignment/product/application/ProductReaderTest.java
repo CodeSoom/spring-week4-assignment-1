f2c036d676a7b65cb262b321b2630723ee9da064
tmxhsk99 <tmxhsk99@naver.com>
2023-07-27 05:18:56 +0900

    test : getProduct 테스트 추가

--- app/src/test/java/com/codesoom/assignment/product/application/ProductReaderTest.java
+++ app/src/test/java/com/codesoom/assignment/product/application/ProductReaderTest.java
@@ -8,11 +8,73 @@ import org.junit.jupiter.api.*;
 
 @SuppressWarnings({"InnerClassMayBeStatic", "NonAsciiCharacters"})
 @DisplayName("ProductReader 클래스")
-public class ProductReaderTest extends JpaTest{
+public class ProductReaderTest extends JpaTest {
+
+    private final String PRODUCT_NAME = "testProduct";
+    private final String PRODUCT_IMAGE_URL = "testImage";
+    private final int PRODUCT_PRICE = 1000;
+    private final String PRODUCT_MAKER = "testMaker";
+
+    private Product createProduct() {
+        Product testProduct = Product.builder()
+                .name(PRODUCT_NAME)
+                .imageUrl(PRODUCT_IMAGE_URL)
+                .price(PRODUCT_PRICE)
+                .maker(PRODUCT_MAKER)
+                .build();
+        return testProduct;
+    }
+
+    @Nested
+    @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+    class getProduct_메서드는 {
+        ProductReader productReader = new ProductReader(productRepository);
+        @Nested
+        @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+        class 해당하는_상품이_없다면 {
+            @BeforeEach
+            void setUp() {
+                productRepository.deleteAll();
+            }
+
+            @Test
+            @DisplayName("ProductNotFoundException을_반환한다")
+            void ProductNotFoundException을_반환한다() {
+                Assertions.assertThatThrownBy(() -> productReader.getProduct(1L))
+                        .isInstanceOf(ProductNotFoundException.class);
+            }
+        }
+
+        @Nested
+        @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+        class 해당하는_상품이_있다면 {
+
+            @BeforeEach
+            void setUp() {
+                productRepository.deleteAll();
+            }
+
+
+            @Test
+            @DisplayName("해당하는_상품을_리턴한다")
+            void 해당하는_상품을_리턴한다() {
+                Product testProduct = createProduct();
+                productRepository.save(testProduct);
+
+                Assertions.assertThat(productReader.getProduct(testProduct.getId()).getName()).isEqualTo(PRODUCT_NAME);
+                Assertions.assertThat(productReader.getProduct(testProduct.getId()).getImageUrl()).isEqualTo(PRODUCT_IMAGE_URL);
+                Assertions.assertThat(productReader.getProduct(testProduct.getId()).getPrice()).isEqualTo(PRODUCT_PRICE);
+                Assertions.assertThat(productReader.getProduct(testProduct.getId()).getMaker()).isEqualTo(PRODUCT_MAKER);
+            }
+        }
+
+
+    }
+
     @Nested
     @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
     class getProductList_메서드는 {
-
+        ProductReader productReader = new ProductReader(productRepository);
         @Nested
         @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
         class 저장된_상품이_없다면 {
@@ -20,10 +82,10 @@ public class ProductReaderTest extends JpaTest{
             void setUp() {
                 productRepository.deleteAll();
             }
+
             @Test
             @DisplayName("빈 리스트를_리턴한다")
             void 빈_리스트를_리턴한다() {
-                ProductReader productReader = new ProductReader(productRepository);
                 Assertions.assertThat(productReader.getProductList()).isEmpty();
             }
         }
@@ -31,24 +93,9 @@ public class ProductReaderTest extends JpaTest{
         @Nested
         @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
         class 저장된_상품이_있다면 {
-            private final String PRODUCT_NAME = "testProduct";
-            private final String PRODUCT_IMAGE_URL = "testImage";
-            private final int PRODUCT_PRICE = 1000;
-            private final String PRODUCT_MAKER = "testMaker";
-
             @BeforeEach
             void setUp() {
                 productRepository.deleteAll();
-            }
-
-            private Product createProduct() {
-                Product testProduct = Product.builder()
-                        .name(PRODUCT_NAME)
-                        .imageUrl(PRODUCT_IMAGE_URL)
-                        .price(PRODUCT_PRICE)
-                        .maker(PRODUCT_MAKER)
-                        .build();
-                return testProduct;
             }
 
             @Test
@@ -57,8 +104,6 @@ public class ProductReaderTest extends JpaTest{
                 Product testProduct = createProduct();
                 productRepository.save(testProduct);
 
-
-                ProductReader productReader = new ProductReader(productRepository);
                 Assertions.assertThat(productReader.getProductList()).isNotEmpty();
                 Product product = productReader.getProductList().iterator().next();
                 Assertions.assertThat(product.getName()).isEqualTo(PRODUCT_NAME);
