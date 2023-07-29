0e310331014c70a2e3be3859b87faf656da02c08
tmxhsk99 <tmxhsk99@naver.com>
2023-07-30 08:44:50 +0900

    chore : lombok getter에서 일반 getter로 수정

--- app/src/test/java/com/codesoom/assignment/product/application/JpaTest.java
+++ app/src/test/java/com/codesoom/assignment/product/application/JpaTest.java
@@ -1,7 +1,6 @@
 package com.codesoom.assignment.product.application;
 
 import com.codesoom.assignment.product.infra.persistence.ProductRepository;
-import lombok.Getter;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -9,8 +8,11 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
 @DataJpaTest
-@Getter
 public class JpaTest {
     @Autowired
     ProductRepository productRepository;
+
+    public ProductRepository getProductRepository() {
+        return productRepository;
+    }
 }
